2f09c931138c3e613fd92fd2c34e3d474309c3aa
yangjiao2 <tubaobao4321@sina.com>
2016-03-30 11:28:31 -0700

    changed print statement

--- textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/SudeepHelloWorld.java
+++ textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/SudeepHelloWorld.java
@@ -8,11 +8,11 @@ public class SudeepHelloWorld
 {
     public static void main( String[] args )
     {
-        //Bad comments
+        // Bad comments
         System.out.println( "Hello World!" );
         // TOBE deleted
         while (true){
-            System.out.println( "Stop me!" );
+            System.out.println( "Woohoo!" );
         }
     }
 }
