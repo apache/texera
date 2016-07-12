603f5a583cba29152b538535686251db471e99f7
Xuxip <haileyxxpan@gmail.com>
2016-07-12 10:40:25 -0700

    find related tweets using dictionary matcher

---
+++ textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/tweetsdictmatcher/ResultedTweetsWriter.java
@@ -0,0 +1,56 @@
+package edu.uci.ics.textdb.sandbox.tweetsdictmatcher;
+
+import java.util.List;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+
+import edu.uci.ics.textdb.api.common.*;
+import edu.uci.ics.textdb.api.storage.IDataStore;
+import edu.uci.ics.textdb.common.constants.DataConstants.SourceOperatorType;
+import edu.uci.ics.textdb.common.exception.StorageException;
+import edu.uci.ics.textdb.common.field.DataTuple;
+import edu.uci.ics.textdb.storage.DataStore;
+import edu.uci.ics.textdb.storage.writer.DataWriter;
+
+public class ResultedTweetsWriter {
+
+	public static void writeResultedTweetsToIndex(String getResultFrom, String writeTo,SourceOperatorType srcOpType, boolean override, String... query) 
+			throws StorageException, Exception  {
+	
+	ITuple tuple = null;
+	IDataStore dataStore = new DataStore(writeTo, TweetsConstants.SCHEMA_TWEETS) ;
+	Analyzer analyzer = new StandardAnalyzer();
+	DataWriter dataWriter = new DataWriter(dataStore, analyzer);
+	if(override){ 
+		dataWriter.clearData();
+	}
+	dataWriter.open();
+	TweetsDictMatcher.open(getResultFrom, srcOpType , query);
+	
+    while ((tuple = TweetsDictMatcher.getNextTuple()) != null) {
+    	dataWriter.writeTuple(removeSpan(tuple));	
+    
+    }
+    TweetsDictMatcher.close();
+	dataWriter.close();
+	 
+	 
+}
+	
+	
+	public static ITuple removeSpan(ITuple tuple){
+	ITuple newtuple = null;
+	
+	List<IField> fields = tuple.getFields();
+	IField[] newfields = new IField[fields.size()-1];
+	newfields[0] = fields.get(0);
+	newfields[1] = fields.get(1);
+	newfields[2] = fields.get(2);
+	newtuple = new DataTuple(TweetsConstants.SCHEMA_TWEETS, newfields);
+	
+	return newtuple;
+	
+}
+	
+}
---
+++ textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/tweetsdictmatcher/test.java
@@ -0,0 +1,85 @@
+package edu.uci.ics.textdb.sandbox.tweetsdictmatcher;
+
+ 
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+
+import edu.uci.ics.textdb.api.common.IField;
+import edu.uci.ics.textdb.api.common.ITuple;
+import edu.uci.ics.textdb.api.common.Schema;
+import edu.uci.ics.textdb.api.storage.IDataStore;
+import edu.uci.ics.textdb.common.constants.DataConstants;
+import edu.uci.ics.textdb.common.constants.TestConstants;
+import edu.uci.ics.textdb.common.constants.DataConstants.SourceOperatorType;
+import edu.uci.ics.textdb.common.exception.StorageException;
+import edu.uci.ics.textdb.common.field.DataTuple;
+import edu.uci.ics.textdb.storage.DataStore;
+import edu.uci.ics.textdb.storage.writer.DataWriter;
+
+public class test {
+	public static void main(String[] args){
+ 
+		dictionaryMatcherTest("../resultIndex", DataConstants.SourceOperatorType.KEYWORDOPERATOR, 
+				    "amazing");
+ 
+	}
+	
+	public static void reusltWriterTest(){
+		try {
+		ResultedTweetsWriter.writeResultedTweetsToIndex(DataConstants.INDEX_DIR, "../resultIndex",
+				DataConstants.SourceOperatorType.KEYWORDOPERATOR, true, "zika" );
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	}
+	
+	public static void dictionaryMatcherTest(String path, SourceOperatorType op, String... query){
+		ITuple tuple = null;
+		int count = 0;
+		try {
+			TweetsDictMatcher.open(path, op, query);
+			 while ((tuple = TweetsDictMatcher.getNextTuple()) != null) {
+		    	 System.out.println(tuple);	
+		    	 count++;
+		    }
+		    TweetsDictMatcher.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(count);
+	}
+	 
+	
+	public static void writerTest() {
+		IDataStore dataStore = new DataStore(DataConstants.INDEX_DIR, TweetsConstants.SCHEMA_TWEETS) ;
+		Analyzer analyzer = new StandardAnalyzer();
+		try {
+			TweetsIndexWriter.writeTweetsToIndex("C:\\Users\\Hailey\\Desktop\\sampleTweets.json", dataStore, analyzer, true);
+		} catch (FileNotFoundException | StorageException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("-----Finished-------");
+	}
+	
+	public static void readerTest(){
+		try {
+			TweetsReader.open("C:\\Users\\Hailey\\Desktop\\sampleTweets.json");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		ITuple tuple = null;
+		while((tuple= TweetsReader.getNextTuple()) != null ){
+				 
+//			System.out.println(tuple.toString());
+		}	
+		TweetsReader.close();
+	}
+}
