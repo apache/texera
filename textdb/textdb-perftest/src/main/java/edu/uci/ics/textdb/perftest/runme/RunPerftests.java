6f3cab86a8837a22fda1d9afc199eacde8798178
Xuxip <haileyxxpan@gmail.com>
2016-08-12 14:03:56 -0700

    delete a line

--- textdb/textdb-perftest/src/main/java/edu/uci/ics/textdb/perftest/runme/RunPerftests.java
+++ textdb/textdb-perftest/src/main/java/edu/uci/ics/textdb/perftest/runme/RunPerftests.java
@@ -55,8 +55,7 @@ public class RunPerftests {
 			KeywordMatcherPerformanceTest.runTest("sample_queries.txt", 1);
 			DictionaryMatcherPerformanceTest.runTest("sample_queries.txt", 1);
 			FuzzyTokenMatcherPerformanceTest.runTest("sample_queries.txt", 1, thresholds);
-			RegexMatcherPerformanceTest.runTest(regexQueries, 2);
-	
+			 
 			
 			
 			
