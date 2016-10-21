a96fcc3d6e3ec794f8f9ad8d94c8764c9d7a2de1
zuozhiw <zuozhiw@uci.edu>
2016-10-21 10:26:15 -0700

    add index sink test

--- textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/sink/IndexSink.java
+++ textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/sink/IndexSink.java
@@ -1,10 +1,20 @@
 package edu.uci.ics.textdb.dataflow.sink;
 
-import org.apache.lucene.analysis.Analyzer;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+
+import edu.uci.ics.textdb.api.common.Attribute;
+import edu.uci.ics.textdb.api.common.FieldType;
+import edu.uci.ics.textdb.api.common.IField;
 import edu.uci.ics.textdb.api.common.ITuple;
 import edu.uci.ics.textdb.api.common.Schema;
+import edu.uci.ics.textdb.common.constants.DataConstants;
 import edu.uci.ics.textdb.common.exception.StorageException;
+import edu.uci.ics.textdb.common.field.DataTuple;
+import edu.uci.ics.textdb.common.field.ListField;
 import edu.uci.ics.textdb.storage.DataStore;
 import edu.uci.ics.textdb.storage.catalog.CatalogManager;
 import edu.uci.ics.textdb.storage.writer.DataWriter;
@@ -18,38 +28,49 @@ public class IndexSink extends AbstractSink {
 
     private String collectionName;
     private String indexDirectory;
-    private Schema collectionSchema;
+    
     private Analyzer luceneAnalyzer;
+    private String luceneAnalyzerStr;
     
     private DataWriter dataWriter;
+    private Schema outputSchema;
     
     private boolean isAppend = false;
     
 
-    public IndexSink(String collectionName, String indexDirectory, Schema schema, Analyzer luceneAnalyzer) {
+    public IndexSink(String collectionName, String indexDirectory, Analyzer luceneAnalyzer, boolean isAppend) {
         this.collectionName = collectionName;
         this.indexDirectory = indexDirectory;
-        this.collectionSchema = schema;
         this.luceneAnalyzer = luceneAnalyzer;
-        
-        DataStore dataStore = new DataStore(indexDirectory, schema);
-        this.dataWriter = new DataWriter(dataStore, luceneAnalyzer);
+        this.isAppend = isAppend;
     }
 
     public void open() throws Exception {
         super.open();
+        
+        this.outputSchema = processOutputSchema(this.getInputOperator().getOutputSchema());
+        
+        DataStore dataStore = new DataStore(indexDirectory, outputSchema);
+        this.dataWriter = new DataWriter(dataStore, luceneAnalyzer);
+        
         if (! isAppend) {
             this.dataWriter.clearData();
-            if (! CatalogManager.isCatalogManagerExist()) {
-                CatalogManager.createCatalog();
+            
+            if (luceneAnalyzer instanceof StandardAnalyzer) {
+                this.luceneAnalyzerStr = DataConstants.STANDARD_LUCENE_ANALYZER;
+            } else {
+                this.luceneAnalyzerStr = DataConstants.TRIGRAM_LUCENE_ANALYZER;
             }
-            CatalogManager.putCollectionSchema(collectionName, indexDirectory, collectionSchema, luceneAnalyzer.toString());
+            CatalogManager.putCollectionSchema(
+                    collectionName, indexDirectory, 
+                    outputSchema, luceneAnalyzerStr);
         }
+        
         this.dataWriter.open();
     }
 
     protected void processOneTuple(ITuple nextTuple) throws StorageException {
-        dataWriter.writeTuple(nextTuple);
+        dataWriter.writeTuple(processOutputTuple(nextTuple));
     }
 
     public void close() throws Exception {
@@ -57,6 +78,29 @@ public class IndexSink extends AbstractSink {
             this.dataWriter.close();
         }
         super.close();
+    }
+    
+    /*
+     * Return the schema to be written into the index.
+     * This function removes the attributes with FieldType LIST, 
+     * because currently DataWriter doesn't support LIST type.
+     */
+    private Schema processOutputSchema(Schema inputSchema) {
+        return new Schema(
+                inputSchema.getAttributes().stream()
+                .filter(attr -> attr.getFieldType() != FieldType.LIST)
+                .toArray(Attribute[]::new));
+    }
+    
+    /*
+     * Return the tuple to be written into the index.
+     * This function removes the fields with FieldType LIST, 
+     * because currently DataWriter doesn't support LIST type.
+     */
+    private ITuple processOutputTuple(ITuple inputTuple) {
+        List<IField> newFields = inputTuple.getFields().stream()
+                .filter(field -> !(field instanceof ListField)).collect(Collectors.toList());
+        return new DataTuple(outputSchema, newFields.stream().toArray(IField[]::new));
     }
 
 }
---
+++ textdb/textdb-dataflow/src/test/java/edu/uci/ics/textdb/dataflow/sink/IndexSinkTest.java
@@ -0,0 +1,21 @@
+package edu.uci.ics.textdb.dataflow.sink;
+
+import org.junit.Test;
+
+/**
+ * Test cases for IndexSink
+ * 
+ * @author Zuozhi Wang
+ *
+ */
+public class IndexSinkTest {
+    
+    
+    
+    
+    @Test
+    public void test1() throws Exception {
+        
+    }
+
+}
