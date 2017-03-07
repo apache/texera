373f84b6ea27e61441a4b20d62c389b12bbc3b5e
Henry <whchen1@uci.edu>
2017-03-06 16:27:36 -0800

    Delete button on the navigation bar now available

--- textdb/textdb-angular-gui/app/navigation-bar.component.ts
+++ textdb/textdb-angular-gui/app/navigation-bar.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+declare var jQuery: any;
 
 @Component({
     moduleId: module.id,
   selector: '[the-navbar]',
   templateUrl: './navigation-bar.component.html'
 })
-export class NavigationBarComponent {}
+export class NavigationBarComponent {
+
+	DeleteOp(data : any){
+    jQuery('#the-flowchart').flowchart('deleteSelected');
+	}
+}
