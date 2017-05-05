7ed33931e66421aeb3df011541d3b70e64d8b07b
Zijia Ke <kevinkozj@gmail.com>
2017-05-05 11:59:29 -0700

    Created a JUnit file for textdb-web/SystemResource
    Empty test right now. Need to add more test cases.

---
+++ textdb/textdb-web/src/test/java/edu/uci/ics/textdb/web/resource/SystemResourceTest.java
@@ -0,0 +1,26 @@
+package edu.uci.ics.textdb.web.resource;
+
+import edu.uci.ics.textdb.web.response.TextdbWebResponse;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by SmartGUI team on 5/5/17.
+ */
+public class SystemResourceTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getMetadata() throws Exception {
+    }
+
+}
