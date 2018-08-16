888c66d226ef74c447e4698ae8ab5dd45c00c5ee
BobbyFine <sirencbl@gmail.com>
2018-08-15 22:32:43 -0700

    First step

--- core/new-gui/src/app/workspace/component/product-tour/product-tour.component.ts
+++ core/new-gui/src/app/workspace/component/product-tour/product-tour.component.ts
@@ -8,7 +8,20 @@ import { TourService } from 'ngx-tour-ngx-bootstrap';
 })
 export class ProductTourComponent implements OnInit {
 
+  public loaded = false;
+
   constructor(public tourService: TourService) { 
+
+    this.tourService.anchorRegister$.subscribe((steps: string) => {
+      console.log('tour configured with these steps:', steps);
+    });
+
+    this.tourService.initialize([{
+      anchorId: 'texera-navigation-grid-container',
+      content: '<center><h3>Welcome to Texera!</h3></center><br><p>Texera is a system to support cloud-based text analytics using declarative and GUI-based workflows.</p><br><center><img src="../../../../assets/Tutor_Intro_Sample.png" height="400" width="800" alt="intro img"></center><br><br>',
+      placement: 'bottom',
+      title: 'Welcome'
+    }])
   }
 
   ngOnInit() {
