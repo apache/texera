059d463720f7a22468667eb0dfe3d61c1d013e97
Yinan Zhou <zhouyinan49@gmail.com>
2018-10-29 13:39:58 -0700

    implement the delay timer using observables

--- core/new-gui/src/app/workspace/component/operator-panel/operator-label/operator-label.component.ts
+++ core/new-gui/src/app/workspace/component/operator-panel/operator-label/operator-label.component.ts
@@ -1,6 +1,7 @@
 import { DragDropService } from './../../../service/drag-drop/drag-drop.service';
-import { Component, Input, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, Input, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { v4 as uuid } from 'uuid';
+import { Observable, fromEvent, interval} from 'rxjs';
 
 import { OperatorSchema } from '../../../types/operator-schema.interface';
 
@@ -16,10 +17,11 @@ import { OperatorSchema } from '../../../types/operator-schema.interface';
 })
 export class OperatorLabelComponent implements AfterViewInit {
 
-  @ViewChild('t') t: any;
+  @ViewChild('t') t: any; // t is the specific tooltip window for an operator
   @Input() operator?: OperatorSchema;
   public operatorLabelID: string;
   private timer: any; // needed to add a delay to tooltip
+  private isHovering: boolean;
 
   constructor(
     private dragDropService: DragDropService
@@ -27,6 +29,7 @@ export class OperatorLabelComponent implements AfterViewInit {
     // generate a random ID for this DOM element
     this.operatorLabelID = 'texera-operator-label-' + uuid();
 
+    this.isHovering = false;
   }
 
   ngAfterViewInit() {
@@ -34,18 +37,31 @@ export class OperatorLabelComponent implements AfterViewInit {
       throw new Error('operator label component: operator is not specified');
     }
     this.dragDropService.registerOperatorLabelDrag(this.operatorLabelID, this.operator.operatorType);
+
   }
 
   // show the tooltip window after 1500ms
   // var t means tooltip
-  displayDescription(t: any) {
-    // t.open();
-    this.timer = setTimeout(() => t.open() , 1500);
+  private displayDescription(t: any): void {
+    this.isHovering = true;
+    const secondsCounter = interval(1500);
+
+    // filter(isHovering) ensures that the cursor is still inside the label
+    // when secontsCounter emites an observable
+    this.timer = secondsCounter.filter(val => this.isHovering).subscribe(val => (
+      this.t.open(),
+      // unsubscribe the timer after the first observable is received to
+      // ensure that the function is not called only once.
+      this.timer.unsubscribe()
+    ));
   }
 
-  // hide the tooltip window and reset the timer
-  hideDescription(t: any) {
-    clearTimeout(this.timer);
-    t.close();
+  // hide the tooltip window
+  // reset the timer
+  // set isHovering to false to indicate that the cursor has left the operator label
+  private hideDescription(t: any): void {
+    this.isHovering = false;
+    this.t.close();
+    this.timer.unsubscribe();
   }
 }
