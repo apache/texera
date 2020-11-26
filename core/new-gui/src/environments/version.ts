16ebb65d9c6ec0dfc0d57e2eca9708f604f10b98
Avinash Kumar <avinash0161@yahoo.com>
2020-11-25 17:53:01 -0800

    adding functionality for having git hash in frontend

---
+++ core/new-gui/src/environments/version.ts
@@ -0,0 +1,14 @@
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+export const Version = {
+    "dirty": true,
+    "raw": "845969ddc-dirty",
+    "hash": "845969ddc",
+    "distance": null,
+    "tag": null,
+    "semver": null,
+    "suffix": "845969ddc-dirty",
+    "semverString": null,
+    "version": "0.0.0"
+};
+/* tslint:enable */
