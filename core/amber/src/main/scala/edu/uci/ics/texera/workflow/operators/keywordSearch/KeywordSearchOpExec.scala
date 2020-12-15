c963e469c87d64b066f5ae94ba29a28e99b152d1
Rohan Mannem <rmannem5@gmail.com>
2020-12-15 12:47:43 -0800

    final changes

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/keywordSearch/KeywordSearchOpExec.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/keywordSearch/KeywordSearchOpExec.scala
@@ -21,8 +21,6 @@ import org.apache.lucene.search.IndexSearcher
 import java.nio.file.Files
 
 class KeywordSearchOpExec(var counter: Int, val opDesc: KeywordSearchOpDesc) extends FilterOpExec {
-
-  @transient lazy val memoryIndex: MemoryIndex = new MemoryIndex();
   @transient lazy val mmapDir: MMapDirectory = new MMapDirectory(
     Files.createTempDirectory("texera-keyword-" + counter)
   )
@@ -37,9 +35,9 @@ class KeywordSearchOpExec(var counter: Int, val opDesc: KeywordSearchOpDesc) ext
 
   def findKeyword(tuple: Tuple): Boolean = {
     try {
+      @transient lazy val memoryIndex: MemoryIndex = new MemoryIndex();
       val tupleValue = tuple.getField(opDesc.columnName).toString
       memoryIndex.addField(opDesc.columnName, tupleValue, analyzer)
-//      val score = index.search(parser.parse(kw))
 
       if (memoryIndex.search(query) > 0.0f) {
         true
@@ -76,68 +74,4 @@ class KeywordSearchOpExec(var counter: Int, val opDesc: KeywordSearchOpDesc) ext
       false
     }
   }
-
-  //  def findKeywordMMap(tuple: Tuple): Boolean = {
-  //    def createIndex(): MMapDirectory = {
-  ////      val indexPath = "/Users/rohan/Downloads/tweetsIndex"
-  ////      val dataPath = "/Users/rohan/Downloads/subsetTweets.csv"
-  ////      val dataPath = "/Users/rohan/Downloads/tweet_1week.csv"
-  //
-  //      val tupleValue = tuple.getField(opDesc.columnName).toString
-  //
-  //      import java.io.File
-  //      val tempFile = File.createTempFile("tempFile", ".txt")
-  //      val dataPath = tempFile.getAbsolutePath
-  //
-  //      val bw = new BufferedWriter(new FileWriter(tempFile))
-  //      bw.write(tupleValue)
-  //      bw.close()
-  //
-  //      val path = Paths.get(dataPath)
-  //      val file = path.toFile
-  //
-  //      val indexWriterConfig = new IndexWriterConfig(analyzer);
-  //
-  //      val date1 = new Date
-  //      val time = new Timestamp(date1.getTime)
-  //      val indexPath = "/Users/rohan/Downloads/tweetsIndex/" + time.toString;
-  //
-  //      val indexDirectory = new MMapDirectory(Paths.get(indexPath));
-  //
-  //      val indexWriter = new IndexWriter(indexDirectory, indexWriterConfig);
-  //
-  //      val document = new Document();
-  //
-  //      val fileReader = new FileReader(file)
-  //
-  //      document.add(new TextField("TEXT", fileReader));
-  //
-  //      indexWriter.addDocument(document)
-  //      fileReader.close()
-  //      indexWriter.close
-  //      tempFile.deleteOnExit
-  //
-  //      indexDirectory
-  //    }
-  //
-  //    val indexDirectory = createIndex()
-  //
-  //    val query: Query = new QueryParser(opDesc.columnName, analyzer).parse(kw)
-  //
-  //    val indexReader = DirectoryReader.open(indexDirectory)
-  //
-  //    val searcher = new IndexSearcher(indexReader)
-  //
-  //    val topDocs = searcher.search(query, 1)
-  //
-  //    indexReader.close()
-  //
-  //
-  //    if (topDocs.totalHits.value > 0) {
-  //      true
-  //    }
-  //    else {
-  //      false
-  //    }
-  //  }
 }
