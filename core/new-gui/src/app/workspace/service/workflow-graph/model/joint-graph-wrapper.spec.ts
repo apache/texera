2e6a2b3ad2f65f7ce129b1abec2f4c7cc5dc423b
Zuozhi Wang <zuozhiw@gmail.com>
2020-12-20 19:20:14 -0800

    test if a failed test will actually fail CI

--- core/new-gui/src/app/workspace/service/workflow-graph/model/joint-graph-wrapper.spec.ts
+++ core/new-gui/src/app/workspace/service/workflow-graph/model/joint-graph-wrapper.spec.ts
@@ -768,6 +768,8 @@ describe('JointGraphWrapperService', () => {
           expect(localJointGraphWrapper.getLinkIDsWithBreakpoint()).toEqual([mockScanResultLink.linkID]);
         }
       });
+
+      expect(true).toEqual(false);
     }));
 
     it('should emit an hide breakpoint event correctly when removing a breakpoint', marbles((m) => {
