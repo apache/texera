21eb9fb7e77a568d61cdaf56d36c6ac3043e02a3
Yicong Huang <hyc541978023@gmail.com>
2021-01-29 10:45:19 -0800

    use capital sql keywords

--- core/scripts/sql/tweets.sql
+++ core/scripts/sql/tweets.sql
@@ -1,21 +1,21 @@
-create table TABLE_NAME
+CREATE TABLE TABLE_NAME
 (
     id                    BIGINT                       NOT NULL PRIMARY KEY,
     created_at            DATETIME                     NULL,
-    text                  VARCHAR(500) charset utf8mb4 NULL,
+    text                  VARCHAR(500) CHARSET utf8mb4 NULL,
     in_reply_to_status_id BIGINT                       NULL,
     in_reply_to_user_id   BIGINT                       NULL,
     favourites_count      INT                          NULL,
     retweet_count         INT                          NULL,
     lang                  VARCHAR(10)                  NULL,
     retweeted             BOOLEAN                      NULL,
-    hashtags              VARCHAR(500) charset utf8mb4 NULL,
-    user_mentions         VARCHAR(500) charset utf8mb4 NULL,
+    hashtags              VARCHAR(500) CHARSET utf8mb4 NULL,
+    user_mentions         VARCHAR(500) CHARSET utf8mb4 NULL,
     user_id               BIGINT                       NULL,
-    user_name             VARCHAR(500) charset utf8mb4 NULL,
-    user_screen_name      VARCHAR(500) charset utf8mb4 NULL,
+    user_name             VARCHAR(500) CHARSET utf8mb4 NULL,
+    user_screen_name      VARCHAR(500) CHARSET utf8mb4 NULL,
     user_location         VARCHAR(500)                 NULL,
-    user_description      VARCHAR(500) charset utf8mb4 NULL,
+    user_description      VARCHAR(500) CHARSET utf8mb4 NULL,
     user_followers_count  INT                          NULL,
     user_friends_count    INT                          NULL,
     user_statues_count    INT                          NULL,
@@ -26,5 +26,5 @@ create table TABLE_NAME
     bounding_box          VARCHAR(500)                 NULL
 );
 
-create fulltext index text on TABLE_NAME (text);
+CREATE FULLTEXT INDEX text_index on TABLE_NAME (text);
 CREATE INDEX create_at_index ON TABLE_NAME (created_at);
