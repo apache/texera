5ba7d3d073824e6a009ccb175fb08f6ec3948d30
Tongjie <tjwang0221@gmail.com>
2021-01-31 23:23:12 -0800

    Progressive Result Display
    The server will send status update (with newest 10 rows of output) to the client.
    
    The client will update the result panel once per second based on the received status update.

--- core/amber/src/main/scala/edu/uci/ics/texera/web/model/event/WorkflowStatusUpdateEvent.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/model/event/WorkflowStatusUpdateEvent.scala
@@ -9,6 +9,7 @@ object WebOperatorStatistics {
   def apply(
       operatorID: String,
       operatorStatistics: OperatorStatistics,
+      dirtyPageIndices: Map[String, List[Int]],
       workflowCompiler: WorkflowCompiler
   ): WebOperatorStatistics = {
     val chartType = OperatorResult.getChartType(operatorID, workflowCompiler)
@@ -16,13 +17,19 @@ object WebOperatorStatistics {
       // if chartType is null (normal sink), don't send results as well
       .flatMap(r => chartType.map(_ => r))
       // convert tuple format
-      .map(r => OperatorResult.fromTuple(operatorID, r, chartType, r.size))
+      .map(r =>
+        chartType match {
+          case Some(_) => OperatorResult.fromTuple(operatorID, r, chartType, r.size)
+          case None    => OperatorResult.fromTuple(operatorID, List.empty, chartType, r.size)
+        }
+      )
 
     WebOperatorStatistics(
       operatorStatistics.operatorState,
       operatorStatistics.aggregatedInputRowCount,
       operatorStatistics.aggregatedOutputRowCount,
-      results
+      results,
+      dirtyPageIndices.get(operatorID)
     )
   }
 
@@ -32,17 +39,19 @@ case class WebOperatorStatistics(
     operatorState: OperatorState,
     aggregatedInputRowCount: Long,
     aggregatedOutputRowCount: Long,
-    aggregatedOutputResults: Option[OperatorResult] // in case of a sink operator
+    aggregatedOutputResults: Option[OperatorResult], // in case of a sink operator
+    aggregatedOutputResultDirtyPageIndices: Option[List[Int]]
 )
 
 object WebWorkflowStatusUpdateEvent {
   def apply(
       update: WorkflowStatusUpdate,
+      sinkOpDirtyPageIndices: Map[String, List[Int]],
       workflowCompiler: WorkflowCompiler
   ): WebWorkflowStatusUpdateEvent = {
     WebWorkflowStatusUpdateEvent(
       update.operatorStatistics.map(e =>
-        (e._1, WebOperatorStatistics.apply(e._1, e._2, workflowCompiler))
+        (e._1, WebOperatorStatistics.apply(e._1, e._2, sinkOpDirtyPageIndices, workflowCompiler))
       )
     )
   }
