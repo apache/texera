444d988a0ce9c1bbedde6a1f8974fcd64bf55890
Yicong Huang <hyc541978023@gmail.com>
2021-02-17 00:58:47 -0800

    reformat

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/visualization/wordCloud/WordCloudOpFinalExec.java
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/visualization/wordCloud/WordCloudOpFinalExec.java
@@ -8,7 +8,6 @@ import edu.uci.ics.texera.workflow.common.tuple.schema.Attribute;
 import edu.uci.ics.texera.workflow.common.tuple.schema.AttributeType;
 import edu.uci.ics.texera.workflow.common.tuple.schema.Schema;
 import org.apache.curator.shaded.com.google.common.collect.Iterators;
-import scala.Int;
 import scala.collection.Iterator;
 import scala.collection.JavaConverters;
 import scala.util.Either;
@@ -19,18 +18,18 @@ import java.util.stream.Collectors;
 /**
  * Merge word count maps into a single map (termFreqMap), calculate the size of each token based on its count, and
  * output as tuples of (word, count).
- * @author Mingji Han, Xiaozhen Liu
  *
+ * @author Mingji Han, Xiaozhen Liu
  */
 public class WordCloudOpFinalExec implements OperatorExecutor {
 
     private HashMap<String, Integer> termFreqMap;
+    private final int topN;
     private static final Schema resultSchema = Schema.newBuilder().add(
             new Attribute("word", AttributeType.STRING),
             new Attribute("count", AttributeType.INTEGER)
     ).build();
 
-    private int topN;
 
     public WordCloudOpFinalExec(int topN) {
         this.topN = topN;
@@ -53,26 +52,25 @@ public class WordCloudOpFinalExec implements OperatorExecutor {
 
     @Override
     public Iterator<Tuple> processTexeraTuple(Either<Tuple, InputExhausted> tuple, LinkIdentity input) {
-       if(tuple.isLeft()) {
-           String term = tuple.left().get().getString(0);
-           int frequency = tuple.left().get().getInt(1);
-           termFreqMap.put(term, termFreqMap.get(term)==null ? frequency : termFreqMap.get(term) + frequency);
-           return JavaConverters.asScalaIterator(Iterators.emptyIterator());
-       }
-       else {
+        if (tuple.isLeft()) {
+            String term = tuple.left().get().getString(0);
+            int frequency = tuple.left().get().getInt(1);
+            termFreqMap.put(term, termFreqMap.get(term) == null ? frequency : termFreqMap.get(term) + frequency);
+            return JavaConverters.asScalaIterator(Iterators.emptyIterator());
+        } else {
 
-           List<Map.Entry<String, Integer>> topNWordFreqs = termFreqMap.entrySet().stream()
-                   .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                   .limit(topN).collect(Collectors.toList());
+            List<Map.Entry<String, Integer>> topNWordFreqs = termFreqMap.entrySet().stream()
+                    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                    .limit(topN).collect(Collectors.toList());
 
-           List<Tuple> termFreqTuples = new ArrayList<>();
-           for (Map.Entry<String, Integer> e : topNWordFreqs) {
-               termFreqTuples.add(Tuple.newBuilder().add(
-                       resultSchema,
-                       Arrays.asList(e.getKey(), e.getValue())
-               ).build());
-           }
-           return JavaConverters.asScalaIterator(termFreqTuples.iterator());
-       }
+            List<Tuple> termFreqTuples = new ArrayList<>();
+            for (Map.Entry<String, Integer> e : topNWordFreqs) {
+                termFreqTuples.add(Tuple.newBuilder().add(
+                        resultSchema,
+                        Arrays.asList(e.getKey(), e.getValue())
+                ).build());
+            }
+            return JavaConverters.asScalaIterator(termFreqTuples.iterator());
+        }
     }
 }
