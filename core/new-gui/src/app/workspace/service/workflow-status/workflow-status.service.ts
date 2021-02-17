e8d03901e70616e9fa3ed1bff3eb42e3fc0ed47e
Zuozhi Wang <zuozhiw@gmail.com>
2021-02-16 19:55:52 -0800

    fix

--- core/new-gui/src/app/workspace/service/workflow-status/workflow-status.service.ts
+++ core/new-gui/src/app/workspace/service/workflow-status/workflow-status.service.ts
@@ -1,7 +1,7 @@
 import { environment } from './../../../../environments/environment';
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
-import { OperatorStatistics, ExecutionState, OperatorState } from '../../types/execute-workflow.interface';
+import { OperatorStatistics, ExecutionState, OperatorState, ResultObject } from '../../types/execute-workflow.interface';
 import { WorkflowWebsocketService } from '../workflow-websocket/workflow-websocket.service';
 import { ExecuteWorkflowService } from '../execute-workflow/execute-workflow.service';
 import { WorkflowActionService } from '../workflow-graph/model/workflow-action.service';
@@ -12,7 +12,11 @@ import { WorkflowActionService } from '../workflow-graph/model/workflow-action.s
 })
 export class WorkflowStatusService {
   // status is responsible for passing websocket responses to other components
-  private status = new Subject<Record<string, OperatorStatistics>>();
+  private statusSubject  = new Subject<Record<string, OperatorStatistics>>();
+  private currentStatus: Record<string, OperatorStatistics> = {};
+
+  private resultSubject = new Subject<Record<string, ResultObject>>();
+  private currentResult: Record<string, ResultObject> = {};
 
   constructor(
     private workflowActionService: WorkflowActionService,
@@ -22,23 +26,26 @@ export class WorkflowStatusService {
     if (!environment.executionStatusEnabled) {
       return;
     }
+    this.getStatusUpdateStream().subscribe(event => this.currentStatus = event);
+    this.getResultUpdateStream().subscribe(event => this.currentResult = event);
+
     this.workflowWebsocketService.websocketEvent().subscribe(event => {
       if (event.type !== 'WorkflowStatusUpdateEvent') {
         return;
       }
-      this.status.next(event.operatorStatistics);
+      this.statusSubject.next(event.operatorStatistics);
     });
-    this.executeWorkflowService.getExecutionStateStream().subscribe(event => {
-      if (event.current.state === ExecutionState.WaitingToRun) {
-        const initialStatistics: Record<string, OperatorStatistics> = {};
-        this.workflowActionService.getTexeraGraph().getAllOperators().forEach(op => {
-          initialStatistics[op.operatorID] = {
-            operatorState: OperatorState.Initializing,
-            aggregatedInputRowCount: 0,
-            aggregatedOutputRowCount: 0
-          };
-        });
-        this.status.next(initialStatistics);
+
+    this.workflowWebsocketService.websocketEvent().subscribe(event => {
+      if (event.type !== 'WorkflowCompletedEvent') {
+        return;
+      }
+      const results: Record<string, ResultObject> = {};
+      event.result.forEach(r => {
+        results[r.operatorID] = r;
+      });
+      if (Object.keys(results).length !== 0) {
+        this.resultSubject.next(results);
       }
     });
 
@@ -58,7 +65,19 @@ export class WorkflowStatusService {
   }
 
   public getStatusUpdateStream(): Observable<Record<string, OperatorStatistics>> {
-    return this.status;
+    return this.statusSubject.asObservable();
+  }
+
+  public getCurrentStatus(): Record<string, OperatorStatistics> {
+    return this.currentStatus;
+  }
+
+  public getResultUpdateStream(): Observable<Record<string, ResultObject>> {
+    return this.resultSubject.asObservable();
+  }
+
+  public getCurrentResult(): Record<string, ResultObject> {
+    return this.currentResult;
   }
 
 }
