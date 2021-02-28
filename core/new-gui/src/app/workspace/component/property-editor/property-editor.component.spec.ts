adabf9d72105af4e6456f14a48d9cefd96232296
Yicong Huang <hyc541978023@gmail.com>
2021-02-27 18:35:22 -0800

    fix tests

--- core/new-gui/src/app/workspace/component/property-editor/property-editor.component.spec.ts
+++ core/new-gui/src/app/workspace/component/property-editor/property-editor.component.spec.ts
@@ -1,28 +1,29 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule, By } from '@angular/platform-browser';
-import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
-
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { PropertyEditorComponent } from './property-editor.component';
-
-import { WorkflowActionService } from './../../service/workflow-graph/model/workflow-action.service';
-import { UndoRedoService } from './../../service/undo-redo/undo-redo.service';
-import { OperatorMetadataService } from './../../service/operator-metadata/operator-metadata.service';
-import { StubOperatorMetadataService } from './../../service/operator-metadata/stub-operator-metadata.service';
-import { JointUIService } from './../../service/joint-ui/joint-ui.service';
-
-import { mockScanSourceSchema, mockViewResultsSchema,
-         mockBreakpointSchema } from './../../service/operator-metadata/mock-operator-metadata.data';
-
+import { WorkflowActionService } from '../../service/workflow-graph/model/workflow-action.service';
+import { UndoRedoService } from '../../service/undo-redo/undo-redo.service';
+import { OperatorMetadataService } from '../../service/operator-metadata/operator-metadata.service';
+import { StubOperatorMetadataService } from '../../service/operator-metadata/stub-operator-metadata.service';
+import { JointUIService } from '../../service/joint-ui/joint-ui.service';
+import {
+  mockBreakpointSchema,
+  mockScanSourceSchema,
+  mockViewResultsSchema
+} from '../../service/operator-metadata/mock-operator-metadata.data';
 import { configure } from 'rxjs-marbles';
-const { marbles } = configure({ run: false });
-
-import { mockResultPredicate, mockScanPredicate, mockPoint,
-         mockScanResultLink, mockScanSentimentLink, mockSentimentResultLink,
-         mockSentimentPredicate} from '../../service/workflow-graph/model/mock-workflow-data';
-import { CustomNgMaterialModule } from '../../../common/custom-ng-material.module';
+import {
+  mockPoint,
+  mockResultPredicate,
+  mockScanPredicate,
+  mockScanResultLink,
+  mockScanSentimentLink,
+  mockSentimentPredicate,
+  mockSentimentResultLink
+} from '../../service/workflow-graph/model/mock-workflow-data';
 import { DynamicSchemaService } from '../../service/dynamic-schema/dynamic-schema.service';
-import { environment } from './../../../../environments/environment';
-
+import { environment } from '../../../../environments/environment';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormlyModule } from '@ngx-formly/core';
 import { TEXERA_FORMLY_CONFIG } from 'src/app/common/formly/formly-config';
@@ -30,23 +31,20 @@ import { FormlyMaterialModule } from '@ngx-formly/material';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ExecuteWorkflowService } from '../../service/execute-workflow/execute-workflow.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { FormlyNgZorroAntdModule } from '@ngx-formly/ng-zorro-antd';
-import { AppRoutingModule } from 'src/app/app-routing.module';
-import { NgxAceModule } from 'ngx-ace-icy';
-import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { FormlyJsonschema } from '@ngx-formly/core/json-schema';
 import { ArrayTypeComponent } from 'src/app/common/formly/array.type';
 import { ObjectTypeComponent } from 'src/app/common/formly/object.type';
 import { MultiSchemaTypeComponent } from 'src/app/common/formly/multischema.type';
 import { NullTypeComponent } from 'src/app/common/formly/null.type';
 import { JSONSchema7 } from 'json-schema';
-
 import * as Ajv from 'ajv';
 import { cloneDeep } from 'lodash';
 import { assertType } from 'src/app/common/util/assert';
 import { WorkflowUtilService } from '../../service/workflow-graph/util/workflow-util.service';
+import { SchemaPropagationService } from '../../service/dynamic-schema/schema-propagation/schema-propagation.service';
+
+const {marbles} = configure({run: false});
 
 /* tslint:disable:no-non-null-assertion */
 
@@ -55,6 +53,7 @@ describe('PropertyEditorComponent', () => {
   let fixture: ComponentFixture<PropertyEditorComponent>;
   let workflowActionService: WorkflowActionService;
   let dynamicSchemaService: DynamicSchemaService;
+  let schemaPropagationService: SchemaPropagationService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -63,14 +62,14 @@ describe('PropertyEditorComponent', () => {
         ArrayTypeComponent,
         ObjectTypeComponent,
         MultiSchemaTypeComponent,
-        NullTypeComponent,
+        NullTypeComponent
       ],
       providers: [
         JointUIService,
         WorkflowActionService,
         WorkflowUtilService,
         UndoRedoService,
-        { provide: OperatorMetadataService, useClass: StubOperatorMetadataService },
+        {provide: OperatorMetadataService, useClass: StubOperatorMetadataService},
         DynamicSchemaService,
         ExecuteWorkflowService,
         FormlyJsonschema
@@ -88,7 +87,7 @@ describe('PropertyEditorComponent', () => {
         // use formly material module instead
         FormlyMaterialModule,
         ReactiveFormsModule,
-        HttpClientTestingModule,
+        HttpClientTestingModule
       ]
     })
       .compileComponents();
@@ -99,6 +98,7 @@ describe('PropertyEditorComponent', () => {
     component = fixture.componentInstance;
     workflowActionService = TestBed.inject(WorkflowActionService);
     dynamicSchemaService = TestBed.inject(DynamicSchemaService);
+    schemaPropagationService = TestBed.inject(SchemaPropagationService);
     fixture.detectChanges();
 
   });
@@ -150,7 +150,6 @@ describe('PropertyEditorComponent', () => {
 
   });
 
-
   it('should switch the content of property editor to another operator from the former operator correctly', () => {
     const jointGraphWrapper = workflowActionService.getJointGraphWrapper();
 
@@ -173,14 +172,13 @@ describe('PropertyEditorComponent', () => {
 
     // result operator has default values, use ajv to fill in default values
     // expected form output should fill in all default values instead of an empty object
-    const ajv = new Ajv({ useDefaults: true });
+    const ajv = new Ajv({useDefaults: true});
     const expectedResultOperatorProperties = cloneDeep(mockResultPredicate.operatorProperties);
     ajv.validate(mockViewResultsSchema.jsonSchema, expectedResultOperatorProperties);
 
     expect(component.currentOperatorID).toEqual(mockResultPredicate.operatorID);
     expect(component.formData).toEqual(expectedResultOperatorProperties);
     expect(component.displayForm).toBeTruthy();
-
 
     // check HTML form are displayed
     const formTitleElementAfterChange = fixture.debugElement.query(By.css('.texera-workspace-property-editor-title'));
@@ -201,7 +199,6 @@ describe('PropertyEditorComponent', () => {
         expect((jsonSchemaFormElementAfterChange.nativeElement as HTMLElement).innerHTML).toContain(propertyDescription);
       }
     });
-
 
   });
 
@@ -311,7 +308,7 @@ describe('PropertyEditorComponent', () => {
     jointGraphWrapper.highlightOperators(mockScanPredicate.operatorID);
 
     // stimulate a form change by the user
-    const formChangeValue = { tableName: 'twitter_sample' };
+    const formChangeValue = {tableName: 'twitter_sample'};
     component.onFormChanges(formChangeValue);
 
     // maintain a counter of how many times the event is emitted
@@ -343,11 +340,11 @@ describe('PropertyEditorComponent', () => {
     // simulate user types in `table` character by character
     const formUserInputMarbleString = '-a-b-c-d-e';
     const formUserInputMarbleValue = {
-      a: { tableName: 't' },
-      b: { tableName: 'ta' },
-      c: { tableName: 'tab' },
-      d: { tableName: 'tabl' },
-      e: { tableName: 'table' },
+      a: {tableName: 't'},
+      b: {tableName: 'ta'},
+      c: {tableName: 'tab'},
+      d: {tableName: 'tabl'},
+      e: {tableName: 'table'}
     };
     const formUserInputEventStream = m.hot(formUserInputMarbleString, formUserInputMarbleValue);
 
@@ -359,10 +356,9 @@ describe('PropertyEditorComponent', () => {
       '-'.repeat(PropertyEditorComponent.formInputDebounceTime / 10) +
       'e-';
     const formChangeEventMarbleValue = {
-      e: { tableName: 'table' } as object
+      e: {tableName: 'table'} as object
     };
     const expectedFormChangeEventStream = m.hot(formChangeEventMarbleStrig, formChangeEventMarbleValue);
-
 
     m.bind();
 
@@ -380,11 +376,10 @@ describe('PropertyEditorComponent', () => {
     // add an operator and highlight the operator so that the
     //  variables in property editor component is set correctly
     workflowActionService.addOperator(mockScanPredicate, mockPoint);
-    const mockOperatorProperty = { tableName: 'table' };
+    const mockOperatorProperty = {tableName: 'table'};
     // set operator property first before displaying the operator property in property panel
     workflowActionService.setOperatorProperty(mockScanPredicate.operatorID, mockOperatorProperty);
     jointGraphWrapper.highlightOperators(mockScanPredicate.operatorID);
-
 
     // stimulate a form change with the same property
     component.onFormChanges(mockOperatorProperty);
@@ -430,7 +425,7 @@ describe('PropertyEditorComponent', () => {
       const jsonSchemaFormElement = fixture.debugElement.query(By.css('.texera-workspace-property-editor-form'));
       // check if the form has the all the json schema property names
       Object.values((mockBreakpointSchema.jsonSchema.oneOf as any)[0].properties).forEach((property: unknown) => {
-        assertType<{type: string, title: string}>(property);
+        assertType<{ type: string, title: string }>(property);
         expect((jsonSchemaFormElement.nativeElement as HTMLElement).innerHTML).toContain(property.title);
       });
     });
@@ -462,7 +457,7 @@ describe('PropertyEditorComponent', () => {
 
       // check if the form has the all the json schema property names
       Object.values((mockBreakpointSchema.jsonSchema.oneOf as any)[0].properties).forEach((property: unknown) => {
-        assertType<{type: string, title: string}>(property);
+        assertType<{ type: string, title: string }>(property);
         expect((jsonSchemaFormElement.nativeElement as HTMLElement).innerHTML).toContain(property.title);
       });
     });
@@ -550,7 +545,6 @@ describe('PropertyEditorComponent', () => {
       buttonState.triggerEventHandler('click', null);
       expect(workflowActionService.setLinkBreakpoint).toHaveBeenCalledTimes(1);
 
-
     });
 
     it('should clear and hide the property editor panel correctly on clicking the remove button on breakpoint editor', () => {
@@ -630,7 +624,7 @@ describe('PropertyEditorComponent', () => {
       jointGraphWrapper.highlightLink(mockScanResultLink.linkID);
       fixture.detectChanges();
 
-      const formData = { count: 100 };
+      const formData = {count: 100};
       // simulate adding a breakpoint
       component.formData = formData;
       component.handleAddBreakpoint();
@@ -689,7 +683,6 @@ describe('PropertyEditorComponent', () => {
     //   };
     //   const expectedFormChangeEventStream = m.hot(formChangeEventMarbleStrig, formChangeEventMarbleValue);
 
-
     //   m.bind();
 
     //   const actualFormChangeEventStream = component.createoutputBreakpointChangeEventStream(formUserInputEventStream);
@@ -700,7 +693,6 @@ describe('PropertyEditorComponent', () => {
 
     // xit('should not emit breakpoint property change event if the new property is the same as the old property', fakeAsync(() => {
     //   const jointGraphWrapper = workflowActionService.getJointGraphWrapper();
-
 
     //   // add a link and highligh the link so that the
     //   //  variables in property editor component is set correctly
@@ -726,7 +718,6 @@ describe('PropertyEditorComponent', () => {
     //   // because the form change value is the same
     //   expect(emitEventCounter).toEqual(0);
     // }));
-
 
   });
 
