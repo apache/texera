a2b6a4865678391f4bb4745bcf119f0130f86341
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-03-15 12:42:23 -0700

    extract some methods

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/pythonUDF/PythonUDFOpExec.java
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/pythonUDF/PythonUDFOpExec.java
@@ -15,11 +15,13 @@ import org.apache.arrow.vector.*;
 import org.apache.arrow.vector.types.FloatingPointPrecision;
 import org.apache.arrow.vector.types.pojo.ArrowType;
 import org.apache.arrow.vector.types.pojo.Field;
+import org.jetbrains.annotations.NotNull;
 import scala.collection.Iterator;
 import scala.collection.JavaConverters;
 import scala.util.Either;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -54,6 +56,11 @@ public class PythonUDFOpExec implements OperatorExecutor {
 
     private Queue<Tuple> inputTupleBuffer;
     private Queue<Tuple> outputTupleBuffer;
+
+    @NotNull
+    private static byte[] communicate(FlightClient client, String message) {
+        return client.doAction(new Action(message)).next().getBody();
+    }
 
     PythonUDFOpExec(String pythonScriptText, String pythonScriptFile, ArrayList<String> inputColumns,
                     ArrayList<Attribute> outputColumns, ArrayList<String> arguments, ArrayList<String> outerFiles, int batchSize) {
@@ -243,67 +250,105 @@ public class PythonUDFOpExec implements OperatorExecutor {
         closeClientAndServer(flightClient, true);
     }
 
-    private void processOneBatch() {
+    /**
+     * For every batch, the operator converts list of {@code Tuple}s into Arrow stream data in almost the exact same
+     * way as it would when using Arrow file, except now it sends stream to the server with
+     * {@link FlightClient#startPut(FlightDescriptor, VectorSchemaRoot, FlightClient.PutListener, CallOption...)} and
+     * {@link FlightClient.ClientStreamListener#putNext()}. The server uses {@code do_put()} to receive data stream
+     * and convert it into a {@code pyarrow.Table} and store it in the server.
+     * {@code startPut} is a non-blocking call, but this method in general is a blocking call, it waits until all the
+     * data are sent.
+     *
+     * @param client         The FlightClient that manages this.
+     * @param values         The input queue that holds tuples, its contents will be consumed in this method.
+     * @param root           Root allocator that manages memory issues in Arrow.
+     * @param arrowSchema    Input Arrow table schema. This should already have been defined (converted).
+     * @param descriptorPath The predefined path that specifies where to store the data in Flight Serve.
+     * @param chunkSize      The chunk size of the arrow stream. This is different than the batch size of the operator,
+     *                       although they may seem similar. This doesn't actually affect serialization speed that much,
+     *                       so in general it can be the same as {@code batchSize}.
+     */
+    private static void writeArrowStream(FlightClient client, Queue<Tuple> values, RootAllocator root,
+                                         org.apache.arrow.vector.types.pojo.Schema arrowSchema,
+                                         String descriptorPath, int chunkSize) throws RuntimeException {
+        SyncPutListener flightListener = new SyncPutListener();
+        VectorSchemaRoot schemaRoot = VectorSchemaRoot.create(arrowSchema, root);
+        FlightClient.ClientStreamListener streamWriter = client.startPut(
+                FlightDescriptor.path(Collections.singletonList(descriptorPath)), schemaRoot, flightListener);
+        try {
+            while (!values.isEmpty()) {
+                schemaRoot.allocateNew();
+                int indexThisChunk = 0;
+                while (indexThisChunk < chunkSize && !values.isEmpty()) {
+                    convertAmber2ArrowTuple(values.remove(), indexThisChunk, schemaRoot);
+                    indexThisChunk++;
+                }
+                schemaRoot.setRowCount(indexThisChunk);
+                streamWriter.putNext();
+                schemaRoot.clear();
+            }
+            streamWriter.completed();
+            flightListener.getResult();
+            flightListener.close();
+            schemaRoot.clear();
+        } catch (Exception e) {
+            closeAndThrow(client, e);
+        }
+    }
+
+    /**
+     * Make the execution of the UDF in Python and read the results that are passed back. This should only be called
+     * after input data is passed to Python. This is a blocking call.
+     *
+     * @param client      The FlightClient that manages this.
+     * @param resultQueue To store the results. Must be empty when it is passed here.
+     */
+    private static void executeUDF(FlightClient client, Queue<Tuple> resultQueue) {
+        try {
+            FlightResponseMap result = PythonUDFOpExec.globalObjectMapper.readValue(communicate(client, "compute"), FlightResponseMap.class);
+            if (result.get("status").equals("Fail")) {
+                String errorMessage = result.get("errorMessage");
+                throw new Exception(errorMessage);
+            }
+            readArrowStream(client, "fromPython", resultQueue);
+        } catch (Exception e) {
+            closeAndThrow(client, e);
+        }
+    }
+
+    /**
+     * Close everything and throw an exception. This should only be called when an exception occurs and needs to be
+     * thrown, but the Arrow Flight Client is still running.
+     *
+     * @param client FlightClient.
+     * @param e      the exception to be wrapped into Amber Exception.
+     */
+    private static void closeAndThrow(FlightClient client, Exception e) throws RuntimeException {
+        closeClientAndServer(client, false);
+        throw new RuntimeException(e.getMessage());
+    }
+
+    private static void deleteTempFile(String filePath) throws IOException {
+        File tempFile = new File(filePath);
+        if (tempFile.exists()) {
+            if (!tempFile.delete()) throw new IOException("Could not delete temp file");
+        }
+    }
+
+    private void processOneBatch() throws RuntimeException {
         writeArrowStream(flightClient, inputTupleBuffer, globalRootAllocator, globalInputSchema, "toPython", batchSize);
-        executeUDF(flightClient, globalObjectMapper, outputTupleBuffer);
+        executeUDF(flightClient, outputTupleBuffer);
     }
 
     @Override
     public void open() {
+
         try {
-            pythonScriptPath = isDynamic ?
-                    getPythonResourcePath(String.valueOf(new Random().nextLong())) + ".py"
-                    : getPythonResourcePath(pythonScriptPath);
-            if (isDynamic) {
-                // dynamic -> create a temp file and write the code into the file
-                File tempScriptFile = new File(pythonScriptPath);
-
-                if (tempScriptFile.createNewFile()) {
-                    FileWriter fileWriter = new FileWriter(pythonScriptPath);
-                    fileWriter.write(pythonScriptText);
-                    fileWriter.close();
-                }
-            } else {
-                // static -> check if the script file exists
-                File scriptFile = new File(pythonScriptPath);
-                if (!scriptFile.exists()) throw new Exception("Script file doest not exist!");
-            }
-
-            int portNumber = getFreeLocalPort();
-            Location location = new Location(URI.create("grpc+tcp://localhost:" + portNumber));
-            System.out.println(PYTHON);
-            List<String> args = new ArrayList<>(
-                    Arrays.asList(PYTHON, DAEMON_SCRIPT_PATH, Integer.toString(portNumber), pythonScriptPath)
-            );
-
-            ProcessBuilder processBuilder = new ProcessBuilder(args).inheritIO();
-            // Start Flight server (Python process)
-            processBuilder.start();
-            // Connect to server
-            boolean connected = false;
-            int tryCount = 0;
-            while (!connected && tryCount < MAX_TRY_COUNT) {
-                try {
-                    Thread.sleep(WAIT_TIME_MS);
-                    flightClient = FlightClient.builder(globalRootAllocator, location).build();
-                    String message = new String(
-                            flightClient.doAction(new Action("healthcheck")).next().getBody(), StandardCharsets.UTF_8);
-                    connected = message.equals("Flight Server is up and running!");
-                } catch (Exception e) {
-                    System.out.println("Flight Client:\tNot connected to the server in this try.");
-                    flightClient.close();
-                    tryCount++;
-                }
-            }
-            if (tryCount == MAX_TRY_COUNT)
-                throw new RuntimeException("Exceeded try limit of " + MAX_TRY_COUNT + " when connecting to Flight Server!");
-        } catch (Exception e) {
-            e.printStackTrace();
-            try {
-                if (isDynamic) deleteTempFile(pythonScriptPath);
-            } catch (Exception innerException) {
-                innerException.printStackTrace();
-            }
+            preparePythonScriptFile();
+            Location flightServerURL = startFlightServer();
+            connectToServer(flightServerURL);
+        } catch (IOException | InterruptedException e) {
+            cleanTerminationWithThrow(e);
         }
 
         // Send user args to Server.
@@ -323,21 +368,54 @@ public class PythonUDFOpExec implements OperatorExecutor {
 
         try {
             writeArrowStream(flightClient, argsTuples, globalRootAllocator, convertAmber2ArrowSchema(argsSchema), "args", batchSize);
-            flightClient.doAction(new Action("open")).next().getBody();
-        } catch (Exception e) {
-            e.printStackTrace();
-            try {
-                if (isDynamic) deleteTempFile(pythonScriptPath);
-            } catch (Exception innerException) {
-                innerException.printStackTrace();
-            }
-            closeAndThrow(flightClient, e);
+            communicate(flightClient, "open");
+        } catch (RuntimeException e) {
+            cleanTerminationWithThrow(e);
         }
+
         // Finally, delete the temp file because it has been loaded in Python.
+        if (isDynamic) safeDeleteTempFile(pythonScriptPath);
+
+    }
+
+    private void safeDeleteTempFile(String fileName) {
         try {
-            if (isDynamic) deleteTempFile(pythonScriptPath);
-        } catch (Exception e) {
-            closeAndThrow(flightClient, e);
+            deleteTempFile(fileName);
+        } catch (Exception innerException) {
+            innerException.printStackTrace();
+        }
+    }
+
+    private void connectToServer(Location flightServerURI) throws InterruptedException {
+        boolean connected = false;
+        int tryCount = 0;
+        while (!connected && tryCount < MAX_TRY_COUNT) {
+
+            try {
+                flightClient = FlightClient.builder(globalRootAllocator, flightServerURI).build();
+                connected = new String(communicate(flightClient, "healthcheck"), StandardCharsets.UTF_8).equals("Flight Server is up and running!");
+                if (!connected) Thread.sleep(WAIT_TIME_MS);
+            } catch (FlightRuntimeException e) {
+                System.out.println("Flight Client:\tNot connected to the server in this try.");
+                flightClient.close();
+                tryCount++;
+            }
+        }
+        if (tryCount == MAX_TRY_COUNT)
+            throw new RuntimeException("Exceeded try limit of " + MAX_TRY_COUNT + " when connecting to Flight Server!");
+    }
+
+    private void preparePythonScriptFile() throws IOException {
+        pythonScriptPath = isDynamic ?
+                getPythonResourcePath(String.valueOf(new Random().nextLong())) + ".py"
+                : getPythonResourcePath(pythonScriptPath);
+        if (isDynamic) {
+            // dynamic -> create a temp file and write the code into the file
+            writeTempPythonFile();
+        } else {
+            // static -> check if the script file exists
+            File scriptFile = new File(pythonScriptPath);
+            if (!scriptFile.exists()) throw new FileNotFoundException("Script file doest not exist!");
         }
     }
 
@@ -406,50 +484,15 @@ public class PythonUDFOpExec implements OperatorExecutor {
         return JavaConverters.asScalaIterator(Collections.emptyIterator());
     }
 
-    /**
-     * For every batch, the operator converts list of {@code Tuple}s into Arrow stream data in almost the exact same
-     * way as it would when using Arrow file, except now it sends stream to the server with
-     * {@link FlightClient#startPut(FlightDescriptor, VectorSchemaRoot, FlightClient.PutListener, CallOption...)} and
-     * {@link FlightClient.ClientStreamListener#putNext()}. The server uses {@code do_put()} to receive data stream
-     * and convert it into a {@code pyarrow.Table} and store it in the server.
-     * {@code startPut} is a non-blocking call, but this method in general is a blocking call, it waits until all the
-     * data are sent.
-     *
-     * @param client         The FlightClient that manages this.
-     * @param values         The input queue that holds tuples.
-     * @param root           Root allocator that manages memory issues in Arrow.
-     * @param arrowSchema    Input Arrow table schema. This should already have been defined (converted).
-     * @param descriptorPath The predefined path that specifies where to store the data in Flight Serve.
-     * @param chunkSize      The chunk size of the arrow stream. This is different than the batch size of the operator,
-     *                       although they may seem similar. This doesn't actually affect serialization speed that much,
-     *                       so in general it can be the same as {@code batchSize}.
-     */
-    private static void writeArrowStream(FlightClient client, Queue<Tuple> values, RootAllocator root,
-                                         org.apache.arrow.vector.types.pojo.Schema arrowSchema,
-                                         String descriptorPath, int chunkSize) {
-        SyncPutListener flightListener = new SyncPutListener();
-        VectorSchemaRoot schemaRoot = VectorSchemaRoot.create(arrowSchema, root);
-        FlightClient.ClientStreamListener streamWriter = client.startPut(
-                FlightDescriptor.path(Collections.singletonList(descriptorPath)), schemaRoot, flightListener);
-        try {
-            while (!values.isEmpty()) {
-                schemaRoot.allocateNew();
-                int indexThisChunk = 0;
-                while (indexThisChunk < chunkSize && !values.isEmpty()) {
-                    convertAmber2ArrowTuple(values.remove(), indexThisChunk, schemaRoot);
-                    indexThisChunk++;
-                }
-                schemaRoot.setRowCount(indexThisChunk);
-                streamWriter.putNext();
-                schemaRoot.clear();
-            }
-            streamWriter.completed();
-            flightListener.getResult();
-            flightListener.close();
-            schemaRoot.clear();
-        } catch (Exception e) {
-            closeAndThrow(client, e);
-        }
+    @NotNull
+    private Location startFlightServer() throws IOException {
+        int portNumber = getFreeLocalPort();
+        Location location = new Location(URI.create("grpc+tcp://localhost:" + portNumber));
+        // Start Flight server (Python process)
+        new ProcessBuilder(PYTHON, DAEMON_SCRIPT_PATH, Integer.toString(portNumber), pythonScriptPath)
+                .inheritIO()
+                .start();
+        return location;
     }
 
     /**
@@ -478,25 +521,13 @@ public class PythonUDFOpExec implements OperatorExecutor {
         }
     }
 
-    /**
-     * Make the execution of the UDF in Python and read the results that are passed back. This should only be called
-     * after input data is passed to Python. This is a blocking call.
-     *
-     * @param client      The FlightClient that manages this.
-     * @param mapper      Used to decode the result status message (Json).
-     * @param resultQueue To store the results. Must be empty when it is passed here.
-     */
-    private static void executeUDF(FlightClient client, ObjectMapper mapper, Queue<Tuple> resultQueue) {
-        try {
-            byte[] resultBytes = client.doAction(new Action("compute")).next().getBody();
-            Map<String, String> result = mapper.readValue(resultBytes, Map.class);
-            if (result.get("status").equals("Fail")) {
-                String errorMessage = result.get("errorMessage");
-                throw new Exception(errorMessage);
-            }
-            readArrowStream(client, "fromPython", resultQueue);
-        } catch (Exception e) {
-            closeAndThrow(client, e);
+    private void writeTempPythonFile() throws IOException {
+        File tempScriptFile = new File(pythonScriptPath);
+
+        if (tempScriptFile.createNewFile()) {
+            FileWriter fileWriter = new FileWriter(pythonScriptPath);
+            fileWriter.write(pythonScriptText);
+            fileWriter.close();
         }
     }
 
@@ -519,23 +550,12 @@ public class PythonUDFOpExec implements OperatorExecutor {
         }
     }
 
-    /**
-     * Close everything and throw an exception. This should only be called when an exception occurs and needs to be
-     * thrown, but the Arrow Flight Client is still running.
-     *
-     * @param client FlightClient.
-     * @param e      the exception to be wrapped into Amber Exception.
-     */
-    private static void closeAndThrow(FlightClient client, Exception e) {
-        closeClientAndServer(client, false);
+    private void cleanTerminationWithThrow(@NotNull Exception e) {
         e.printStackTrace();
-        throw new RuntimeException(e.getMessage());
+        if (isDynamic) safeDeleteTempFile(pythonScriptPath);
+        closeAndThrow(flightClient, e);
     }
 
-    private static void deleteTempFile(String filePath) throws Exception {
-        File tempFile = new File(filePath);
-        if (tempFile.exists()) {
-            if (!tempFile.delete()) throw new Exception("Could not delete temp file");
-        }
+    static class FlightResponseMap extends HashMap<String, String> {
     }
 }
