0b4d8bd6f0b3720bd19b90b14cc1e47a82e66586
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-05-23 17:39:37 -0700

    add private

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/distinct/DistinctOpExec.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/distinct/DistinctOpExec.scala
@@ -8,7 +8,7 @@ import edu.uci.ics.texera.workflow.common.tuple.Tuple
 import scala.collection.mutable
 
 class DistinctOpExec extends OperatorExecutor {
-  val hashset: mutable.HashSet[Tuple] = mutable.HashSet()
+  private val hashset: mutable.HashSet[Tuple] = mutable.HashSet()
   override def processTexeraTuple(
       tuple: Either[Tuple, InputExhausted],
       input: LinkIdentity
