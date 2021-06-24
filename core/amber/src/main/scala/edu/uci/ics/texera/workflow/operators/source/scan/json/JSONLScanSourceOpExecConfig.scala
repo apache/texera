b6a813b4ea726d8e977208d0a17ab1229c363ebe
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-06-24 00:04:47 -0700

    refactor jsonl

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/source/scan/json/JSONLScanSourceOpExecConfig.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/source/scan/json/JSONLScanSourceOpExecConfig.scala
@@ -4,58 +4,43 @@ import edu.uci.ics.amber.engine.architecture.breakpoint.globalbreakpoint.GlobalB
 import edu.uci.ics.amber.engine.architecture.deploysemantics.deploymentfilter.UseAll
 import edu.uci.ics.amber.engine.architecture.deploysemantics.deploystrategy.RoundRobinDeployment
 import edu.uci.ics.amber.engine.architecture.deploysemantics.layer.WorkerLayer
-import edu.uci.ics.amber.engine.common.virtualidentity.{
-  ActorVirtualIdentity,
-  LayerIdentity,
-  OperatorIdentity
-}
+import edu.uci.ics.amber.engine.common.Constants
+import edu.uci.ics.amber.engine.common.virtualidentity.{ActorVirtualIdentity, LayerIdentity, OperatorIdentity}
 import edu.uci.ics.amber.engine.operators.OpExecConfig
-import edu.uci.ics.texera.workflow.common.tuple.schema.Schema
 
 import java.io.{BufferedReader, FileReader}
 
-class JSONLScanSourceOpExecConfig(
-    tag: OperatorIdentity,
-    numWorkers: Int,
-    filePath: String,
-    schema: Schema,
-    flatten: Boolean
-) extends OpExecConfig(tag) {
-  override lazy val topology: Topology = {
+class JSONLScanSourceOpExecConfig(val tag: OperatorIdentity, val desc: JSONLScanSourceOpDesc)
+    extends OpExecConfig(tag) {
+    override lazy val topology: Topology = {
+        val numWorkers = Constants.defaultNumWorkers
+        val reader = new BufferedReader(new FileReader(desc.filePath.get))
+        var totalLines = 0
+        while ( {
+            reader.readLine != null
+        }) totalLines += 1
+        reader.close()
 
-    val reader = new BufferedReader(new FileReader(filePath))
-    var totalLines = 0
-    while ({ reader.readLine != null }) totalLines += 1
-    reader.close()
-
-    new Topology(
-      Array(
-        new WorkerLayer(
-          LayerIdentity(tag, "main"),
-          i => {
-            val startOffset: Long = totalLines / numWorkers * i
-            val endOffset: Long =
-              if (i != numWorkers - 1) totalLines / numWorkers * (i + 1) else totalLines
-            new JSONLScanSourceOpExec(
-              filePath,
-              schema,
-              flatten,
-              startOffset,
-              endOffset
-            )
-          },
-          numWorkers,
-          UseAll(), // it's source operator
-          RoundRobinDeployment()
+        new Topology(
+            Array(
+                new WorkerLayer(
+                    LayerIdentity(tag, "main"),
+                    i => {
+                        val startOffset: Long = totalLines / numWorkers * i
+                        val endOffset: Long =
+                            if (i != numWorkers - 1) totalLines / numWorkers * (i + 1) else totalLines
+                        new JSONLScanSourceOpExec(desc, startOffset, endOffset)
+                    },
+                    numWorkers,
+                    UseAll(), // it's source operator
+                    RoundRobinDeployment()
+                )
+            ),
+            Array()
         )
-      ),
-      Array()
-    )
-  }
+    }
 
-  override def assignBreakpoint(
-      breakpoint: GlobalBreakpoint[_]
-  ): Array[ActorVirtualIdentity] = {
-    topology.layers(0).identifiers
-  }
+    override def assignBreakpoint(breakpoint: GlobalBreakpoint[_]): Array[ActorVirtualIdentity] = {
+        topology.layers(0).identifiers
+    }
 }
