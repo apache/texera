c041c839ba254dea427109c19cbea7f1e4a916e2
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-06-25 01:06:59 -0700

    fix

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/visualization/htmlviz/HtmlVizOpExec.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/visualization/htmlviz/HtmlVizOpExec.scala
@@ -4,31 +4,32 @@ import edu.uci.ics.amber.engine.common.InputExhausted
 import edu.uci.ics.amber.engine.common.virtualidentity.LinkIdentity
 import edu.uci.ics.texera.workflow.common.operators.OperatorExecutor
 import edu.uci.ics.texera.workflow.common.tuple.Tuple
-import edu.uci.ics.texera.workflow.common.tuple.schema.{Attribute, AttributeType}
+import edu.uci.ics.texera.workflow.common.tuple.schema.{AttributeType, OperatorSchemaInfo}
 
 import scala.collection.Iterator
 import scala.util.Either
 
 /**
-  * HTML Visualization operator to render any given HTML code
-  */
-class HtmlVizOpExec(htmlContentAttrName: String) extends OperatorExecutor {
+ * HTML Visualization operator to render any given HTML code
+ */
+class HtmlVizOpExec(desc: HtmlVizOpDesc, operatorSchemaInfo: OperatorSchemaInfo)
+    extends OperatorExecutor {
 
   override def open(): Unit = {}
 
   override def close(): Unit = {}
 
   override def processTexeraTuple(
-      tuple: Either[Tuple, InputExhausted],
-      input: LinkIdentity
-  ): Iterator[Tuple] =
+                                     tuple: Either[Tuple, InputExhausted],
+                                     input: LinkIdentity
+                                 ): Iterator[Tuple] =
     tuple match {
       case Left(t) =>
-        val result = Tuple
-          .newBuilder()
-          .add("HTML-content", AttributeType.STRING, t.getField(htmlContentAttrName))
-          .build()
+        val result = Tuple.newBuilder(operatorSchemaInfo.outputSchema)
+            .add("html-content", AttributeType.STRING, t.getField(desc.htmlContentAttrName))
+            .build()
         Iterator(result)
+
       case Right(_) => Iterator()
     }
 }
