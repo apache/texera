3dbb6fc09a81ab7a25b6f5eb401e5fda6df6051c
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-07-13 12:41:49 -0700

    fix merge errors

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/controller/promisehandlers/WorkerExecutionCompletedHandler.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/controller/promisehandlers/WorkerExecutionCompletedHandler.scala
@@ -7,13 +7,16 @@ import edu.uci.ics.amber.engine.architecture.controller.promisehandlers.QueryWor
   ControllerInitiateQueryResults,
   ControllerInitiateQueryStatistics
 }
+import edu.uci.ics.amber.engine.architecture.controller.promisehandlers.WorkerExecutionCompletedHandler.WorkerExecutionCompleted
+import edu.uci.ics.amber.engine.architecture.controller.{
+  ControllerAsyncRPCHandlerInitializer,
+  ControllerState
+}
 import edu.uci.ics.amber.engine.architecture.principal.OperatorResult
-import edu.uci.ics.amber.engine.common.rpc.AsyncRPCServer.{CommandCompleted, ControlCommand}
-import edu.uci.ics.amber.engine.common.virtualidentity.{ActorVirtualIdentity, VirtualIdentity}
+import edu.uci.ics.amber.engine.common.rpc.AsyncRPCServer.ControlCommand
 import edu.uci.ics.amber.engine.common.virtualidentity.ActorVirtualIdentity
 import edu.uci.ics.amber.engine.common.virtualidentity.util.CONTROLLER
 import edu.uci.ics.amber.engine.operators.SinkOpExecConfig
-import edu.uci.ics.amber.error.WorkflowRuntimeError
 
 import scala.collection.mutable
 
@@ -41,12 +44,12 @@ trait WorkerExecutionCompletedHandler {
       // because the worker might be killed before the next query statistics interval
       // and the user sees the last update before completion
       val statsRequests = new mutable.MutableList[Future[Unit]]()
-      statsRequests += execute(ControllerInitiateQueryStatistics(Option(List(sender))), Controller)
+      statsRequests += execute(ControllerInitiateQueryStatistics(Option(List(sender))), CONTROLLER)
 
       // if operator is sink, additionally query result immediately one last time
       val resultRequests = new mutable.MutableList[Future[Map[String, OperatorResult]]]()
       if (operator.isInstanceOf[SinkOpExecConfig]) {
-        resultRequests += execute(ControllerInitiateQueryResults(Option(List(sender))), Controller)
+        resultRequests += execute(ControllerInitiateQueryResults(Option(List(sender))), CONTROLLER)
       }
 
       val allRequests = Future.collect(statsRequests ++ resultRequests)
@@ -55,7 +58,7 @@ trait WorkerExecutionCompletedHandler {
         // if entire workflow is completed, clean up
         if (workflow.isCompleted) {
           // send query result again to collect final execution result
-          val finalResult = execute(ControllerInitiateQueryResults(), Controller)
+          val finalResult = execute(ControllerInitiateQueryResults(), CONTROLLER)
           // after query result come back: send completed event, cleanup ,and kill workflow
           finalResult.flatMap(ret => {
             if (eventListener.workflowCompletedListener != null) {
