82d9eeeba2e7c91361e28ed619c670b9abd39485
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-07-13 07:37:38 -0700

    minor update

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/messaginglayer/TupleToBatchConverter.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/messaginglayer/TupleToBatchConverter.scala
@@ -1,11 +1,11 @@
 package edu.uci.ics.amber.engine.architecture.messaginglayer
 
 import edu.uci.ics.amber.engine.architecture.sendsemantics.datatransferpolicy._
-import edu.uci.ics.amber.engine.common.ambermessage.DataPayload
 import edu.uci.ics.amber.engine.common.tuple.ITuple
-import edu.uci.ics.amber.engine.common.virtualidentity.ActorVirtualIdentity
+import edu.uci.ics.amber.engine.common.virtualidentity.{ActorVirtualIdentity, LinkIdentity}
 
 import scala.Function.tupled
+import scala.collection.mutable
 
 /** This class is a container of all the transfer policyExecs.
   * @param selfID ActorVirtualIdentity of self.
@@ -15,7 +15,7 @@ class TupleToBatchConverter(
     selfID: ActorVirtualIdentity,
     dataOutputPort: DataOutputPort
 ) {
-  private var policyExecs = new Array[DataSendingPolicyExec](0)
+  private var policyExecs = mutable.HashMap[LinkIdentity, DataSendingPolicyExec]()
 
   /**
     * Add down stream operator and its corresponding transfer policy executor.
@@ -34,15 +34,7 @@ class TupleToBatchConverter(
     }
 
     // update the existing policy executors.
-    for (i <- policyExecs.indices) {
-      if (policyExecs(i).policy.policyTag == policy.policyTag) {
-        policyExecs(i) = policyExec
-        return
-      }
-    }
-
-    // if no existing policy executor is found to be updated, append it.
-    policyExecs = policyExecs :+ policyExec
+    policyExecs.update(policy.policyTag, policyExec)
 
   }
 
@@ -52,33 +44,26 @@ class TupleToBatchConverter(
     * @param tuple ITuple to be passed.
     */
   def passTupleToDownstream(tuple: ITuple): Unit = {
-    var i = 0
-    while (i < policyExecs.length) {
-      policyExecs(i).addTupleToBatch(tuple) foreach tupled((to, batch) =>
+    policyExecs.valuesIterator.foreach(policyExec =>
+      policyExec.addTupleToBatch(tuple) foreach tupled((to, batch) =>
         dataOutputPort.sendTo(to, batch)
       )
-      i += 1
-    }
+    )
   }
 
   /* Old API: for compatibility */
   @deprecated
   def resetPolicies(): Unit = {
-    policyExecs.foreach(_.reset())
+    policyExecs.values.foreach(_.reset())
   }
 
   /**
     * Send the last batch and EOU marker to all down streams
     */
   def emitEndOfUpstream(): Unit = {
-    var i = 0
-    while (i < policyExecs.length) {
-      val receiversAndBatches: Array[(ActorVirtualIdentity, DataPayload)] = policyExecs(i).noMore()
-      receiversAndBatches.foreach {
-        case (id, batch) => dataOutputPort.sendTo(id, batch)
-      }
-      i += 1
-    }
+    policyExecs.values.foreach(policyExec =>
+      policyExec.noMore() foreach tupled((to, batch) => dataOutputPort.sendTo(to, batch))
+    )
   }
 
 }
