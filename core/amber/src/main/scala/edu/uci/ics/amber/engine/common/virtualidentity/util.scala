abcb330ddfdfde8aa54bdbd4c4990a775dfb42ac
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-07-14 02:26:48 -0700

    remove CLIENT

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/common/virtualidentity/util.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/common/virtualidentity/util.scala
@@ -4,7 +4,6 @@ object util {
 
   lazy val CONTROLLER: ActorVirtualIdentity = ActorVirtualIdentity("CONTROLLER")
   lazy val SELF: ActorVirtualIdentity = ActorVirtualIdentity("SELF")
-  lazy val CLIENT: ActorVirtualIdentity = ActorVirtualIdentity("CLIENT")
 
   def makeLayer(operatorIdentity: OperatorIdentity, layerID: String): LayerIdentity = {
     LayerIdentity(operatorIdentity.workflow, operatorIdentity.operator, layerID)
