701f37ab8c996d2d1c66ce5057a9db9cf1f05b72
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-07-15 01:00:57 -0700

    fix format

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/linksemantics/HashBasedShuffle.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/linksemantics/HashBasedShuffle.scala
@@ -1,7 +1,10 @@
 package edu.uci.ics.amber.engine.architecture.linksemantics
 
 import edu.uci.ics.amber.engine.architecture.deploysemantics.layer.WorkerLayer
-import edu.uci.ics.amber.engine.architecture.sendsemantics.datatransferpolicy.{DataSendingPolicy, HashBasedShufflePolicy}
+import edu.uci.ics.amber.engine.architecture.sendsemantics.datatransferpolicy.{
+  DataSendingPolicy,
+  HashBasedShufflePolicy
+}
 import edu.uci.ics.amber.engine.common.tuple.ITuple
 import edu.uci.ics.amber.engine.common.virtualidentity.{ActorVirtualIdentity, LinkIdentity}
 
@@ -11,14 +14,15 @@ class HashBasedShuffle(
     batchSize: Int,
     hashFunc: ITuple => Int
 ) extends LinkStrategy(from, to, batchSize) {
-  override def getPolicies
-      : Iterable[(ActorVirtualIdentity, LinkIdentity, DataSendingPolicy, Seq[ActorVirtualIdentity])] = {
+  override def getPolicies: Iterable[
+    (ActorVirtualIdentity, LinkIdentity, DataSendingPolicy, Seq[ActorVirtualIdentity])
+  ] = {
     assert(from.isBuilt && to.isBuilt)
     from.identifiers.map(x =>
       (
         x,
-      id,
-        HashBasedShufflePolicy( batchSize, to.identifiers, hashFunc),
+        id,
+        HashBasedShufflePolicy(batchSize, to.identifiers, hashFunc),
         to.identifiers.toSeq
       )
     )
--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/linksemantics/OneToOne.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/linksemantics/OneToOne.scala
@@ -1,18 +1,22 @@
 package edu.uci.ics.amber.engine.architecture.linksemantics
 
 import edu.uci.ics.amber.engine.architecture.deploysemantics.layer.WorkerLayer
-import edu.uci.ics.amber.engine.architecture.sendsemantics.datatransferpolicy.{DataSendingPolicy, OneToOnePolicy}
+import edu.uci.ics.amber.engine.architecture.sendsemantics.datatransferpolicy.{
+  DataSendingPolicy,
+  OneToOnePolicy
+}
 import edu.uci.ics.amber.engine.common.virtualidentity.{ActorVirtualIdentity, LinkIdentity}
 
 class OneToOne(from: WorkerLayer, to: WorkerLayer, batchSize: Int)
     extends LinkStrategy(from, to, batchSize) {
-  override def getPolicies
-      : Iterable[(ActorVirtualIdentity, LinkIdentity, DataSendingPolicy, Seq[ActorVirtualIdentity])] = {
+  override def getPolicies: Iterable[
+    (ActorVirtualIdentity, LinkIdentity, DataSendingPolicy, Seq[ActorVirtualIdentity])
+  ] = {
     assert(from.isBuilt && to.isBuilt && from.numWorkers == to.numWorkers)
     from.identifiers.indices.map(i =>
       (
         from.identifiers(i),
-          id,
+        id,
         OneToOnePolicy(batchSize, Array(to.identifiers(i))),
         Seq(to.identifiers(i))
       )
