88adecd5a1950d382f715761f4425aa63a358265
DanielMao1 <daniel4006695539@gmail.com>
2021-07-22 14:12:22 +0800

    fix bugs due to latest version of master branch update

--- core/amber/src/test/scala/edu/uci/ics/texera/workflow/operators/intervalJoin/IntervalOpExecSpec.scala
+++ core/amber/src/test/scala/edu/uci/ics/texera/workflow/operators/intervalJoin/IntervalOpExecSpec.scala
@@ -24,7 +24,7 @@ class IntervalOpExecSpec extends AnyFlatSpec with BeforeAndAfter {
   var opDesc: IntervalJoinOpDesc = _
   var counter: Int = 0
 
-  def linkID(): LinkIdentity = LinkIdentity(Option(layerID()), Option(layerID()))
+  def linkID(): LinkIdentity = LinkIdentity(layerID(), layerID())
 
   def layerID(): LayerIdentity = {
     counter += 1
