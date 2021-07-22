77406bca4395555c2a266f6b20df7a4cd863d56a
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-07-21 19:02:34 -0700

    clean up

--- core/amber/src/main/python/core/proxy/test_proxy_client.py
+++ core/amber/src/main/python/core/proxy/test_proxy_client.py
@@ -60,6 +60,7 @@ class TestProxyClient:
         with server:
             def hello():
                 return "hello-function"
+
             action_count = len(client.list_actions())
             server.register("hello-function", hello)
             assert len(client.list_actions()) == action_count + 1
@@ -77,64 +78,6 @@ class TestProxyClient:
             assert len(client.list_actions()) == action_count + 1
             assert client.call_action("hello-class") == b'hello-class'
             assert client.call_action("shutdown") == b'Bye bye!'
-
-    # def test_client_can_send_control_message(self, server, client):
-    #     with server:
-    #         server.register_control_handler()
-
-    # def test_client_can_call_registered_lambdas_with_args(self, server, client):
-    #     with server:
-    #         server.register("echo", lambda x: x)
-    #         assert len(client.list_actions()) == 2
-    #         assert client.call_action("echo", "no") == b'no'
-    #         assert client.call_action("shutdown") == b'Bye bye!'
-    #
-    # def test_client_can_call_registered_lambdas_with_args2(self, server, client):
-    #     with server:
-    #         server.register("add", lambda a, b: a + b)
-    #         assert len(client.list_actions()) == 2
-    #         assert client.call_action("add", a=5, b=4) == b'9'
-    #         assert client.call_action("add", 1.1, 2.3) == b'3.4'
-    #         assert client.call_action("add", a=[1, 2, 3], b=[5]) == b'[1, 2, 3, 5]'
-    #         assert client.call_action("shutdown") == b'Bye bye!'
-    #
-    # def test_client_can_call_registered_lambdas_with_args_and_ack(self, server, client):
-    #     with server:
-    #         server.register("add", ProxyServer.ack()(lambda a, b: a + b))
-    #         assert len(client.list_actions()) == 2
-    #         assert client.call_action("add", a=5, b=4) == b'ack'
-    #         assert client.call_action("add", 1.1, 2.3) == b'ack'
-    #         assert client.call_action("add", a=[1, 2, 3], b=[5]) == b'ack'
-    #         assert client.call_action("shutdown") == b'Bye bye!'
-    #
-    # def test_client_can_call_registered_lambdas_with_args_and_other_ack(self, server, client):
-    #     class Extend:
-    #         @ProxyServer.ack(msg="extended!")
-    #         def __call__(self, a: list, b: Iterator):
-    #             a.extend(b)
-    #             return a
-    #
-    #     def extend(a: list, b: Iterator):
-    #         a.extend(b)
-    #         return a
-    #
-    #     with server:
-    #         server.register("extend_with_ack", Extend())
-    #         server.register("extend_with_result", extend)
-    #         assert len(client.list_actions()) == 3
-    #         assert client.call_action("extend_with_ack", [5], (4,)) == b'extended!'
-    #         assert client.call_action("extend_with_result", [], "hello") == b"['h', 'e', 'l', 'l', 'o']"
-    #         assert client.call_action("extend_with_result", a=[1, 2, 3], b=[5]) == b'[1, 2, 3, 5]'
-    #         assert client.call_action("shutdown") == b'Bye bye!'
-    #
-    # def test_client_can_call_registered_lambdas_with_args_and_exceptions(self, server, client):
-    #     with server:
-    #         server.register("div", lambda a, b: a / b)
-    #         assert len(client.list_actions()) == 2
-    #         assert client.call_action("div", a=5, b=2) == b'2.5'
-    #         with pytest.raises(FlightServerError):
-    #             client.call_action("div", a=1, b=0)
-    #         assert client.call_action("shutdown") == b'Bye bye!'
 
     def test_client_cannot_send_data_without_handler(self, server, client, data_table):
         with server:
