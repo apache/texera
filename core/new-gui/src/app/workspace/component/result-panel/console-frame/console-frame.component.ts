849c7b5285b26d7606e5087e54adc1a6f2173446
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-08-13 16:13:29 -0700

    handle Failed State

--- core/new-gui/src/app/workspace/component/result-panel/console-frame/console-frame.component.ts
+++ core/new-gui/src/app/workspace/component/result-panel/console-frame/console-frame.component.ts
@@ -4,6 +4,7 @@ import { NzModalService } from 'ng-zorro-antd/modal';
 import { ResultPanelToggleService } from '../../../service/result-panel-toggle/result-panel-toggle.service';
 import { WorkflowActionService } from '../../../service/workflow-graph/model/workflow-action.service';
 import { BreakpointTriggerInfo } from '../../../types/workflow-common.interface';
+import { ExecutionState } from 'src/app/workspace/types/execute-workflow.interface';
 
 @Component({
   selector: 'texera-console-frame',
@@ -27,26 +28,33 @@ export class ConsoleFrameComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    // update highlighted operator
-    const highlightedOperators = this.workflowActionService.getJointGraphWrapper().getCurrentHighlightedOperatorIDs();
-    this.resultPanelOperatorID = highlightedOperators.length === 1 ? highlightedOperators[0] : undefined;
-    const breakpointTriggerInfo = this.executeWorkflowService.getBreakpointTriggerInfo();
-    if (this.resultPanelOperatorID && this.resultPanelOperatorID === breakpointTriggerInfo?.operatorID) {
-      this.breakpointTriggerInfo = breakpointTriggerInfo;
-      this.breakpointAction = true;
-      // const result = breakpointTriggerInfo.report.map(r => r.faultedTuple.tuple).filter(t => t !== undefined);
-      // this.setupResultTable(result, result.length);
-      const errorsMessages: Record<string, string> = {};
-      breakpointTriggerInfo.report.forEach(r => {
-        const pathsplitted = r.actorPath.split('/');
-        const workerName = pathsplitted[pathsplitted.length - 1];
-        const workerText = 'Worker ' + workerName + ':                ';
-        if (r.messages.toString().toLowerCase().includes('exception')) {
-          errorsMessages[workerText] = r.messages.toString();
-        }
-      });
-      this.errorMessages = errorsMessages;
+    const executionState = this.executeWorkflowService.getExecutionState();
+
+    if (executionState.state === ExecutionState.Failed) {
+      this.errorMessages = this.executeWorkflowService.getErrorMessages();
+    } else {
+      // update highlighted operator
+      const highlightedOperators = this.workflowActionService.getJointGraphWrapper().getCurrentHighlightedOperatorIDs();
+      this.resultPanelOperatorID = highlightedOperators.length === 1 ? highlightedOperators[0] : undefined;
+      const breakpointTriggerInfo = this.executeWorkflowService.getBreakpointTriggerInfo();
+      if (this.resultPanelOperatorID && this.resultPanelOperatorID === breakpointTriggerInfo?.operatorID) {
+        this.breakpointTriggerInfo = breakpointTriggerInfo;
+        this.breakpointAction = true;
+        // const result = breakpointTriggerInfo.report.map(r => r.faultedTuple.tuple).filter(t => t !== undefined);
+        // this.setupResultTable(result, result.length);
+        const errorsMessages: Record<string, string> = {};
+        breakpointTriggerInfo.report.forEach(r => {
+          const splitPath = r.actorPath.split('/');
+          const workerName = splitPath[splitPath.length - 1];
+          const workerText = 'Worker ' + workerName + ':                ';
+          if (r.messages.toString().toLowerCase().includes('exception')) {
+            errorsMessages[workerText] = r.messages.toString();
+          }
+        });
+        this.errorMessages = errorsMessages;
+      }
     }
+
   }
 
   public onClickSkipTuples(): void {
