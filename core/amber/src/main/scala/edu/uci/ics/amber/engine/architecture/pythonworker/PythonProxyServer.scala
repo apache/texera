e4660a77bf855c88ff949ffcdfa181108cf2da4d
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-08-20 10:52:13 -0700

    fix some warnings

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/pythonworker/PythonProxyServer.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/pythonworker/PythonProxyServer.scala
@@ -62,25 +62,24 @@ private class AmberProducer(
     val isEnd: Boolean = dataHeader.isEnd
 
     val root = flightStream.getRoot
-    try {
-      // consume all data in the stream, it will store on the root vectors.
-      while (flightStream.next) {
-        ackStream.onNext(PutResult.metadata(flightStream.getLatestMetadata))
-      }
-      // closing the stream will release the dictionaries
-      flightStream.takeDictionaryOwnership
 
-      if (isEnd) {
-        // EndOfUpstream
-        assert(root.getRowCount == 0)
-        dataOutputPort.sendTo(to, EndOfUpstream())
-      } else {
-        // normal data batches
-        val queue = mutable.Queue[Tuple]()
-        for (i <- 0 until root.getRowCount)
-          queue.enqueue(ArrowUtils.getTexeraTuple(i, root))
-        dataOutputPort.sendTo(to, DataFrame(queue.toArray))
-      }
+    // consume all data in the stream, it will store on the root vectors.
+    while (flightStream.next) {
+      ackStream.onNext(PutResult.metadata(flightStream.getLatestMetadata))
+    }
+    // closing the stream will release the dictionaries
+    flightStream.takeDictionaryOwnership
+
+    if (isEnd) {
+      // EndOfUpstream
+      assert(root.getRowCount == 0)
+      dataOutputPort.sendTo(to, EndOfUpstream())
+    } else {
+      // normal data batches
+      val queue = mutable.Queue[Tuple]()
+      for (i <- 0 until root.getRowCount)
+        queue.enqueue(ArrowUtils.getTexeraTuple(i, root))
+      dataOutputPort.sendTo(to, DataFrame(queue.toArray))
 
     }
 
