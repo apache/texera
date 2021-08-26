d58c033a7548b43025eb32da5ef96ca6e07c5970
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-08-26 16:02:44 -0700

    change to interface

--- core/new-gui/src/app/common/type/dynamic-component-config.ts
+++ core/new-gui/src/app/common/type/dynamic-component-config.ts
@@ -1,6 +1,6 @@
 import { Type } from '@angular/core';
 
-export declare class DynamicComponentConfig<T> {
+export interface DynamicComponentConfig<T> {
   component?: Type<T>;
   componentInputs?: Partial<T>;
 }
