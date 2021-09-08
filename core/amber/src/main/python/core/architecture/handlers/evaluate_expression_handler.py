a224f693dab4fd6c62d3eed721eacf91805433db
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-09-08 10:24:21 -0700

    handle container types

--- core/amber/src/main/python/core/architecture/handlers/evaluate_expression_handler.py
+++ core/amber/src/main/python/core/architecture/handlers/evaluate_expression_handler.py
@@ -8,6 +8,11 @@ from ..managers.context import Context
 class EvaluateExpressionHandler(Handler):
     cmd = EvaluateExpressionV2
 
+    @staticmethod
+    def is_expandable(obj) -> bool:
+        return ((hasattr(obj, "__dict__") and len(obj.__dict__) > 0) or hasattr(obj, "__getitem__")) and (
+            not (hasattr(obj, "__len__") and len(obj) == 0))
+
     def __call__(self, context: Context, command: cmd, *args, **kwargs):
         contextualized_expression = re.sub(r'self', r'context.dp._udf_operator', command.expression)
         value = eval(contextualized_expression)
@@ -15,11 +20,24 @@ class EvaluateExpressionHandler(Handler):
         type_str = type(value).__name__
 
         attributes = list()
+
+        # add attributes
         if hasattr(value, "__dict__"):
             for k, v in value.__dict__.items():
+                print(TypedValue(expression=k, value_str=repr(v), value_type=type(v).__name__,
+                                 expandable=hasattr(v, "__dict__") or hasattr(v, "__getitem__")))
                 attributes.append(TypedValue(expression=k, value_str=repr(v), value_type=type(v).__name__,
-                                             expandable=hasattr(v, "__dict__")))
+                                             expandable=self.is_expandable(v)))
 
+        # add container items
+        if hasattr(value, "__getitem__"):
+            for i in range(len(value)):
+                print(TypedValue(expression=f"[{i}]", value_str=repr(value[i]), value_type=type(value[i]).__name__,
+                                 expandable=True))
+                attributes.append(
+                    TypedValue(expression=f"__getitem__({i})", value_str=repr(value[i]),
+                               value_type=type(value[i]).__name__,
+                               expandable=self.is_expandable(value[i])))
         return EvaluatedValue(
             value=TypedValue(expression=command.expression, value_str=value_str, value_type=type_str, expandable=True),
             attributes=attributes)
