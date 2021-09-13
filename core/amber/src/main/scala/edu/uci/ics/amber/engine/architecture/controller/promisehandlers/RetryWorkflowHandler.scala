d2778bed72677fbff1b719e157f2147d6755d413
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-09-13 15:14:55 -0700

    send resume workflow

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/controller/promisehandlers/RetryWorkflowHandler.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/controller/promisehandlers/RetryWorkflowHandler.scala
@@ -1,14 +1,12 @@
 package edu.uci.ics.amber.engine.architecture.controller.promisehandlers
 
 import com.twitter.util.Future
+import edu.uci.ics.amber.engine.architecture.controller.ControllerAsyncRPCHandlerInitializer
+import edu.uci.ics.amber.engine.architecture.controller.promisehandlers.ResumeHandler.ResumeWorkflow
 import edu.uci.ics.amber.engine.architecture.controller.promisehandlers.RetryWorkflowHandler.RetryWorkflow
-import edu.uci.ics.amber.engine.architecture.controller.{
-  ControllerAsyncRPCHandlerInitializer,
-  ControllerState
-}
 import edu.uci.ics.amber.engine.architecture.pythonworker.promisehandlers.RetryPythonHandler.RetryPython
-import edu.uci.ics.amber.engine.architecture.worker.promisehandlers.ResumeHandler.ResumeWorker
 import edu.uci.ics.amber.engine.common.rpc.AsyncRPCServer.ControlCommand
+import edu.uci.ics.amber.engine.common.virtualidentity.util.CONTROLLER
 
 object RetryWorkflowHandler {
   final case class RetryWorkflow() extends ControlCommand[Unit]
@@ -38,22 +36,7 @@ trait RetryWorkflowHandler {
         .unit
 
       // resume all workers
-      // resume message has no effect on non-paused workers
-      Future
-        .collect(
-          workflow.getAllWorkers.map { worker =>
-            send(ResumeWorker(), worker).map { ret =>
-              workflow.getWorkerInfo(worker).state = ret
-            }
-          }.toSeq
-        )
-        .map { _ =>
-          // update frontend status
-          updateFrontendWorkflowStatus()
-          enableStatusUpdate() //re-enabled it since it is disabled in pause
-          actorContext.parent ! ControllerState.Running //for testing
-
-        }
+      execute(ResumeWorkflow(), CONTROLLER)
     }
   }
 }
