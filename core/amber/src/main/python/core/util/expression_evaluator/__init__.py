70bc41b9cc3c8b902a1872006bc99516aeda7b30
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-09-14 11:53:34 -0700

    make some APIs private

--- core/amber/src/main/python/core/util/expression_evaluator/__init__.py
+++ core/amber/src/main/python/core/util/expression_evaluator/__init__.py
@@ -5,7 +5,12 @@ from proto.edu.uci.ics.amber.engine.architecture.worker import EvaluatedValue, T
 
 
 class RuntimeContext:
-    def __init__(self, elements):
+    """
+    A context container for explicit exposure of defined attributes.
+    It also provides a regex-based mapping to inject any expressions within the context.
+    """
+
+    def __init__(self, elements: Dict[str, Any]):
         for name, value in elements.items():
             self.__setattr__(name, value)
 
@@ -14,23 +19,9 @@ class RuntimeContext:
 
 
 class ExpressionEvaluator:
-
-    @staticmethod
-    def is_expandable(obj) -> bool:
-        return ExpressionEvaluator.contains_attributes(obj) or \
-               (ExpressionEvaluator.is_iterable(obj) and not ExpressionEvaluator.is_empty_container(obj))
-
-    @staticmethod
-    def is_iterable(obj) -> bool:
-        return hasattr(obj, "__iter__")
-
-    @staticmethod
-    def contains_attributes(obj) -> bool:
-        return hasattr(obj, "__dict__") and len(obj.__dict__) > 0
-
-    @staticmethod
-    def is_empty_container(obj) -> bool:
-        return hasattr(obj, "__len__") and len(obj) == 0
+    """
+    Provides a series of static evaluation methods of a given expression, with an optional context.
+    """
 
     @staticmethod
     def evaluate(expression: str, runtime_context: Optional[RuntimeContext] = None) -> EvaluatedValue:
@@ -39,7 +30,7 @@ class ExpressionEvaluator:
                 "needs a valid RuntimeContext to evaluate mapped expression"
 
             context_mapping = runtime_context.to_mapping()
-            contextualized_expression = ExpressionEvaluator.contextualize_expression(expression, context_mapping)
+            contextualized_expression = ExpressionEvaluator._contextualize_expression(expression, context_mapping)
             value = eval(contextualized_expression)
         else:
             value = eval(expression)
@@ -50,22 +41,39 @@ class ExpressionEvaluator:
         attributes = list()
 
         # add attributes
-        attributes += ExpressionEvaluator.extract_attributes(value)
+        attributes += ExpressionEvaluator._extract_attributes(value)
 
         # add container items
-        attributes += ExpressionEvaluator.extract_container_items(value)
+        attributes += ExpressionEvaluator._extract_container_items(value)
         return EvaluatedValue(
             value=TypedValue(
                 expression=expression,
                 value_ref=expression,
                 value_str=value_str,
                 value_type=type_str,
-                expandable=ExpressionEvaluator.is_expandable(value)),
+                expandable=ExpressionEvaluator._is_expandable(value)),
             attributes=attributes
         )
 
     @staticmethod
-    def contextualize_expression(expression: str, context_replacements: Dict[Pattern[str], str]) -> str:
+    def _is_expandable(obj) -> bool:
+        return ExpressionEvaluator._contains_attributes(obj) or \
+               (ExpressionEvaluator._is_iterable(obj) and not ExpressionEvaluator._is_empty_container(obj))
+
+    @staticmethod
+    def _is_iterable(obj) -> bool:
+        return hasattr(obj, "__iter__")
+
+    @staticmethod
+    def _contains_attributes(obj) -> bool:
+        return hasattr(obj, "__dict__") and len(obj.__dict__) > 0
+
+    @staticmethod
+    def _is_empty_container(obj) -> bool:
+        return hasattr(obj, "__len__") and len(obj) == 0
+
+    @staticmethod
+    def _contextualize_expression(expression: str, context_replacements: Dict[Pattern[str], str]) -> str:
 
         contextualized_expression = expression
         for pattern, contextualized_pattern in context_replacements.items():
@@ -73,9 +81,9 @@ class ExpressionEvaluator:
         return contextualized_expression
 
     @staticmethod
-    def extract_container_items(value: Any) -> List[TypedValue]:
+    def _extract_container_items(value: Any) -> List[TypedValue]:
         contained_items = []
-        if ExpressionEvaluator.is_iterable(value):
+        if ExpressionEvaluator._is_iterable(value):
             if hasattr(value, "items"):
                 for k, v in value.items():
                     contained_items.append(
@@ -84,7 +92,7 @@ class ExpressionEvaluator:
                             value_ref=repr(k),
                             value_str=repr(v),
                             value_type=type(v).__name__,
-                            expandable=ExpressionEvaluator.is_expandable(v)
+                            expandable=ExpressionEvaluator._is_expandable(v)
                         )
                     )
 
@@ -97,13 +105,13 @@ class ExpressionEvaluator:
                             value_ref=repr(i),
                             value_str=repr(value_tuple[i]),
                             value_type=type(value_tuple[i]).__name__,
-                            expandable=ExpressionEvaluator.is_expandable(value_tuple[i])
+                            expandable=ExpressionEvaluator._is_expandable(value_tuple[i])
                         )
                     )
         return contained_items
 
     @staticmethod
-    def extract_attributes(value: Any) -> List[TypedValue]:
+    def _extract_attributes(value: Any) -> List[TypedValue]:
         attributes = []
         if hasattr(value, "__dict__"):
             for k, v in value.__dict__.items():
@@ -113,7 +121,7 @@ class ExpressionEvaluator:
                         value_ref=k,
                         value_str=repr(v),
                         value_type=type(v).__name__,
-                        expandable=ExpressionEvaluator.is_expandable(v)
+                        expandable=ExpressionEvaluator._is_expandable(v)
                     )
                 )
         return attributes
