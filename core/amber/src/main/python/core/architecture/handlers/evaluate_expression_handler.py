0d2545c6fbe4d5078172b6ddb4552bd9a1b36a5f
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-09-14 09:22:31 -0700

    clean code

--- core/amber/src/main/python/core/architecture/handlers/evaluate_expression_handler.py
+++ core/amber/src/main/python/core/architecture/handlers/evaluate_expression_handler.py
@@ -1,31 +1,34 @@
 import re
+from typing import Any, List
 
 from proto.edu.uci.ics.amber.engine.architecture.worker import EvaluateExpressionV2, EvaluatedValue, TypedValue
 from .handler_base import Handler
 from ..managers.context import Context
 
 
-def is_expandable(obj) -> bool:
-    return contains_attributes(obj) or (is_iterable(obj) and not is_empty_container(obj))
+class ExpressionEvaluator:
 
+    @staticmethod
+    def is_expandable(obj) -> bool:
+        return ExpressionEvaluator.contains_attributes(obj) or \
+               (ExpressionEvaluator.is_iterable(obj) and not ExpressionEvaluator.is_empty_container(obj))
 
-def is_iterable(obj) -> bool:
-    return hasattr(obj, "__iter__")
+    @staticmethod
+    def is_iterable(obj) -> bool:
+        return hasattr(obj, "__iter__")
 
+    @staticmethod
+    def contains_attributes(obj) -> bool:
+        return hasattr(obj, "__dict__") and len(obj.__dict__) > 0
 
-def contains_attributes(obj) -> bool:
-    return hasattr(obj, "__dict__") and len(obj.__dict__) > 0
+    @staticmethod
+    def is_empty_container(obj) -> bool:
+        return hasattr(obj, "__len__") and len(obj) == 0
 
+    @staticmethod
+    def evaluate(expression: str, context: Context) -> EvaluatedValue:
+        contextualized_expression = ExpressionEvaluator.contextualize_expression(expression)
 
-def is_empty_container(obj) -> bool:
-    return hasattr(obj, "__len__") and len(obj) == 0
-
-
-class EvaluateExpressionHandler(Handler):
-    cmd = EvaluateExpressionV2
-
-    def __call__(self, context: Context, command: cmd, *args, **kwargs):
-        contextualized_expression = re.sub(r'self', r'context.dp._udf_operator', command.expression)
         value = eval(contextualized_expression)
         value_str = repr(value)
         type_str = type(value).__name__
@@ -33,37 +36,84 @@ class EvaluateExpressionHandler(Handler):
         attributes = list()
 
         # add attributes
-        if hasattr(value, "__dict__"):
-            for k, v in value.__dict__.items():
-                attributes.append(TypedValue(expression=k,
-                                             value_ref=k,
-                                             value_str=repr(v),
-                                             value_type=type(v).__name__,
-                                             expandable=is_expandable(v)))
+        attributes += ExpressionEvaluator.extract_attributes(value)
 
         # add container items
-        if is_iterable(value):
+        attributes += ExpressionEvaluator.extract_container_items(value)
+        return EvaluatedValue(
+            value=TypedValue(
+                expression=expression,
+                value_ref=expression,
+                value_str=value_str,
+                value_type=type_str,
+                expandable=True),
+            attributes=attributes
+        )
+
+    @staticmethod
+    def contextualize_expression(expression: str) -> str:
+        context_replacements = [
+            (r'self', r'context.dp._udf_operator'),
+            (r'tuple_', r'context.dp._current_input_tuple'),
+            (r'link', r'context.dp._current_input_link'),
+
+        ]
+        contextualized_expression = expression
+        for pattern, contextualized_pattern in context_replacements:
+            contextualized_expression = re.sub(pattern, contextualized_pattern, contextualized_expression)
+        return contextualized_expression
+
+    @staticmethod
+    def extract_container_items(value: Any) -> List[TypedValue]:
+        contained_items = []
+        if ExpressionEvaluator.is_iterable(value):
             if hasattr(value, "items"):
                 for k, v in value.items():
-                    attributes.append(
-                        TypedValue(expression=f"__getitem__({repr(k)})",
-                                   value_ref=repr(k),
-                                   value_str=repr(v),
-                                   value_type=type(v).__name__,
-                                   expandable=is_expandable(v)))
+                    contained_items.append(
+                        TypedValue(
+                            expression=f"__getitem__({repr(k)})",
+                            value_ref=repr(k),
+                            value_str=repr(v),
+                            value_type=type(v).__name__,
+                            expandable=ExpressionEvaluator.is_expandable(v)
+                        )
+                    )
+
             else:
                 value_tuple = tuple(value)
                 for i, item in enumerate(value_tuple):
-                    attributes.append(
-                        TypedValue(expression=f"__getitem__({i})",
-                                   value_ref=repr(i),
-                                   value_str=repr(value_tuple[i]),
-                                   value_type=type(value_tuple[i]).__name__,
-                                   expandable=is_expandable(value_tuple[i])))
-        return EvaluatedValue(
-            value=TypedValue(expression=command.expression,
-                             value_ref=command.expression,
-                             value_str=value_str,
-                             value_type=type_str,
-                             expandable=True),
-            attributes=attributes)
+                    contained_items.append(
+                        TypedValue(
+                            expression=f"__getitem__({i})",
+                            value_ref=repr(i),
+                            value_str=repr(value_tuple[i]),
+                            value_type=type(value_tuple[i]).__name__,
+                            expandable=ExpressionEvaluator.is_expandable(value_tuple[i])
+                        )
+                    )
+        return contained_items
+
+    @staticmethod
+    def extract_attributes(value: Any) -> List[TypedValue]:
+        attributes = []
+        if hasattr(value, "__dict__"):
+            for k, v in value.__dict__.items():
+                attributes.append(
+                    TypedValue(
+                        expression=k,
+                        value_ref=k,
+                        value_str=repr(v),
+                        value_type=type(v).__name__,
+                        expandable=ExpressionEvaluator.is_expandable(v)
+                    )
+                )
+        return attributes
+
+
+class EvaluateExpressionHandler(Handler):
+    cmd = EvaluateExpressionV2
+
+    def __call__(self, context: Context, command: cmd, *args, **kwargs):
+        evaluated_value = ExpressionEvaluator.evaluate(command.expression, context)
+
+        return evaluated_value
