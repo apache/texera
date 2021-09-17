63f92e788d9de0854bfcda7c029b96e0003df599
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-09-16 23:41:17 -0700

    fix format

--- core/amber/src/main/scala/edu/uci/ics/texera/web/basicauth/JwtAuth.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/basicauth/JwtAuth.scala
@@ -4,13 +4,13 @@ import org.jose4j.jwt.consumer.{JwtConsumer, JwtConsumerBuilder}
 import org.jose4j.keys.HmacKey
 
 object JwtAuth {
-    val jwtTokenSecret: String = "dfwzsdzwh823zebdwdz772632gdsbd21"
-    // create the JwtConsumer instance
-    val jwtConsumer: JwtConsumer = new JwtConsumerBuilder()
-        .setAllowedClockSkewInSeconds(30)
-        .setRequireExpirationTime()
-        .setRequireSubject()
-        .setVerificationKey(new HmacKey(jwtTokenSecret.getBytes))
-        .setRelaxVerificationKeyValidation()
-        .build
+  val jwtTokenSecret: String = "dfwzsdzwh823zebdwdz772632gdsbd21"
+  // create the JwtConsumer instance
+  val jwtConsumer: JwtConsumer = new JwtConsumerBuilder()
+    .setAllowedClockSkewInSeconds(30)
+    .setRequireExpirationTime()
+    .setRequireSubject()
+    .setVerificationKey(new HmacKey(jwtTokenSecret.getBytes))
+    .setRelaxVerificationKeyValidation()
+    .build
 }
