5ab4fc4aff5730298c754a66994e0e1609e10911
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-09-16 23:52:19 -0700

    Update TexeraWebApplication.scala

--- core/amber/src/main/scala/edu/uci/ics/texera/web/TexeraWebApplication.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/TexeraWebApplication.scala
@@ -94,7 +94,7 @@ class TexeraWebApplication extends io.dropwizard.Application[TexeraWebConfigurat
     // register MultiPartFeature
     environment.jersey.register(classOf[MultiPartFeature])
 
-    environment.jersey().register(classOf[SystemMetadataResource])
+    environment.jersey.register(classOf[SystemMetadataResource])
     //    environment.jersey().register(classOf[MockKillWorkerResource])
     environment.jersey.register(classOf[SchemaPropagationResource])
     environment.jersey.register(classOf[UserResource])
