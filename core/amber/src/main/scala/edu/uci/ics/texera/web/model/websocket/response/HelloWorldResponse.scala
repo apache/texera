1a2e616689ec68a8b785455bb35170e3cf595f07
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-09-24 11:21:04 -0700

    fix format

--- core/amber/src/main/scala/edu/uci/ics/texera/web/model/websocket/response/HelloWorldResponse.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/model/websocket/response/HelloWorldResponse.scala
@@ -3,5 +3,3 @@ package edu.uci.ics.texera.web.model.websocket.response
 import edu.uci.ics.texera.web.model.websocket.event.TexeraWebSocketEvent
 
 case class HelloWorldResponse(message: String) extends TexeraWebSocketEvent
-
-
