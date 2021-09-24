784def1092c1a1ebf19c52fe9fc2ea971a22757f
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-09-23 19:00:07 -0700

    support random secret

--- core/amber/src/main/scala/edu/uci/ics/texera/web/auth/JwtAuth.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/auth/JwtAuth.scala
@@ -1,11 +1,18 @@
 package edu.uci.ics.texera.web.auth
 
+import com.typesafe.config.Config
 import edu.uci.ics.amber.engine.common.AmberUtils
 import org.jose4j.jwt.consumer.{JwtConsumer, JwtConsumerBuilder}
 import org.jose4j.keys.HmacKey
 
+import java.util.Random
+
 object JwtAuth {
-  val jwtTokenSecret: String = AmberUtils.amberConfig.getString("user-sys.jwt.256-bit-secret")
+  val jwtConfig: Config = AmberUtils.amberConfig.getConfig("user-sys.jwt")
+  val jwtTokenSecret: String = jwtConfig.getString("256-bit-secret").toLowerCase() match {
+    case "random" => getRandomHexString
+    case _        => jwtConfig.getString("256-bit-secret")
+  }
   // create the JwtConsumer instance
   val jwtConsumer: JwtConsumer = new JwtConsumerBuilder()
     .setAllowedClockSkewInSeconds(30)
@@ -14,4 +21,13 @@ object JwtAuth {
     .setVerificationKey(new HmacKey(jwtTokenSecret.getBytes))
     .setRelaxVerificationKeyValidation()
     .build
+
+  private def getRandomHexString: String = {
+    val bytes = 32
+    val r = new Random()
+    val sb = new StringBuffer
+    while (sb.length < bytes)
+      sb.append(Integer.toHexString(r.nextInt()))
+    sb.toString.substring(0, bytes)
+  }
 }
