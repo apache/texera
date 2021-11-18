1bd73b325774b81e85521f0732f65207f6fb6be9
jessieeeovo <jessieyao0602@gmail.com>
2021-11-18 08:58:19 -0800

    fix frontend for bar and line chart visualization

--- core/new-gui/src/app/workspace/component/visualization-panel-content/visualization-frame-content.component.ts
+++ core/new-gui/src/app/workspace/component/visualization-panel-content/visualization-frame-content.component.ts
@@ -127,9 +127,12 @@ export class VisualizationFrameContentComponent implements AfterContentInit, OnD
     if (!this.data || !this.chartType) {
       return;
     }
+    console.log("starttt");
+    console.log("data" + this.data);
     if (this.data?.length < 1) {
       return;
     }
+    console.log("enddd");
     this.displayHTML = false;
     this.displayWordCloud = false;
     this.displayMap = true;
@@ -353,20 +356,28 @@ export class VisualizationFrameContentComponent implements AfterContentInit, OnD
 
     const dataToDisplay: Array<[string, ...PrimitiveArray]> = [];
     const category: string[] = [];
-    for (let i = 1; i < this.columns?.length; i++) {
-      category.push(this.columns[i]);
+
+    const result = this.data as Array<Record<string, Primitive>>;
+
+    // category for x-axis
+    for (let i = 1; i < Object.values(result[0]).length; i++) {
+      category.push(String(Object.keys(result[0])[i]));
     }
+    console.log("cate:" + category);
 
-    const columnCount = this.columns.length;
+    const columnCount = category.length;
 
-    for (const row of this.data) {
-      const items: [string, ...PrimitiveArray] = [Object.values(row)[0]];
-      for (let i = 1; i < columnCount; i++) {
+    // data
+    for (const row of result) {
+      var items: [string, ...PrimitiveArray] = [String(Object.values(row)[0])];
+      for (let i = 1; i < columnCount + 1; i++) {
         items.push(Number(Object.values(row)[i]));
       }
       dataToDisplay.push(items);
     }
+    console.log("data:" + dataToDisplay);
 
+    // generate chart
     if (this.c3ChartElement) {
       this.c3ChartElement.destroy();
     }
