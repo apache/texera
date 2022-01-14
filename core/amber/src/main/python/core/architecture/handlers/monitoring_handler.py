8e4966ccaf786673c75e8869c0ea5218074cd7b1
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2022-01-14 15:01:55 -0800

    hardcode to 0 and add comments

--- core/amber/src/main/python/core/architecture/handlers/monitoring_handler.py
+++ core/amber/src/main/python/core/architecture/handlers/monitoring_handler.py
@@ -11,8 +11,13 @@ class MonitoringHandler(Handler):
             return SelfWorkloadMetrics(
                 context.input_queue.sub_size(),
                 context.input_queue.main_size(),
-                -1,  # TODO: dataInputPort.getStashedMessageCount()
-                -1  # TODO: controlInputPort.getStashedMessageCount()
+                # TODO: Given that following information are retained at java side which is
+                #   hard to fetch, and the fact that those two information are not
+                #   urgently used, we decided to hard code them to 0 for now.
+                #   Later if we have the OrderingEnforcer is ported to Python side, we can
+                #   collect those information.
+                0,  # dataInputPort.getStashedMessageCount()
+                0   # controlInputPort.getStashedMessageCount()
 
             )
         except:
