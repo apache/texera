faf8d845c7d333872b0a0f867f29916a83b7fc9f
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2022-01-26 22:03:29 -0800

    add test

--- core/amber/src/main/python/core/models/test_tuple.py
+++ core/amber/src/main/python/core/models/test_tuple.py
@@ -1,5 +1,6 @@
 import pandas
 import pytest
+from copy import deepcopy
 
 from core.models import Tuple
 
@@ -7,7 +8,7 @@ from core.models import Tuple
 class TestTuple:
     @pytest.fixture
     def target_tuple(self):
-        return Tuple(pandas.Series({"x": 1, "y": "a"}))
+        return Tuple({"x": 1, "y": "a"})
 
     def test_tuple_from_list(self, target_tuple):
         assert Tuple([("x", 1), ("y", "a")]) == target_tuple
@@ -26,6 +27,28 @@ class TestTuple:
 
     def test_tuple_as_series(self, target_tuple):
         assert (target_tuple.as_series() == pandas.Series({"x": 1, "y": "a"})).all()
+
+    def test_tuple_get_fields(self, target_tuple):
+        assert target_tuple.get_fields() == (1, "a")
+
+    def test_tuple_get_field_names(self, target_tuple):
+        assert target_tuple.get_field_names() == ("x", "y")
+
+    def test_tuple_get_item(self, target_tuple):
+        assert target_tuple['x'] == 1
+        assert target_tuple['y'] == 'a'
+        assert target_tuple[0] == 1
+        assert target_tuple[1] == 'a'
+
+    def test_tuple_set_item(self, target_tuple):
+        target_tuple['x'] = 3
+        assert target_tuple['x'] == 3
+        assert target_tuple['y'] == 'a'
+        assert target_tuple[0] == 3
+        assert target_tuple[1] == 'a'
+        target_tuple['z'] = 1.1
+        assert target_tuple[2] == 1.1
+        assert target_tuple['z'] == 1.1
 
     def test_tuple_str(self, target_tuple):
         assert str(target_tuple) == "Tuple['x': 1, 'y': 'a']"
@@ -56,3 +79,20 @@ class TestTuple:
             Tuple([1])
         with pytest.raises(TypeError):
             Tuple([None])
+
+    def test_tuple_lazy_get_from_arrow(self):
+        class MockArrow:
+            def __init__(self, name):
+                self.name = name
+
+            def as_py(self):
+                return chr(96 + int(self.name))
+
+        def field_accessor(field_name):
+            return MockArrow(field_name)
+
+        chr_tuple = Tuple({'1': 'a', '3': 'c'})
+        tuple_ = Tuple({'1': field_accessor, "3": field_accessor})
+        assert tuple_ == Tuple({'1': 'a', '3': 'c'})
+        tuple_ = Tuple({'1': field_accessor, "3": field_accessor})
+        assert deepcopy(tuple_) == chr_tuple
