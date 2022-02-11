7042a330a84a3ecf237bd035b5a6c4e3b187e1a0
Shengquan Ni <13672781+shengquan-ni@users.noreply.github.com>
2022-02-10 18:17:59 -0800

    apply fix

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/common/AmberUtils.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/common/AmberUtils.scala
@@ -1,6 +1,6 @@
 package edu.uci.ics.amber.engine.common
 
-import akka.actor.{ActorSystem, DeadLetter, Props}
+import akka.actor.{ActorSystem, Address, DeadLetter, Props}
 import com.typesafe.config.{Config, ConfigFactory}
 import edu.uci.ics.amber.clustering.ClusterListener
 import edu.uci.ics.amber.engine.architecture.messaginglayer.DeadLetterMonitorActor
@@ -35,19 +35,15 @@ object AmberUtils {
         akka.cluster.seed-nodes = [ "akka://Amber@$localIpAddress:2552" ]
         """)
       .withFallback(akkaConfig)
-
-    val system = ActorSystem("Amber", masterConfig)
-    system.actorOf(Props[ClusterListener], "cluster-info")
-    val deadLetterMonitorActor =
-      system.actorOf(Props[DeadLetterMonitorActor], name = "dead-letter-monitor-actor")
-    system.eventStream.subscribe(deadLetterMonitorActor, classOf[DeadLetter])
-
-    system
+    Constants.masterNodeAddr = mkMasterAddress(localIpAddress)
+    initAmberSystem(masterConfig)
   }
 
   def akkaConfig: Config = ConfigFactory.load("cluster").withFallback(amberConfig)
 
   def amberConfig: Config = ConfigFactory.load()
+
+  def mkMasterAddress(addr:String):Address = Address("akka","Amber",addr,2552)
 
   def startActorWorker(mainNodeAddress: Option[String]): ActorSystem = {
     val addr = mainNodeAddress.getOrElse("localhost")
@@ -59,12 +55,17 @@ object AmberUtils {
         akka.cluster.seed-nodes = [ "akka://Amber@$addr:2552" ]
         """)
       .withFallback(akkaConfig)
-    val system = ActorSystem("Amber", workerConfig)
+    Constants.masterNodeAddr = mkMasterAddress(addr)
+    initAmberSystem(workerConfig)
+  }
+
+
+  def initAmberSystem(actorSystemConf:Config): ActorSystem ={
+    val system = ActorSystem("Amber", actorSystemConf)
     system.actorOf(Props[ClusterListener], "cluster-info")
     val deadLetterMonitorActor =
       system.actorOf(Props[DeadLetterMonitorActor], name = "dead-letter-monitor-actor")
     system.eventStream.subscribe(deadLetterMonitorActor, classOf[DeadLetter])
-    Constants.masterNodeAddr = Option(addr)
     system
   }
 }
