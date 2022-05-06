28851ba98e416d79a3fdc0e45bc383634eed4d00
jessieeeovo <jessieyao0602@gmail.com>
2022-05-05 19:25:39 -0700

    add comments; revise the logic for workflow difference calculation

--- core/new-gui/src/app/dashboard/service/workflow-version/workflow-version.service.ts
+++ core/new-gui/src/app/dashboard/service/workflow-version/workflow-version.service.ts
@@ -8,12 +8,19 @@ import { isEqual, reduce } from "lodash";
 import { Breakpoint, OperatorLink, OperatorPredicate, Point } from "src/app/workspace/types/workflow-common.interface";
 
 export const DISPLAY_WORKFLOW_VERIONS_EVENT = "display_workflow_versions_event";
-const TYPES_FOR_WORKFLOW_DIFF_CALC = ["operators"];
 
-type WorkflowContents = keyof WorkflowContent;
-type Elements = Breakpoint | OperatorLink | OperatorPredicate | Point;
+type WorkflowContentKeys = keyof WorkflowContent;
+type Element = Breakpoint | OperatorLink | OperatorPredicate | Point;
 type DifferentOpIDsList = {
   [key in "modified" | "added"]: string[];
+};
+
+// only element types specified in this list are consider when calculating the difference between workflows
+const ELEMENT_TYPES_IN_WORKFLOW_DIFF_CALC: Partial<WorkflowContentKeys>[] = ["operators"];
+// it maps name of the element type to its ID field name used in WorkflowContent
+const ID_FILED_FOR_ELEMENTS_CONFIG: { [key: string]: string } = {
+  operators: "operatorID",
+  commentBoxes: "commentBoxID"
 };
 
 @Injectable({
@@ -54,7 +61,7 @@ export class WorkflowVersionService {
     // we need to display the version on the paper but keep the original workflow in the background
     this.workflowActionService.setTempWorkflow(this.workflowActionService.getWorkflow());
     // get the list of IDs of different elements when comparing displaying to the editing version
-    this.differentOpIDsList = this.getDifference(this.workflowActionService.getWorkflow(), workflow);
+    this.differentOpIDsList = this.getDifference(this.workflowActionService.getWorkflow().content, workflow.content);
     // disable persist to DB because it is read only
     this.workflowPersistService.setWorkflowPersistFlag(false);
     // disable the undoredo service because reloading the workflow is considered an action
@@ -69,28 +76,11 @@ export class WorkflowVersionService {
     this.highlightOpVersionDiff(this.differentOpIDsList);
   }
 
-  public getPositionDifference(v1: Workflow, v2: Workflow) {
-    var p1 = v1.content.operatorPositions;
-    var p2 = v2.content.operatorPositions;
-    var difference = reduce(
-      p2,
-      function (result: string[], value, key) {
-        return p1[key as keyof typeof p1] == undefined || isEqual(value, p1[key as keyof typeof p1])
-          ? result
-          : result.concat(key);
-      },
-      []
-    );
-    return difference;
-  }
-
   public highlightOpVersionDiff(differentOpIDsList: DifferentOpIDsList) {
     for (var id of differentOpIDsList.modified) {
-      console.log(id);
       this.highlighOpBoundary(id, "255,118,20,0.5");
     }
     for (var id of differentOpIDsList.added) {
-      console.log(id);
       this.highlighOpBoundary(id, "75,255,20,0.5");
     }
   }
@@ -103,43 +93,47 @@ export class WorkflowVersionService {
       .attr("rect.boundary/fill", "rgba(" + color + ")");
   }
 
-  public getDifference(v1: Workflow, v2: Workflow) {
-    var diff = { added: [], modified: [] };
-    var c1 = v1.content;
-    var c2 = v2.content;
-    var differentTypes = reduce(
-      c2,
-      function (result: string[], value, key) {
-        return isEqual(value, c1[key as WorkflowContents]) ? result : result.concat(key);
-      },
-      []
-    );
-    for (var type of differentTypes) {
-      if (TYPES_FOR_WORKFLOW_DIFF_CALC.includes(type)) {
-        let getIDList = function (element: Elements, index: number) {
-          var id = "";
-          if (type.substring(type.length - 2, type.length) === "es") {
-            id = type.substring(0, type.length - 2) + "ID";
-          } else {
-            id = type.substring(0, type.length - 1) + "ID";
-          }
-          return { id: element[id as keyof Elements], index: index };
-        };
-        var l1 = c1[type as WorkflowContents] as Elements[];
-        var IDList1 = l1.map(getIDList);
-        var l2 = c2[type as WorkflowContents] as Elements[];
-        var IDList2 = l2.map(getIDList);
-        console.log(l1);
-        diff.added = IDList2.filter(x => !IDList1.map(y => y.id).includes(x.id)).map(x => x.id);
-        diff.modified = IDList2.filter(x => IDList1.map(y => y.id).includes(x.id))
-          .filter(function (x) {
-            var i1 = IDList1.filter(y => y.id == x.id)[0].index;
-            return !isEqual(l1[i1], l2[x.index]);
-          })
-          .map(x => x.id);
+  // TODO: the logic of the function will be refined later
+  public getDifference(workflowContent1: WorkflowContent, workflowContent2: WorkflowContent) {
+    var difference: DifferentOpIDsList = { added: [], modified: [] };
+    // get a list of element types that are changed between versions
+    var eleTypeWithDiffList: WorkflowContentKeys[] = [];
+    for (var eleType of ELEMENT_TYPES_IN_WORKFLOW_DIFF_CALC) {
+      if (!isEqual(workflowContent1[eleType], workflowContent2[eleType])) {
+        eleTypeWithDiffList.push(eleType);
       }
     }
-    return diff;
+
+    for (var eleType of eleTypeWithDiffList) {
+      if (ELEMENT_TYPES_IN_WORKFLOW_DIFF_CALC.includes(eleType)) {
+        // return an object with key: ID of the element, value: detailed content of the element
+        let geteleIDMap = function (workflowContent: WorkflowContent, eleType: WorkflowContentKeys) {
+          var elements = workflowContent[eleType] as Element[];
+          var eleIDtoContentMap: { [key: string]: Element } = {};
+          for (var element of elements) {
+            eleIDtoContentMap[element[ID_FILED_FOR_ELEMENTS_CONFIG[eleType] as keyof Element]] = element;
+          }
+          return eleIDtoContentMap;
+        };
+
+        var eleIDtoContentMap1 = geteleIDMap(workflowContent1, eleType);
+        var eleIDtoContentMap2 = geteleIDMap(workflowContent2, eleType);
+
+        for (var eleID of Object.keys(eleIDtoContentMap2)) {
+          if (!Object.keys(eleIDtoContentMap1).includes(eleID)) {
+            // there is an addition if the element ID exist in historical but not current workflow version
+            difference.added.push(eleID);
+          } else {
+            // there might be a modification if the element ID exist in both historical and current workflow versions
+            if (!isEqual(eleIDtoContentMap1[eleID], eleIDtoContentMap2[eleID])) {
+              // if the contents in two workflow versions are different for the same element ID
+              difference.modified.push(eleID);
+            }
+          }
+        }
+      }
+    }
+    return difference;
   }
 
   public revertToVersion() {
@@ -174,7 +168,11 @@ export class WorkflowVersionService {
   }
 
   public unhighlightOpVersionDiff(differentOpIDsList: DifferentOpIDsList) {
-    for (var id of Object.values(differentOpIDsList).reduce((accumulator, value) => accumulator.concat(value), [])) {
+    var differentOpIDs: string[] = [];
+    for (var opIDs in Object.values(differentOpIDsList)) {
+      differentOpIDs.concat(opIDs);
+    }
+    for (var id of differentOpIDs) {
       this.highlighOpBoundary(id, "0,0,0,0");
     }
   }
