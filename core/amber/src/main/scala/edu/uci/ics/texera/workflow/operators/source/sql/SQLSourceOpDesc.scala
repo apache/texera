735283c2d7014f37de1c5108ea80ba31a7af860e
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2022-05-13 15:23:23 -0700

    revert changes

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/source/sql/SQLSourceOpDesc.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/source/sql/SQLSourceOpDesc.scala
@@ -157,8 +157,9 @@ abstract class SQLSourceOpDesc extends SourceOperatorDescriptor {
           case Types.BIT | // -7 Types.BIT
               Types.BOOLEAN => // 16 Types.BOOLEAN
             schemaBuilder.add(new Attribute(columnName, AttributeType.BOOLEAN))
-          case Types.BINARY | //-2 Types.BINARY
-              Types.DATE | //91 Types.DATE
+          case Types.BINARY => //-2 Types.BINARY
+            schemaBuilder.add(new Attribute(columnName, AttributeType.BINARY))
+          case Types.DATE | //91 Types.DATE
               Types.TIME | //92 Types.TIME
               Types.LONGVARCHAR | //-1 Types.LONGVARCHAR
               Types.CHAR | //1 Types.CHAR
