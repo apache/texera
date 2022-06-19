a0f29cf17fd8d0788c90c515482e58d7fba2a774
Avinash Kumar <3867317+avinash0161@users.noreply.github.com>
2022-06-18 17:39:37 -0700

    sort reshape

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/sortPartitions/SortPartitionOpExec.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/sortPartitions/SortPartitionOpExec.scala
@@ -1,5 +1,10 @@
 package edu.uci.ics.texera.workflow.operators.sortPartitions
 
+import com.twitter.util.Future
+import edu.uci.ics.amber.engine.architecture.worker.PauseManager
+import edu.uci.ics.amber.engine.architecture.worker.promisehandlers.AcceptMutableStateHandler.AcceptMutableState
+import edu.uci.ics.amber.engine.common.amberexception.WorkflowRuntimeException
+import edu.uci.ics.amber.engine.common.rpc.AsyncRPCClient
 import edu.uci.ics.amber.engine.common.{Constants, InputExhausted}
 import edu.uci.ics.amber.engine.common.virtualidentity.{ActorVirtualIdentity, LinkIdentity}
 import edu.uci.ics.texera.workflow.common.operators.OperatorExecutor
@@ -9,23 +14,24 @@ import edu.uci.ics.texera.workflow.common.tuple.schema.{AttributeType, OperatorS
 import scala.collection.mutable.ArrayBuffer
 
 class SortPartitionOpExec(
-                           sortAttributeName: String,
-                           operatorSchemaInfo: OperatorSchemaInfo,
-                           localIdx: Int,
-                           domainMin: Long,
-                           domainMax: Long,
-                           numberOfWorkers: Int
-                         ) extends OperatorExecutor {
+    sortAttributeName: String,
+    operatorSchemaInfo: OperatorSchemaInfo,
+    localIdx: Int,
+    domainMin: Long,
+    domainMax: Long,
+    numberOfWorkers: Int
+) extends OperatorExecutor {
 
   var ownTuples: ArrayBuffer[Tuple] = _
 
   // used by helper workers to store tuples of skewed workers
   var skewedWorkerTuples: ArrayBuffer[Tuple] = _
   var skewedWorkerIdentity: ActorVirtualIdentity = null
+  val helperToSkewedBatchSize = 4000
 
   // used by skewed worker
   var waitingForTuplesFromHelper: Boolean = false
-  var numOfTuplesReceivedFromHelper: Long = 0
+  var numOfBatchesReceivedFromHelper: Int = 0
   var helperWorkerIdentity: ActorVirtualIdentity = null
 
   def getWorkerIdxForKey(key: Long): Int = {
@@ -39,10 +45,72 @@ class SortPartitionOpExec(
     ((key - domainMin) / keysPerReceiver).toInt
   }
 
+  /**
+    * Used by skewed worker to accept tuples from helper worker. When all tuples have been received, this
+    * function returns true.
+    *
+    * Returns: Whether all tuples have been received
+    */
+  def mergeIntoStoredTuplesList(tuples: ArrayBuffer[Tuple], totalMessagesToExpect: Int): Boolean = {
+    try {
+      ownTuples.appendAll(tuples)
+      numOfBatchesReceivedFromHelper += 1
+      if (numOfBatchesReceivedFromHelper == totalMessagesToExpect) {
+        waitingForTuplesFromHelper = false
+        true
+      } else {
+        false
+      }
+    } catch {
+      case exception: Exception =>
+        false
+    }
+  }
+
+  def getListsToSendToSkewed(): ArrayBuffer[ArrayBuffer[Tuple]] = {
+    val sendingLists = new ArrayBuffer[ArrayBuffer[Tuple]]
+    var count = 1
+    var curr = new ArrayBuffer[Tuple]
+    for (t <- skewedWorkerTuples) {
+      curr.append(t)
+      if (count % helperToSkewedBatchSize == 0) {
+        sendingLists.append(curr)
+        curr = new ArrayBuffer[Tuple]
+      }
+      count += 1
+    }
+    if (!curr.isEmpty) sendingLists.append(curr)
+    sendingLists
+  }
+
+  def sendTuplesToSkewedWorker(asyncRPCClient: AsyncRPCClient): Unit = {
+    val skewedFutures = new ArrayBuffer[Future[Boolean]]()
+    val listsToSend = getListsToSendToSkewed()
+    listsToSend.foreach(list => {
+      skewedFutures.append(
+        asyncRPCClient.send(
+          AcceptMutableState(list, listsToSend.size),
+          skewedWorkerIdentity
+        )
+      )
+    })
+    Future
+      .collect(skewedFutures)
+      .onSuccess(s => {
+        if (s.contains(false)) {
+          throw new WorkflowRuntimeException("Error in sending tuples to skewed worker of Sort")
+        }
+      })
+  }
+
+  def sortTuples(): Iterator[Tuple] = ownTuples.sortWith(compareTuples).iterator
+
   override def processTexeraTuple(
-                                   tuple: Either[Tuple, InputExhausted],
-                                   input: LinkIdentity
-                                 ): Iterator[Tuple] = {
+      tuple: Either[Tuple, InputExhausted],
+      input: LinkIdentity,
+      pauseManager: PauseManager,
+      asyncRPCClient: AsyncRPCClient
+  ): Iterator[Tuple] = {
     tuple match {
       case Left(t) =>
         if (Constants.reshapeSkewHandlingEnabled) {
@@ -74,18 +142,25 @@ class SortPartitionOpExec(
       case Right(_) =>
         if (helperWorkerIdentity == null && skewedWorkerIdentity == null) {
           // this worker is neither the skewed worker nor the helper worker
-          ownTuples.sortWith(compareTuples).iterator
+          sortTuples()
         } else if (helperWorkerIdentity != null) {
           // this worker is the skewed worker.
           if (!waitingForTuplesFromHelper) {
             // It has received the state and can output the results
-            ownTuples.sortWith(compareTuples).iterator
+            sortTuples()
           } else {
             // It will pause its execution here. The execution will be resumed once the state is received
             // from the helper worker
+            pauseManager.pausedByOperatorLogic = true
+            Iterator()
           }
-        } else {
+        } else if (skewedWorkerIdentity != null) {
           // this worker is the helper worker. It needs to send state to the skewed worker.
+          sendTuplesToSkewedWorker(asyncRPCClient)
+          sortTuples()
+        } else {
+          // shouldn't arrive here
+          Iterator()
         }
 
     }
