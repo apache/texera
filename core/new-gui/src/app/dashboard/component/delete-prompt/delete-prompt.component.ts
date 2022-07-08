2794ec0db2d4e43a506b0b8926eaaba76cedb6d4
Ethan Deng <ethand.2091@gmail.com>
2022-07-08 11:20:50 -0700

    added new component(modal) dedicated to deletion prompt

---
+++ core/new-gui/src/app/dashboard/component/delete-prompt/delete-prompt.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+
+@Component({
+  selector: 'delete-prompt',
+  templateUrl: './delete-prompt.component.html',
+  styleUrls: ['./delete-prompt.component.css']
+})
+export class DeletePromptComponent {
+  @Input() deletionType!: string;
+  @Input() deletionName!: string;
+
+  constructor(public activeModal: NgbActiveModal) {}
+
+  /**
+   * deleteSavedWorkflow sends the user
+   * confirm to the main component. It does not call any method in service.
+   */
+  public delete(): void {
+    this.activeModal.close(true);
+  }
+
+}
