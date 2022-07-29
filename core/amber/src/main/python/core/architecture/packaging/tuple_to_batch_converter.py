0234d5140abae51ab2bd59aa2a34365906408e07
PlsBeHumble <zhuoyul4@uci.edu>
2022-07-29 10:43:03 -0700

    geo fixed execution tab
    Finished task:
    1. make sorting icon into one, and when you click it will change between ascending and descending.
    2. turn user name from pure text to avatar
    3. limit the name description when it is too long
    Task did finish:
    1. failed to add the scrollbar for the execution table

--- core/amber/src/main/python/core/architecture/packaging/tuple_to_batch_converter.py
+++ core/amber/src/main/python/core/architecture/packaging/tuple_to_batch_converter.py
@@ -29,9 +29,7 @@ from proto.edu.uci.ics.amber.engine.common import ActorVirtualIdentity, LinkIden
 
 
 class TupleToBatchConverter:
-    def __init__(
-        self,
-    ):
+    def __init__(self,):
         self._partitioners: OrderedDict[LinkIdentity, Partitioning] = OrderedDict()
         self._partitioning_to_partitioner: dict[
             type(Partitioning), type(Partitioner)
