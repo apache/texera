c0b500f4423636a03f4b52ea4a124bd4550c96e3
Liqin Ye <liqiny@uci.edu>
2022-08-29 10:56:56 -0700

    shorten and right align operator version displayed on frontend

--- core/new-gui/src/app/workspace/component/property-editor/operator-property-edit-frame/operator-property-edit-frame.component.ts
+++ core/new-gui/src/app/workspace/component/property-editor/operator-property-edit-frame/operator-property-edit-frame.component.ts
@@ -186,7 +186,7 @@ export class OperatorPropertyEditFrameComponent implements OnInit, OnChanges, On
     const operator = this.workflowActionService.getTexeraGraph().getOperator(this.currentOperatorId);
     // set the operator data needed
     const currentOperatorSchema = this.dynamicSchemaService.getDynamicSchema(this.currentOperatorId);
-    this.operatorVersion = currentOperatorSchema.operatorVersion;
+    this.operatorVersion = currentOperatorSchema.operatorVersion.slice(0, 9);
     this.setFormlyFormBinding(currentOperatorSchema.jsonSchema);
     this.formTitle = operator.customDisplayName ?? currentOperatorSchema.additionalMetadata.userFriendlyName;
 
