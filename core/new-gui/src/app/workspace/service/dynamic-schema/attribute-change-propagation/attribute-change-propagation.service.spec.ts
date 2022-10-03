1a3db6a0d25e0b82901d2c680c000eb64e5a1eb8
Ahei <aheicx@gmail.com>
2022-10-03 10:48:11 -0700

    Unit tests for AttributeChangePropagationService

---
+++ core/new-gui/src/app/workspace/service/dynamic-schema/attribute-change-propagation/attribute-change-propagation.service.spec.ts
@@ -0,0 +1,152 @@
+import { HttpClient } from "@angular/common/http";
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { async, fakeAsync, inject, TestBed, tick, waitForAsync } from "@angular/core/testing";
+import { environment } from "../../../../../environments/environment";
+import { JointUIService } from "../../joint-ui/joint-ui.service";
+import { OperatorMetadataService } from "../../operator-metadata/operator-metadata.service";
+import {
+  mockPoint,
+  mockScanPredicate,
+  mockScanSentimentLink,
+  mockSentimentPredicate,
+} from "../../workflow-graph/model/mock-workflow-data";
+import { StubOperatorMetadataService } from "../../operator-metadata/stub-operator-metadata.service";
+import { UndoRedoService } from "../../undo-redo/undo-redo.service";
+import { WorkflowActionService } from "../../workflow-graph/model/workflow-action.service";
+import { WorkflowUtilService } from "../../workflow-graph/util/workflow-util.service";
+import { DynamicSchemaService } from "../dynamic-schema.service";
+import { AttributeChangePropagationService } from "./attribute-change-propagation.service";
+import { OperatorSchema } from "src/app/workspace/types/operator-schema.interface";
+import { OperatorLink, OperatorPredicate } from "src/app/workspace/types/workflow-common.interface";
+import { 
+  mockSchemaPropagationResponse1, 
+  mockSchemaPropagationResponse2, 
+  mockSchemaPropagationResponse3, 
+  mockSentimentOperatorA, 
+  mockSentimentOperatorB, 
+  mockLinkAtoB,
+  mockLinkBtoC,
+  mockSentimentOperatorC,
+  mockSchemaPropagationResponse4,
+  mockSchemaPropagationResponse5
+} from "./mock-attribute-change-propagation.data";
+import { mockNlpSentimentSchema } from "../../operator-metadata/mock-operator-metadata.data";
+import { AppSettings } from "src/app/common/app-setting";
+import { SchemaPropagationService, SCHEMA_PROPAGATION_DEBOUNCE_TIME_MS, SCHEMA_PROPAGATION_ENDPOINT } from "../schema-propagation/schema-propagation.service";
+
+
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+describe("AttributeChangePropagationService", () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: OperatorMetadataService,
+          useClass: StubOperatorMetadataService,
+        },
+        WorkflowActionService,
+        DynamicSchemaService,
+        AttributeChangePropagationService,
+        SchemaPropagationService,
+      ],
+    });
+
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    environment.schemaPropagationEnabled = true;
+  });
+
+  it("should be created", inject([AttributeChangePropagationService], (service: AttributeChangePropagationService) => {
+    expect(service).toBeTruthy();
+  }));
+
+  it("should propagate new attribute name when atteibute is renamed", fakeAsync(() => {
+    const workflowActionService: WorkflowActionService = TestBed.inject(WorkflowActionService);
+    const schemaPropagationService: SchemaPropagationService = TestBed.inject(SchemaPropagationService);
+    const attributeChangePropagationService: AttributeChangePropagationService = TestBed.inject(AttributeChangePropagationService);
+
+    workflowActionService.addOperator(mockSentimentOperatorA, mockPoint);
+    workflowActionService.addOperator(mockSentimentOperatorB, mockPoint);
+    workflowActionService.addLink(mockLinkAtoB);
+
+    const req1 = httpTestingController.expectOne(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
+    expect(req1.request.method === "POST");
+    expect(req1.request.url).toEqual(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
+    req1.flush(mockSchemaPropagationResponse1);
+    httpTestingController.verify();
+
+    // trigger inputSchemaChangeStream
+    workflowActionService.setOperatorProperty(mockSentimentOperatorA.operatorID, { testAttr: "test" });
+    tick(SCHEMA_PROPAGATION_DEBOUNCE_TIME_MS);
+    const req2 = httpTestingController.expectOne(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
+    expect(req2.request.method === "POST");
+    expect(req2.request.url).toEqual(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
+    req2.flush(mockSchemaPropagationResponse2);
+    httpTestingController.verify();
+
+    expect(workflowActionService.getTexeraGraph().getOperator(mockSentimentOperatorB.operatorID).operatorProperties.attribute).toEqual("user_display_name")
+    
+  }));
+
+  it("should delete attribute in succeeding operators when attribute is deleted", fakeAsync(() => {
+    const workflowActionService: WorkflowActionService = TestBed.inject(WorkflowActionService);
+    const schemaPropagationService: SchemaPropagationService = TestBed.inject(SchemaPropagationService);
+    const attributeChangePropagationService: AttributeChangePropagationService = TestBed.inject(AttributeChangePropagationService);
+
+    workflowActionService.addOperator(mockSentimentOperatorA, mockPoint);
+    workflowActionService.addOperator(mockSentimentOperatorB, mockPoint);
+    workflowActionService.addLink(mockLinkAtoB);
+
+    const req1 = httpTestingController.expectOne(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
+    expect(req1.request.method === "POST");
+    expect(req1.request.url).toEqual(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
+    req1.flush(mockSchemaPropagationResponse1);
+    httpTestingController.verify();
+
+    // trigger inputSchemaChangeStream
+    workflowActionService.setOperatorProperty(mockSentimentOperatorA.operatorID, { testAttr: "test" });
+    tick(SCHEMA_PROPAGATION_DEBOUNCE_TIME_MS);
+    const req2 = httpTestingController.expectOne(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
+    expect(req2.request.method === "POST");
+    expect(req2.request.url).toEqual(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
+    req2.flush(mockSchemaPropagationResponse3);
+    httpTestingController.verify();
+
+    expect(workflowActionService.getTexeraGraph().getOperator(mockSentimentOperatorB.operatorID).operatorProperties.attribute).toEqual("");
+  }));
+
+  it("should propagate new attribute name in three consecutive operators", fakeAsync(() => {
+    const workflowActionService: WorkflowActionService = TestBed.inject(WorkflowActionService);
+    const schemaPropagationService: SchemaPropagationService = TestBed.inject(SchemaPropagationService);
+    const attributeChangePropagationService: AttributeChangePropagationService = TestBed.inject(AttributeChangePropagationService);
+
+    workflowActionService.addOperator(mockSentimentOperatorA, mockPoint);
+    workflowActionService.addOperator(mockSentimentOperatorB, mockPoint);
+    workflowActionService.addOperator(mockSentimentOperatorC, mockPoint);
+    workflowActionService.addLink(mockLinkAtoB);
+    httpTestingController.expectOne(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
+    workflowActionService.addLink(mockLinkBtoC);
+
+    const req1 = httpTestingController.expectOne(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
+    expect(req1.request.method === "POST");
+    expect(req1.request.url).toEqual(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
+    req1.flush(mockSchemaPropagationResponse4);
+    httpTestingController.verify();
+
+    // trigger inputSchemaChangeStream
+    workflowActionService.setOperatorProperty(mockSentimentOperatorA.operatorID, { testAttr: "test" });
+    tick(SCHEMA_PROPAGATION_DEBOUNCE_TIME_MS);
+    const req2 = httpTestingController.expectOne(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
+    expect(req2.request.method === "POST");
+    expect(req2.request.url).toEqual(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
+    req2.flush(mockSchemaPropagationResponse5);
+    httpTestingController.verify();
+
+    expect(workflowActionService.getTexeraGraph().getOperator(mockSentimentOperatorC.operatorID).operatorProperties.attribute).toEqual("screen_display_time");
+  }));
+
+});
