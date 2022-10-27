f03feaed414e23c13b50092b36d396f30caa7d55
Shengquan Ni <13672781+shengquan-ni@users.noreply.github.com>
2022-10-26 22:18:14 -0700

    Update MongoDBStorage.scala

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/sink/storage/MongoDBStorage.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/sink/storage/MongoDBStorage.scala
@@ -15,6 +15,10 @@ import collection.JavaConverters._
 
 class MongoDBStorage(id: String, schema: Schema) extends SinkStorageReader {
 
+  schema.getAttributeNames.stream.forEach(name =>
+    assert(!name.matches(".*[\\$\\.].*"), s"illegal attribute name '$name' for mongo DB")
+  )
+
   val url: String = AmberUtils.amberConfig.getString("storage.mongodb.url")
   val databaseName: String = AmberUtils.amberConfig.getString("storage.mongodb.database")
   val client: MongoClient = MongoClients.create(url)
