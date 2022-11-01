b4c7d040012a4405724508458365287a1623ff9d
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2022-11-01 11:38:34 -0700

    fix format

--- core/new-gui/src/app/workspace/component/input-autocomplete/input-autocomplete.component.ts
+++ core/new-gui/src/app/workspace/component/input-autocomplete/input-autocomplete.component.ts
@@ -1,10 +1,10 @@
-import {Component} from "@angular/core";
-import {FieldType} from "@ngx-formly/core";
-import {UntilDestroy, untilDestroyed} from "@ngneat/until-destroy";
-import {UserFileService} from "src/app/dashboard/service/user-file/user-file.service";
-import {FormControl} from "@angular/forms";
-import {debounceTime} from "rxjs/operators";
-import {map} from "rxjs";
+import { Component } from "@angular/core";
+import { FieldType } from "@ngx-formly/core";
+import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
+import { UserFileService } from "src/app/dashboard/service/user-file/user-file.service";
+import { FormControl } from "@angular/forms";
+import { debounceTime } from "rxjs/operators";
+import { map } from "rxjs";
 
 @UntilDestroy()
 @Component({
@@ -19,7 +19,6 @@ import {map} from "rxjs";
  * need to upgrade formly to v6 to properly fix this issue.
  */
 export class InputAutoCompleteComponent extends FieldType<any> {
-
   // the autocomplete selection list
   public suggestions: string[] = [];
 
@@ -56,18 +55,15 @@ export class InputAutoCompleteComponent extends FieldType<any> {
         .pipe(debounceTime(300))
         .pipe(untilDestroyed(this))
         .subscribe(suggestedFiles => {
-          if (!this.equalsIgnoreOrder(this.suggestions, suggestedFiles))
-            this.suggestions = [...suggestedFiles];
+          if (!this.equalsIgnoreOrder(this.suggestions, suggestedFiles)) this.suggestions = [...suggestedFiles];
         });
     } else {
       // no valid input, perform full scan
       this.userFileService
         .retrieveDashboardUserFileEntryList()
-        .pipe(
-          map(list => list.map(x => x.ownerName + "/" + x.file.name))
-        ).pipe(untilDestroyed(this)).subscribe(
-        allAccessibleFiles => this.suggestions = allAccessibleFiles
-      );
+        .pipe(map(list => list.map(x => x.ownerName + "/" + x.file.name)))
+        .pipe(untilDestroyed(this))
+        .subscribe(allAccessibleFiles => (this.suggestions = allAccessibleFiles));
     }
   }
 }
