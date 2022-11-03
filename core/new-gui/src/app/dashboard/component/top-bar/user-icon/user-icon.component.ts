3e942ddd6bf1baa87a81535e7ee2d181017a1a14
linxinyuan <linxinyuan@gmail.com>
2022-11-02 19:52:51 -0700

    init

--- core/new-gui/src/app/dashboard/component/top-bar/user-icon/user-icon.component.ts
+++ core/new-gui/src/app/dashboard/component/top-bar/user-icon/user-icon.component.ts
@@ -1,15 +1,11 @@
 import { Component } from "@angular/core";
 import { UserService } from "../../../../common/service/user/user.service";
 import { User } from "../../../../common/type/user";
-import { UserLoginModalComponent } from "./user-login/user-login-modal.component";
 import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
 import { NzModalService } from "ng-zorro-antd/modal";
 
-/**
- * UserIconComponent is used to control user system on the top right corner
- * It includes the button for login/registration/logout
- * It also includes what is shown on the top right
- */
+
+
 @UntilDestroy()
 @Component({
   selector: "texera-user-icon",
@@ -33,19 +29,16 @@ export class UserIconComponent {
     this.userService.logout();
   }
 
-  /**
-   * handle the event when user click on the login (sign in) button
-   */
-  public onClickLogin(): void {
-    this.openLoginComponent();
-  }
 
   /**
-   * This method will open the login/register pop up
-   * It will switch to the tab based on the mode number given
-   * @param mode 0 indicates login and 1 indicates registration
+   * this method will retrieve a usable Google OAuth Instance first,
+   * with that available instance, get googleUsername and authorization code respectively,
+   * then sending the code to the backend
    */
-  private openLoginComponent(): void {
-    this.modalService.create({ nzContent: UserLoginModalComponent, nzOkText: null });
+  public googleLogin(): void {
+    this.userService
+      .googleLogin()
+      .pipe(untilDestroyed(this))
+      .subscribe();
   }
 }
