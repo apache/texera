877612b7f16f5ac26c99c663f7a58e1e1f794dc6
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2022-11-15 23:43:22 -0800

    record exception history

--- core/amber/src/main/python/core/architecture/managers/exception_manager.py
+++ core/amber/src/main/python/core/architecture/managers/exception_manager.py
@@ -3,13 +3,17 @@ from typing import Optional
 
 class ExceptionManager:
     def __init__(self):
-        self.exception = Optional[Exception]
+        self.exception: Optional[Exception] = None
+        self.exception_history = list()
 
     def set_exception(self, exception: Exception):
         self.exception = exception
+        self.exception_history.append(exception)
 
     def has_exception(self) -> bool:
         return self.exception is not None
 
     def get_exception(self):
-        return self.exception
+        exception = self.exception
+        self.exception = None
+        return exception
