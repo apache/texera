66b91e96bd5c3fbe55a6428e1078af3d6a2807f3
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2022-11-17 14:49:33 -0800

    update internal queue

--- core/amber/src/main/python/core/models/internal_queue.py
+++ core/amber/src/main/python/core/models/internal_queue.py
@@ -1,23 +1,36 @@
 from dataclasses import dataclass
+from typing import TypeVar
 
 from core.models.marker import Marker
 from core.models.payload import DataPayload
 from core.util.customized_queue.linked_blocking_multi_queue import (
-    LinkedBlockingMultiQueue,
-)
+    LinkedBlockingMultiQueue, )
 from core.util.customized_queue.queue_base import IQueue
 from proto.edu.uci.ics.amber.engine.common import ActorVirtualIdentity, ControlPayloadV2
 
+T = TypeVar("T")
 
-class InternalQueue(LinkedBlockingMultiQueue):
+
+class InternalQueue(IQueue):
     def __init__(self):
-        super().__init__(
-            {
-                IQueue.QueueControl: ("system", 0),
-                ControlElement: ("control", 1),
-                (DataElement, Marker): ("data", 2),
-            }
-        )
+        self._queue = LinkedBlockingMultiQueue()
+        self._queue._add_sub_queue("system", 0)
+        self._queue._add_sub_queue("control", 1)
+        self._queue._add_sub_queue("data", 2)
+
+    def is_empty(self, key=None) -> bool:
+        return self._queue.is_empty(key)
+
+    def get(self) -> T:
+        return self._queue.get()
+
+    def put(self, item):
+        if isinstance(item, DataElement):
+            self._queue.put("data", item)
+        elif isinstance(item, ControlElement):
+            self._queue.put("control", item)
+        else:
+            self._queue.put("system", item)
 
 
 @dataclass
