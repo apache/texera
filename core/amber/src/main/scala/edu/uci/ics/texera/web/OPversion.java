07e4de48d54e1d13cd5cb7a6cbb05c14ac9d552c
Ahei <aheicx@gmail.com>
2022-11-21 11:39:34 -0800

    Fixed path contain check

--- core/amber/src/main/scala/edu/uci/ics/texera/web/OPversion.java
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/OPversion.java
@@ -14,7 +14,7 @@ public class OPversion {
     static {
         try {
             Path path = Paths.get(currentPath);
-            if(currentPath.contains("core/amber")){
+            if(path.endsWith("core/amber/")){
                 path = Paths.get(currentPath).getParent().getParent();
             }
             git = Git.open(new File(path+"/.git"));
