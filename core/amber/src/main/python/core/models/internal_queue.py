e0d25c12b4b938e8b34991997a0747a19460d5a1
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2022-11-22 23:56:04 -0800

    add enum for queue identifier

--- core/amber/src/main/python/core/models/internal_queue.py
+++ core/amber/src/main/python/core/models/internal_queue.py
@@ -1,13 +1,13 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from enum import Enum
 from typing import TypeVar
 
 from core.models.marker import Marker
 from core.models.payload import DataPayload
 from core.util.customized_queue.linked_blocking_multi_queue import (
-    LinkedBlockingMultiQueue,
-)
+    LinkedBlockingMultiQueue, )
 from core.util.customized_queue.queue_base import IQueue, QueueElement
 from proto.edu.uci.ics.amber.engine.common import ActorVirtualIdentity, ControlPayloadV2
 
@@ -32,12 +32,18 @@ class ControlElement(InternalQueueElement):
 T = TypeVar("T", bound=InternalQueueElement)
 
 
+class QueueID(Enum):
+    SYSTEM = "system"
+    CONTROL = "control"
+    DATA = "data"
+
+
 class InternalQueue(IQueue):
     def __init__(self):
         self._queue = LinkedBlockingMultiQueue()
-        self._queue.add_sub_queue("system", 0)
-        self._queue.add_sub_queue("control", 1)
-        self._queue.add_sub_queue("data", 2)
+        self._queue.add_sub_queue(QueueID.SYSTEM.value, 0)
+        self._queue.add_sub_queue(QueueID.CONTROL.value, 1)
+        self._queue.add_sub_queue(QueueID.DATA.value, 2)
 
     def is_empty(self, key=None) -> bool:
         return self._queue.is_empty(key)
@@ -47,11 +53,11 @@ class InternalQueue(IQueue):
 
     def put(self, item: T) -> None:
         if isinstance(item, (DataElement, Marker)):
-            self._queue.put("data", item)
+            self._queue.put(QueueID.DATA.value, item)
         elif isinstance(item, ControlElement):
-            self._queue.put("control", item)
+            self._queue.put(QueueID.CONTROL.value, item)
         else:
-            self._queue.put("system", item)
+            self._queue.put(QueueID.SYSTEM.value, item)
 
     def __len__(self) -> int:
         return self._queue.size()
