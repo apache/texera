ce1e4452c01a014227c0244d9cdc245c5c4c99fa
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2022-12-01 03:23:50 -0800

    add comments and handle exception for replace_print

--- core/amber/src/main/python/core/util/console_message/replace_print.py
+++ core/amber/src/main/python/core/util/console_message/replace_print.py
@@ -28,7 +28,14 @@ class replace_print(ContextManager):
 
         self.buf = buf  # the provided buffer to write to
 
-    def __enter__(self):
+    def __enter__(self) -> None:
+        """
+        Enters the context, replace builtin.print function with a wrapped function.
+        Now we hard code the wrapped_print to output complete print result to the
+        given buffer.
+        :return:
+        """
+
         def wrapped_print(*args, **kwargs):
             # use StringIO to obtain the written complete string from the original
             # print function.
@@ -39,5 +46,19 @@ class replace_print(ContextManager):
 
         builtins.print = wrapped_print
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
+        """
+        Exits the context, revert the replacement to recover the original
+        builtin.print function.
+
+        It does not handle exception within the context, it simply raises it outside
+        the context.
+
+        :param exc_type: potential exception type.
+        :param exc_val: potential exception value.
+        :param exc_tb: potential exception traceback.
+        :return: bool, if no exception was raised, return True, otherwise,
+        return False.
+        """
         builtins.print = self.builtins_print
+        return exc_val is None
