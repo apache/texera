c4277e156f9dbe858142c557bd4a7d22805fdd3c
linxinyuan <linxinyuan@gmail.com>
2022-12-08 22:09:16 -0800

    comment for auth service

--- core/new-gui/src/app/common/service/auth-guard/auth-guard.service.ts
+++ core/new-gui/src/app/common/service/auth-guard/auth-guard.service.ts
@@ -1,6 +1,11 @@
 ﻿import { Injectable } from "@angular/core";
 import { Router, CanActivate } from "@angular/router";
 import { UserService } from "../user/user.service";
+
+/**
+ * AuthGuardService is a service can tell the router whether
+ * it should allow navigation to a requested route.
+ */
 @Injectable()
 export class AuthGuardService implements CanActivate {
   constructor(private userService: UserService, public router: Router) {}
