c868358680e6b48f8d32aea1041c81c8ec4c6a19
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2022-12-09 00:54:34 -0800

    make private variables

--- core/amber/src/main/python/core/architecture/managers/debug_manager.py
+++ core/amber/src/main/python/core/architecture/managers/debug_manager.py
@@ -6,25 +6,25 @@ from core.models.single_blocking_io import SingleBlockingIO
 
 class DebugManager:
     def __init__(self, condition: Condition):
-        self.debug_in = SingleBlockingIO(condition)
-        self.debug_out = SingleBlockingIO(condition)
-        self.debugger = Pdb(stdin=self.debug_in, stdout=self.debug_out, nosigint=True)
+        self._debug_in = SingleBlockingIO(condition)
+        self._debug_out = SingleBlockingIO(condition)
+        self.debugger = Pdb(stdin=self._debug_in, stdout=self._debug_out, nosigint=True)
 
         # Customized prompt, we can design our prompt for the debugger.
         self.debugger.prompt = ""
 
     def has_debug_command(self) -> bool:
-        return self.debug_in.value is not None
+        return self._debug_in.value is not None
 
     def has_debug_event(self) -> bool:
-        return self.debug_out.value is not None
+        return self._debug_out.value is not None
 
     def get_debug_event(self) -> str:
         """
         Blocking gets for the next debug event.
         :return str: the fetched event, in string format.
         """
-        return self.debug_out.readline()
+        return self._debug_out.readline()
 
     def put_debug_command(self, command: str) -> None:
         """
@@ -32,5 +32,5 @@ class DebugManager:
         :param command: the command to be put, in string format.
         :return:
         """
-        self.debug_in.write(command)
-        self.debug_in.flush()
+        self._debug_in.write(command)
+        self._debug_in.flush()
