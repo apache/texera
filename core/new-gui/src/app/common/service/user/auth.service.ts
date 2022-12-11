c49c92a0ab1a8a2053dab2135b6bbdfd29a24cb6
linxinyuan <linxinyuan@gmail.com>
2022-12-11 02:55:12 -0800

    fix format

--- core/new-gui/src/app/common/service/user/auth.service.ts
+++ core/new-gui/src/app/common/service/user/auth.service.ts
@@ -24,7 +24,7 @@ export const TOKEN_REFRESH_INTERVAL_IN_MIN = 15;
   providedIn: "root",
 })
 export class AuthService {
-  inviteOnly = environment.inviteOnly;
+  private inviteOnly = environment.inviteOnly;
   public static readonly LOGIN_ENDPOINT = "auth/login";
   public static readonly REFRESH_TOKEN = "auth/refresh";
   public static readonly REGISTER_ENDPOINT = "auth/register";
