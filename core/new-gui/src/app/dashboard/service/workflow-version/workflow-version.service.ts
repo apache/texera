apache/texera

commit 3304c2d7618fdf1f45c55cf1bf6ef93fcdcd2689

Yjs-based Shared Editing [3/4] Implement New Frontend Architecture based on Yjs (#1674)
## Introduction
This PR is the "meat" of this series of work. It implements lock free real-time shared editing based on the popular CRDT library, [Yjs](https://github.com/yjs/yjs). 

## Architectural Change
This is better explained with these two diagrams:
### Previous Design
![Texera Frontend-Before](https://user-images.githubusercontent.com/36582710/192446300-af572f8c-a999-41e3-8d0f-91a89bbec957.svg)

### New Final Design
![Texera Frontend-New-2](https://user-images.githubusercontent.com/36582710/192446097-8ac9b0c7-7ef4-4c58-a349-464e2752e425.svg)

## Other Changes Introduced
- Removed old shared editing implementation.
- Add shared editing server to deployment scripts. **`y-websocket` needs to run for the new frontend to work.**

## Next Step

The final step is adding co-editor awareness on top of this. This PR only allows a workflow to be collaboratively edited, but the user cannot know which other user is making the changes.

Co-authored-by: zuozhiw <zuozhiw@gmail.com>

core/new-gui/src/app/workspace/component/code-editor-dialog/code-editor-dialog.component.ts
@@ -1,7 +1,6 @@
 import { Component, Inject } from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
-import { WorkflowCollabService } from "../../service/workflow-collab/workflow-collab.service";
 import { WorkflowActionService } from "../../service/workflow-graph/model/workflow-action.service";
 import { OperatorPredicate } from "../../types/workflow-common.interface";
 
@@ -25,30 +24,16 @@ export class CodeEditorDialogComponent {
     language: "python",
     fontSize: "11",
     automaticLayout: true,
-    readOnly: true,
+    readOnly: false,
   };
   code: string;
-
-  public lockGranted: boolean = false;
 
   constructor(
     private dialogRef: MatDialogRef<CodeEditorDialogComponent>,
     @Inject(MAT_DIALOG_DATA) code: any,
-    private workflowActionService: WorkflowActionService,
-    private workflowCollabService: WorkflowCollabService
+    private workflowActionService: WorkflowActionService
   ) {
     this.code = code;
-    this.handleLockChange();
-  }
-
-  private handleLockChange(): void {
-    this.workflowCollabService
-      .getLockStatusStream()
-      .pipe(untilDestroyed(this))
-      .subscribe((lockGranted: boolean) => {
-        this.lockGranted = lockGranted;
-        this.editorOptions.readOnly = !this.lockGranted;
-      });
   }
 
   onCodeChange(code: string): void {

core/new-gui/src/app/workspace/component/codearea-custom-template/codearea-custom-template.component.html
@@ -1,13 +1,5 @@
 <div class="attribute-container">
-  <button
-    (click)="onClickEditor()"
-    class="code-button"
-    nz-button
-    nzShape="round"
-    nzType="primary"
-    [ngClass]="{ 'readonly': !lockGranted }"
-  >
-    <div *ngIf="lockGranted">Edit code content</div>
-    <div *ngIf="!lockGranted">View code content</div>
+  <button (click)="onClickEditor()" class="code-button" nz-button nzShape="round" nzType="primary">
+    <div>Edit code content</div>
   </button>
 </div>

core/new-gui/src/app/workspace/component/codearea-custom-template/codearea-custom-template.component.scss
@@ -4,15 +4,4 @@
 
 .attribute-container {
   text-align: center;
-  .code-button {
-    &.readonly {
-      color: black;
-      background-color: blanchedalmond;
-      border-color: blanchedalmond;
-
-      &:hover {
-        background-color: #fff0d9;
-      }
-    }
-  }
 }

core/new-gui/src/app/workspace/component/codearea-custom-template/codearea-custom-template.component.ts
@@ -2,9 +2,6 @@ import { Component } from "@angular/core";
 import { FieldType } from "@ngx-formly/core";
 import { MatDialog, MatDialogRef } from "@angular/material/dialog";
 import { CodeEditorDialogComponent } from "../code-editor-dialog/code-editor-dialog.component";
-import { WorkflowCollabService } from "../../service/workflow-collab/workflow-collab.service";
-import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
-import { WorkflowActionService } from "../../service/workflow-graph/model/workflow-action.service";
 
 /**
  * CodeareaCustomTemplateComponent is the custom template for 'codearea' type of formly field.
@@ -15,56 +12,24 @@ import { WorkflowActionService } from "../../service/workflow-graph/model/workfl
  * Clicking on the 'Edit code content' button will create a new dialogue with CodeEditorComponent
  * as its content. The data of this field will be sent to this dialogue, which contains a Monaco editor.
  */
-@UntilDestroy()
 @Component({
   selector: "texera-codearea-custom-template",
   templateUrl: "./codearea-custom-template.component.html",
   styleUrls: ["./codearea-custom-template.component.scss"],
 })
 export class CodeareaCustomTemplateComponent extends FieldType {
-  lockGranted: boolean = false;
   dialogRef: MatDialogRef<CodeEditorDialogComponent> | undefined;
 
-  constructor(
-    public dialog: MatDialog,
-    public workflowCollabService: WorkflowCollabService,
-    public workflowActionService: WorkflowActionService
-  ) {
+  constructor(public dialog: MatDialog) {
     super();
-    this.handleLockChange();
-    this.handleCodeChange();
   }
 
+  /**
+   * Opens the code editor.
+   */
   onClickEditor(): void {
     this.dialogRef = this.dialog.open(CodeEditorDialogComponent, {
       data: this.formControl?.value || "",
     });
-  }
-
-  private handleLockChange(): void {
-    this.workflowCollabService
-      .getLockStatusStream()
-      .pipe(untilDestroyed(this))
-      .subscribe((lockGranted: boolean) => {
-        this.lockGranted = lockGranted;
-      });
-  }
-
-  private handleCodeChange(): void {
-    this.workflowActionService
-      .getTexeraGraph()
-      .getOperatorPropertyChangeStream()
-      .pipe(untilDestroyed(this))
-      .subscribe(({ operator }) => {
-        if (this.dialogRef != undefined && !this.lockGranted) {
-          // here the assumption is the operator being edited must be highlighted
-          const currentOperatorId: string = this.workflowActionService
-            .getJointGraphWrapper()
-            .getCurrentHighlightedOperatorIDs()[0];
-          if (currentOperatorId === operator.operatorID) {
-            this.dialogRef.componentInstance.code = operator.operatorProperties["code"];
-          }
-        }
-      });
   }
 }

core/new-gui/src/app/workspace/component/navigation/navigation.component.html
@@ -7,23 +7,6 @@
       src="assets/logos/full_logo_small.png?v=1"
     />
 
-    <div *ngIf="userSystemEnabled && workflowCollabEnabled" class="texera-navigation-lockholder-status">
-      <button
-        (click)="onClickAcquireLock()"
-        nz-button
-        [disabled]="lockGranted || workflowReadonly"
-        title="acquire lock"
-        class="lockholder-status"
-        [ngClass]="{ 'clickable': !lockGranted && !workflowReadonly }"
-      >
-        <i *ngIf="lockGranted && !workflowReadonly" nz-icon nzTheme="outline" nzType="lock"></i>
-        <i *ngIf="!lockGranted || workflowReadonly" nz-icon nzTheme="outline" nzType="unlock"></i>
-        <span *ngIf="lockGranted && !workflowReadonly"> {{ "You can edit now" }} </span>
-        <span *ngIf="!lockGranted && !workflowReadonly"> {{ "Readonly - click to edit" }} </span>
-        <span *ngIf="workflowReadonly"> {{ "Readonly - cannot edit" }} </span>
-      </button>
-    </div>
-
     <!-- workflow metadata display -->
     <div *ngIf="userSystemEnabled" class="texera-navigation-workflow-metadata">
       <button
@@ -41,7 +24,6 @@
           *ngIf="!displayParticularWorkflowVersion"
           (change)="onWorkflowNameChange()"
           [(ngModel)]="currentWorkflowName"
-          [disabled]="!lockGranted"
           class="workflow-name"
           placeholder="Untitled Workflow"
         />
@@ -49,13 +31,7 @@
           {{particularVersionDate}}
         </span>
       </label>
-      <button
-        *ngIf="displayParticularWorkflowVersion"
-        [disabled]="!lockGranted"
-        nz-button
-        nzType="primary"
-        (click)="revertToVersion()"
-      >
+      <button *ngIf="displayParticularWorkflowVersion" nz-button nzType="primary" (click)="revertToVersion()">
         Restore this version
       </button>
       <button
@@ -86,18 +62,18 @@
         <button
           (click)="persistWorkflow()"
           *ngIf="userSystemEnabled"
-          [disabled]="!userService.isLogin() || isSaving || !lockGranted"
+          [disabled]="!userService.isLogin() || isSaving"
           nz-button
           title="save"
         >
           <i *ngIf="!isSaving" nz-icon nzTheme="outline" nzType="save"></i>
           <i *ngIf="isSaving" [nzSpin]="true" [nzType]="'sync'" nz-icon nzTheme="outline"></i>
         </button>
-        <button (click)="onClickDeleteAllOperators()" [disabled]="!lockGranted" nz-button title="delete all">
+        <button (click)="onClickDeleteAllOperators()" nz-button title="delete all">
           <i nz-icon nzTheme="outline" nzType="delete"></i>
         </button>
-        <nz-upload [nzDisabled]="!lockGranted" [nzBeforeUpload]="onClickImportWorkflow">
-          <button nz-button [disabled]="!lockGranted" title="import workflow">
+        <nz-upload [nzBeforeUpload]="onClickImportWorkflow">
+          <button nz-button title="import workflow">
             <i nz-icon nzTheme="outline" nzType="upload"></i>
           </button>
         </nz-upload>
@@ -121,7 +97,7 @@
         <button (click)="onClickRestoreZoomOffsetDefault()" nz-button title="reset zoom">
           <i nz-icon nzTheme="outline" nzType="fullscreen"></i>
         </button>
-        <button nz-button (click)="onClickAutoLayout()" [disabled]="!lockGranted" title="auto layout">
+        <button nz-button (click)="onClickAutoLayout()" title="auto layout">
           <i nz-icon nzType="partition" nzTheme="outline"></i>
         </button>
         <button (click)="onClickAddCommentBox()" nz-button title="add a comment">
@@ -154,26 +130,10 @@
             <!--            </li>-->
           </ul>
         </nz-dropdown-menu>
-        <!-- <button
-          (click)="onClickGroupOperators()"
-          [disabled]="!highlightedElementsGroupable()"
-          nz-button
-          title="group"
-        >
-          <i nz-icon nzTheme="outline" nzType="group"></i>
-        </button>
-        <button
-          (click)="onClickUngroupOperators()"
-          [disabled]="!highlightedElementsUngroupable()"
-          nz-button
-          title="ungroup"
-        >
-          <i nz-icon nzTheme="outline" nzType="ungroup"></i>
-        </button> -->
         <button
           (click)="operatorMenu.disableHighlightedOperators()"
           *ngIf="operatorMenu.isDisableOperator || !operatorMenu.isDisableOperatorClickable"
-          [disabled]="!operatorMenu.isDisableOperatorClickable || !lockGranted"
+          [disabled]="!operatorMenu.isDisableOperatorClickable"
           nz-button
           title="disable operators"
         >
@@ -182,7 +142,7 @@
         <button
           (click)="operatorMenu.disableHighlightedOperators()"
           *ngIf="!operatorMenu.isDisableOperator && operatorMenu.isDisableOperatorClickable"
-          [disabled]="!operatorMenu.isDisableOperatorClickable || !lockGranted"
+          [disabled]="!operatorMenu.isDisableOperatorClickable"
           nz-button
           title="operators disabled, click to re-enable"
         >
@@ -190,7 +150,7 @@
         </button>
         <button
           *ngIf="operatorMenu.operatorCacheEnabled && (operatorMenu.isCacheOperator || ! operatorMenu.isCacheOperatorClickable)"
-          [disabled]="! operatorMenu.isCacheOperatorClickable || !lockGranted"
+          [disabled]="! operatorMenu.isCacheOperatorClickable"
           (click)="operatorMenu.cacheHighlightedOperators()"
           nz-button
           title="cache operators"
@@ -199,7 +159,7 @@
         </button>
         <button
           *ngIf="operatorMenu.operatorCacheEnabled && (! operatorMenu.isCacheOperator && operatorMenu.isCacheOperatorClickable)"
-          [disabled]="! operatorMenu.isCacheOperatorClickable || !lockGranted"
+          [disabled]="! operatorMenu.isCacheOperatorClickable"
           (click)="operatorMenu.cacheHighlightedOperators()"
           nz-button
           title="operators cached, click to remove cache"
@@ -224,7 +184,7 @@
       <nz-button-group nzSize="large">
         <button
           (click)="undoRedoService.undoAction()"
-          [disabled]="displayParticularWorkflowVersion || !undoRedoService.canUndo() || !lockGranted"
+          [disabled]="displayParticularWorkflowVersion || !undoRedoService.canUndo()"
           class="animate-to-reveal-stop-button"
           nz-button
           nzType="default"
@@ -233,7 +193,7 @@
         </button>
         <button
           (click)="undoRedoService.redoAction()"
-          [disabled]="displayParticularWorkflowVersion || !undoRedoService.canRedo() || !lockGranted"
+          [disabled]="displayParticularWorkflowVersion || !undoRedoService.canRedo()"
           class="animate-to-reveal-stop-button"
           nz-button
           nzType="default"
@@ -246,8 +206,7 @@
           [disabled]="
 						executionState === ExecutionState.Uninitialized ||
 						executionState === ExecutionState.Completed ||
-						executionState === ExecutionState.Aborted ||
-            !lockGranted
+						executionState === ExecutionState.Aborted
 					"
           class="texera-navigation-stop-button"
           nz-button
@@ -259,7 +218,7 @@
         </button>
         <button
           (click)="onClickRunHandler()"
-          [disabled]="runDisable || !workflowWebsocketService.isConnected || displayParticularWorkflowVersion|| !lockGranted"
+          [disabled]="runDisable || !workflowWebsocketService.isConnected || displayParticularWorkflowVersion"
           class="texera-navigation-run-button animate-to-reveal-stop-button"
           nz-button
           nzType="primary"

core/new-gui/src/app/common/service/user/stub-user.service.ts
@@ -48,4 +48,8 @@ export class StubUserService implements PublicInterfaceOf<UserService> {
   userChanged(): Observable<User | undefined> {
     return this.userChangeSubject.asObservable();
   }
+
+  getCurrentUser(): User | undefined {
+    return this.user;
+  }
 }

commit cb15a4f5771e92b99f08c4c189655705d7543f05

Fix Several Components Not Disabled During Execution (#1763)
This PR fixes a bug where the following functionalities are not disabled
when `workflowModificatonEnabled` is `true`:

- Disable operator
- Cache operator
- Auto Layout
- Delete All
- Import Workflow
- Add Comment
- Restore to a Version
- Python UDF's Edit button
- Python UDF code editor
- Context Menu items

These bugs are introduced by #1674's deletion of "lock" in previous
shared editing implementation.

For other components, I use `WorkflowActionService`'s flag. For Python
UDF's readonly status I use formly's status, since we allow temporarily
enabling modification of properties during execution.

Added back the "readonly" button for UDF, and used a workaround similar
to #1688 to fix the unit test. It would look like below:
![Screen Recording 2022-12-08 at 11 30
20](https://user-images.githubusercontent.com/36582710/206551162-d2727068-2b65-494f-a8f6-a494eea3adc2.gif)

There is one more complexity for version display: since when displaying
a version, workflow modification is always disabled, we cannot use this
flag to determine whether restore version should be disabled. This has
never been handled properly. For now I added a temporary flag to
remember the workflow action disabled status before displaying a
particular version.

core/new-gui/src/app/dashboard/service/workflow-version/workflow-version.service.ts
@@ -30,6 +30,7 @@ export class WorkflowVersionService {
   private workflowVersionsObservable = new Subject<readonly string[]>();
   private displayParticularWorkflowVersion = new BehaviorSubject<boolean>(false);
   private differentOpIDsList: DifferentOpIDsList = { modified: [], added: [], deleted: [] };
+  public modificationEnabledBeforeTempWorkflow = true;
   public operatorPropertyDiff: { [key: string]: Map<String, String> } = {};
   constructor(
     private workflowActionService: WorkflowActionService,
@@ -59,6 +60,7 @@ export class WorkflowVersionService {
   }
 
   public displayParticularVersion(workflow: Workflow) {
+    this.modificationEnabledBeforeTempWorkflow = this.workflowActionService.checkWorkflowModificationEnabled();
     // we need to display the version on the paper but keep the original workflow in the background
     this.workflowActionService.setTempWorkflow(this.workflowActionService.getWorkflow());
     // get the list of IDs of different elements when comparing displaying to the editing version
@@ -201,6 +203,7 @@ export class WorkflowVersionService {
     this.workflowActionService.resetTempWorkflow();
     this.workflowPersistService.setWorkflowPersistFlag(true);
     this.setDisplayParticularVersion(false);
+    if (!this.modificationEnabledBeforeTempWorkflow) this.workflowActionService.disableWorkflowModification();
   }
 
   public closeParticularVersionDisplay() {
@@ -218,6 +221,7 @@ export class WorkflowVersionService {
     this.undoRedoService.enableWorkFlowModification();
     this.workflowPersistService.setWorkflowPersistFlag(true);
     this.setDisplayParticularVersion(false);
+    if (!this.modificationEnabledBeforeTempWorkflow) this.workflowActionService.disableWorkflowModification();
   }
 
   public unhighlightOpVersionDiff(differentOpIDsList: DifferentOpIDsList) {

core/new-gui/src/app/workspace/component/code-editor-dialog/code-editor-dialog.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, ElementRef, Inject, OnDestroy, ViewChild } from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
-import { UntilDestroy } from "@ngneat/until-destroy";
+import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
 import { WorkflowActionService } from "../../service/workflow-graph/model/workflow-action.service";
 import { YText } from "yjs/dist/src/types/YText";
 import { MonacoBinding } from "y-monaco";
@@ -8,6 +8,7 @@ import { CoeditorPresenceService } from "../../service/workflow-graph/model/coed
 import { DomSanitizer, SafeStyle } from "@angular/platform-browser";
 import { Coeditor } from "../../../common/type/user";
 import { YType } from "../../types/shared-editing.interface";
+import { FormControl } from "@angular/forms";
 
 declare const monaco: any;
 
@@ -36,20 +37,21 @@ export class CodeEditorDialogComponent implements AfterViewInit, SafeStyle, OnDe
     fontSize: "11",
     automaticLayout: true,
   };
-  code: string;
   @ViewChild("editor", { static: true }) divEditor: ElementRef | undefined;
   loaded: boolean = false;
 
-  private ytext?: YText;
+  private formControl: FormControl;
+  private code?: YText;
+  private editor?: any;
 
   constructor(
     private sanitizer: DomSanitizer,
     private dialogRef: MatDialogRef<CodeEditorDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) code: any,
+    @Inject(MAT_DIALOG_DATA) formControl: FormControl,
     private workflowActionService: WorkflowActionService,
     public coeditorPresenceService: CoeditorPresenceService
   ) {
-    this.code = code;
+    this.formControl = formControl;
   }
 
   ngOnDestroy(): void {
@@ -67,7 +69,7 @@ export class CodeEditorDialogComponent implements AfterViewInit, SafeStyle, OnDe
 
     this.workflowActionService.getTexeraGraph().updateSharedModelAwareness("editingCode", true);
 
-    this.ytext = (
+    this.code = (
       this.workflowActionService
         .getTexeraGraph()
         .getSharedOperatorType(currentOperatorId)
@@ -75,6 +77,7 @@ export class CodeEditorDialogComponent implements AfterViewInit, SafeStyle, OnDe
     ).get("code") as YText;
 
     this.initMonaco();
+    this.handleDisabledStatusChange();
   }
 
   /**
@@ -97,13 +100,26 @@ export class CodeEditorDialogComponent implements AfterViewInit, SafeStyle, OnDe
    */
   private initMonaco() {
     const editor = monaco.editor.create(this.divEditor?.nativeElement, this.editorOptions);
-    if (this.ytext) {
+    if (this.code) {
       new MonacoBinding(
-        this.ytext,
+        this.code,
         editor.getModel(),
         new Set([editor]),
         this.workflowActionService.getTexeraGraph().getSharedModelAwareness()
       );
     }
+    this.editor = editor;
+  }
+
+  /**
+   * Uses the formControl's status to change readonly status of the editor.
+   * @private
+   */
+  private handleDisabledStatusChange(): void {
+    this.formControl.statusChanges.pipe(untilDestroyed(this)).subscribe(_ => {
+      this.editor.updateOptions({
+        readOnly: this.formControl.disabled,
+      });
+    });
   }
 }

core/new-gui/src/app/workspace/component/codearea-custom-template/codearea-custom-template.component.html
@@ -4,7 +4,9 @@
     class="code-button"
     nz-button
     nzShape="round"
-    nzType="primary">
-    <div>Edit code content</div>
+    nzType="primary"
+    [ngClass]="{ 'readonly': readonly }">
+    <div *ngIf="!readonly">Edit code content</div>
+    <div *ngIf="readonly">View code content</div>
   </button>
 </div>

core/new-gui/src/app/workspace/component/codearea-custom-template/codearea-custom-template.component.scss
@@ -4,4 +4,15 @@
 
 .attribute-container {
   text-align: center;
+  .code-button {
+    &.readonly {
+      color: black;
+      background-color: blanchedalmond;
+      border-color: blanchedalmond;
+
+      &:hover {
+        background-color: #fff0d9;
+      }
+    }
+  }
 }

core/new-gui/src/app/workspace/component/codearea-custom-template/codearea-custom-template.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { AfterViewInit, Component } from "@angular/core";
 import { FieldType } from "@ngx-formly/core";
 import { MatDialog, MatDialogRef } from "@angular/material/dialog";
 import { CodeEditorDialogComponent } from "../code-editor-dialog/code-editor-dialog.component";
@@ -20,12 +20,32 @@ import { CoeditorPresenceService } from "../../service/workflow-graph/model/coed
   templateUrl: "./codearea-custom-template.component.html",
   styleUrls: ["./codearea-custom-template.component.scss"],
 })
-export class CodeareaCustomTemplateComponent extends FieldType {
+export class CodeareaCustomTemplateComponent extends FieldType<any> implements AfterViewInit {
   dialogRef: MatDialogRef<CodeEditorDialogComponent> | undefined;
+  readonly: boolean = false;
 
   constructor(public dialog: MatDialog, private coeditorPresenceService: CoeditorPresenceService) {
     super();
     this.handleShadowingMode();
+  }
+
+  ngAfterViewInit() {
+    this.handleReadonlyStatusChange();
+  }
+
+  /**
+   * Syncs the disabled status of the button with formControl.
+   * Used to fit the unit test since undefined might occur.
+   * TODO: Using <code>formControl</code> here instead of
+   *  <code>WorkflowActionService.checkWorkflowModificationEnabled()</code>
+   *  since the readonly status of operator properties also locally depend on whether "Unlock for Logic Change"
+   *  is enabled, which can only be accessed via <code>formControl</code>. Might need a more unified solution.
+   */
+  handleReadonlyStatusChange(): void {
+    if (this.field === undefined) return;
+    this.field.formControl.statusChanges.pipe(untilDestroyed(this)).subscribe(() => {
+      this.readonly = this.field.formControl.disabled;
+    });
   }
 
   /**
@@ -33,7 +53,7 @@ export class CodeareaCustomTemplateComponent extends FieldType {
    */
   onClickEditor(): void {
     this.dialogRef = this.dialog.open(CodeEditorDialogComponent, {
-      data: this.formControl?.value || "",
+      data: this.field.formControl,
     });
   }
 

core/new-gui/src/app/workspace/component/navigation/navigation.component.html
@@ -43,6 +43,7 @@
         *ngIf="displayParticularWorkflowVersion"
         nz-button
         nzType="primary"
+        [disabled]="!workflowVersionService.modificationEnabledBeforeTempWorkflow"
         (click)="revertToVersion()">
         Restore this version
       </button>
@@ -115,7 +116,7 @@
         <button
           (click)="persistWorkflow()"
           *ngIf="userSystemEnabled"
-          [disabled]="!userService.isLogin() || isSaving"
+          [disabled]="!userService.isLogin() || isSaving || !workflowActionService.checkWorkflowModificationEnabled()"
           nz-button
           title="save">
           <i
@@ -132,6 +133,7 @@
         </button>
         <button
           (click)="onClickDeleteAllOperators()"
+          [disabled]="!workflowActionService.checkWorkflowModificationEnabled()"
           nz-button
           title="delete all">
           <i
@@ -139,9 +141,12 @@
             nzTheme="outline"
             nzType="delete"></i>
         </button>
-        <nz-upload [nzBeforeUpload]="onClickImportWorkflow">
+        <nz-upload
+          [nzDisabled]="!workflowActionService.checkWorkflowModificationEnabled()"
+          [nzBeforeUpload]="onClickImportWorkflow">
           <button
             nz-button
+            [disabled]="!workflowActionService.checkWorkflowModificationEnabled()"
             title="import workflow">
             <i
               nz-icon
@@ -202,6 +207,7 @@
         <button
           nz-button
           (click)="onClickAutoLayout()"
+          [disabled]="!workflowActionService.checkWorkflowModificationEnabled()"
           title="auto layout">
           <i
             nz-icon
@@ -210,6 +216,7 @@
         </button>
         <button
           (click)="onClickAddCommentBox()"
+          [disabled]="!workflowActionService.checkWorkflowModificationEnabled()"
           nz-button
           title="add a comment">
           <i

core/new-gui/src/app/workspace/component/workflow-editor/context-menu/context-menu/context-menu.component.html
@@ -13,7 +13,8 @@
   <li
     nz-menu-item
     *ngIf="(operatorMenu.effectivelyHighlightedOperators.value.length > 0 ||
-  operatorMenu.effectivelyHighlightedCommentBoxes.value.length > 0)"
+  operatorMenu.effectivelyHighlightedCommentBoxes.value.length > 0) &&
+  this.workflowActionService.checkWorkflowModificationEnabled()"
     (click)="onCut()">
     <span
       nz-icon
@@ -24,7 +25,8 @@
   <li
     nz-menu-item
     *ngIf="(operatorMenu.effectivelyHighlightedOperators.value.length === 0 &&
-  operatorMenu.effectivelyHighlightedCommentBoxes.value.length === 0)"
+  operatorMenu.effectivelyHighlightedCommentBoxes.value.length === 0) &&
+  this.workflowActionService.checkWorkflowModificationEnabled()"
     (click)="onPaste()">
     <span
       nz-icon
@@ -75,7 +77,8 @@
   <li
     nz-menu-item
     *ngIf="(operatorMenu.effectivelyHighlightedOperators.value.length > 0 ||
-  operatorMenu.effectivelyHighlightedCommentBoxes.value.length > 0)"
+  operatorMenu.effectivelyHighlightedCommentBoxes.value.length > 0) &&
+  this.workflowActionService.checkWorkflowModificationEnabled()"
     (click)="onDelete()">
     <span
       nz-icon

core/new-gui/src/app/workspace/service/operator-menu/operator-menu.service.ts
@@ -137,21 +137,25 @@ export class OperatorMenuService {
   handleDisableOperatorStatusChange() {
     merge(
       this.effectivelyHighlightedOperators,
-      this.workflowActionService.getTexeraGraph().getDisabledOperatorsChangedStream()
+      this.workflowActionService.getTexeraGraph().getDisabledOperatorsChangedStream(),
+      this.workflowActionService.getWorkflowModificationEnabledStream()
     ).subscribe(event => {
       const allDisabled = this.effectivelyHighlightedOperators.value.every(op =>
         this.workflowActionService.getTexeraGraph().isOperatorDisabled(op)
       );
 
       this.isDisableOperator = !allDisabled;
-      this.isDisableOperatorClickable = this.effectivelyHighlightedOperators.value.length !== 0;
+      this.isDisableOperatorClickable =
+        this.effectivelyHighlightedOperators.value.length !== 0 &&
+        this.workflowActionService.checkWorkflowModificationEnabled();
     });
   }
 
   handleCacheOperatorStatusChange() {
     merge(
       this.effectivelyHighlightedOperators,
-      this.workflowActionService.getTexeraGraph().getCachedOperatorsChangedStream()
+      this.workflowActionService.getTexeraGraph().getCachedOperatorsChangedStream(),
+      this.workflowActionService.getWorkflowModificationEnabledStream()
     ).subscribe(event => {
       const effectiveHighlightedOperatorsExcludeSink = this.effectivelyHighlightedOperators.value.filter(
         op => !isSink(this.workflowActionService.getTexeraGraph().getOperator(op))
@@ -162,7 +166,9 @@ export class OperatorMenuService {
       );
 
       this.isCacheOperator = !allCached;
-      this.isCacheOperatorClickable = effectiveHighlightedOperatorsExcludeSink.length !== 0;
+      this.isCacheOperatorClickable =
+        effectiveHighlightedOperatorsExcludeSink.length !== 0 &&
+        this.workflowActionService.checkWorkflowModificationEnabled();
     });
   }
 