7e0b4df220cbf704bd61885ba70a290ad6bd6fe3
linxinyuan <linxinyuan@gmail.com>
2023-02-02 16:36:50 -0800

    resolve conflicts

--- core/new-gui/src/app/common/service/user/stub-user.service.ts
+++ core/new-gui/src/app/common/service/user/stub-user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 
-import { Observable, of, ReplaySubject, Subject } from "rxjs";
+import { Observable, of, Subject } from "rxjs";
 import { Role, User } from "../../type/user";
 import { UserService } from "./user.service";
 import { PublicInterfaceOf } from "../../util/stub";
