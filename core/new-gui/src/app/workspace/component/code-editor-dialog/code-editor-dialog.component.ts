10c3cb8d9179dd739b0968c6a14c161490763f6a
unknown <sreetejreddy1998@gmail.com>
2023-04-17 17:24:52 -0700

    fix: bug

--- core/new-gui/src/app/workspace/component/code-editor-dialog/code-editor-dialog.component.ts
+++ core/new-gui/src/app/workspace/component/code-editor-dialog/code-editor-dialog.component.ts
@@ -1,5 +1,5 @@
 /// <reference path="../../../../../node_modules/monaco-editor/monaco.d.ts" />
-import { AfterViewInit, Component, ElementRef, Inject, OnDestroy, ViewChild } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, Inject, OnDestroy, ViewChild, OnInit } from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
 import { WorkflowActionService } from "../../service/workflow-graph/model/workflow-action.service";
@@ -21,8 +21,6 @@ import { FormControl } from "@angular/forms";
 import { createUrl } from "../../../common/util/url";
 
 declare const monaco: any;
-let loadedMonaco = false;
-let loadPromise: Promise<void>;
 
 /**
  * CodeEditorDialogComponent is the content of the dialogue invoked by CodeareaCustomTemplateComponent.
@@ -36,6 +34,7 @@ let loadPromise: Promise<void>;
  * The dialogue can be closed with ESC key or by clicking on areas outside
  * the dialogue. Closing the dialogue will send the edited contend back to the custom template field.
  */
+
 @UntilDestroy()
 @Component({
   selector: "texera-code-editor-dialog",
@@ -56,6 +55,7 @@ export class CodeEditorDialogComponent implements AfterViewInit, SafeStyle, OnDe
   private formControl: FormControl;
   private code?: YText;
   private editor?: any;
+  private socket?: any;
 
   constructor(
     private sanitizer: DomSanitizer,
@@ -69,6 +69,10 @@ export class CodeEditorDialogComponent implements AfterViewInit, SafeStyle, OnDe
 
   ngOnDestroy(): void {
     this.workflowActionService.getTexeraGraph().updateSharedModelAwareness("editingCode", false);
+    if (this.socket) {
+      this.socket.close();
+      this.socket = undefined;
+    }
   }
 
   createLanguageClient(transports: MessageTransports): MonacoLanguageClient {
@@ -99,11 +103,11 @@ export class CodeEditorDialogComponent implements AfterViewInit, SafeStyle, OnDe
     const currentOperatorId: string = this.workflowActionService
     .getJointGraphWrapper()
     .getCurrentHighlightedOperatorIDs()[0];
-    console.log(currentOperatorId);
     return `inmemory://${currentOperatorId}.py`
   }
 
   ngAfterViewInit() {
+    this.initMonaco()
     const currentOperatorId: string = this.workflowActionService
       .getJointGraphWrapper()
       .getCurrentHighlightedOperatorIDs()[0];
@@ -120,11 +124,7 @@ export class CodeEditorDialogComponent implements AfterViewInit, SafeStyle, OnDe
         .getSharedOperatorType(currentOperatorId)
         .get("operatorProperties") as YType<Readonly<{ [key: string]: any }>>
     ).get("code") as YText;
-    const func = () => {
-      this.initMonaco();
-      this.handleDisabledStatusChange();
-    };
-    func();
+    this.handleDisabledStatusChange();
   }
 
   /**
@@ -146,7 +146,6 @@ export class CodeEditorDialogComponent implements AfterViewInit, SafeStyle, OnDe
    * @private
    */
   private initMonaco() {
-    this.customSuggestions();
     const editor = monaco.editor.create(this.divEditor?.nativeElement, this.editorOptions);
     if (this.code) {
       new MonacoBinding(
@@ -157,151 +156,28 @@ export class CodeEditorDialogComponent implements AfterViewInit, SafeStyle, OnDe
       );
     }
     this.editor = editor;
-
-    MonacoServices.install();
     this.connectLanguageServer();
-    const editorService = editor._codeEditorService;
-    const openEditorBase = editorService.openCodeEditor.bind(editorService);
-    monaco.languages.registerDefinitionProvider("python", {
-      provideDefinition(model: any, position: any, toen: any) {
-        return [
-          {
-            uri: monaco.Uri.file(this.getModelURI()),
-            range: {
-              startLineNumber: 69,
-              endLineNumber: 69,
-              startColumn: 7,
-              endColumn: 7,
-            },
-          },
-        ];
-      },
-    });
-    editorService.openCodeEditor = async (
-      input: {
-        resource: any;
-        options: { selection: { startLineNumber: any; endLineNumber: any; startColumn: any; endColumn: any } };
-      },
-      source: any
-    ) => {
-      const result = await openEditorBase(input, source);
-      if (result === null) {
-        console.log("Open definition for:", input);
-        let model = monaco.editor.getModel(input.resource);
-        editor.setModel(model);
-        editor.revealRangeInCenterIfOutsideViewport({
-          startLineNumber: input.options.selection.startLineNumber,
-          endLineNumber: input.options.selection.endLineNumber,
-          startColumn: input.options.selection.startColumn,
-          endColumn: input.options.selection.endColumn,
-        });
-        editor.setPosition({
-          lineNumber: input.options.selection.startLineNumber,
-          column: input.options.selection.startColumn,
-        });
-      }
-      return result; // always return the base result
-    };
   }
-
-  private customSuggestions() {
-    monaco.languages.registerCompletionItemProvider("python", {
-      provideCompletionItems: (
-        model: { getWordUntilPosition: (arg0: { lineNumber: any; column: number }) => any },
-        position: { lineNumber: any; column: number }
-      ) => {
-        const wordBeforePosition = model.getWordUntilPosition({
-          lineNumber: position.lineNumber,
-          column: position.column - 1,
-        });
-
-        const wordUntilPosition = model.getWordUntilPosition(position);
-        if (wordBeforePosition.word.trim() === "" || wordUntilPosition.word.trim() === "") {
-          const keywords = completionTriggerKeywords;
-
-          const suggestions = keywords.map(id => ({
-            label: id.label,
-            kind: id.kind,
-            description: id.description,
-            documentation: id.description,
-            insertText: id.insertText,
-            detail: id.description,
-            insertTextRules: id.insertTextRules,
-            range: {
-              startLineNumber: position.lineNumber,
-              startColumn: wordUntilPosition.startColumn,
-              endLineNumber: position.lineNumber,
-              endColumn: wordUntilPosition.endColumn - 1,
-            },
-          }));
-          return { suggestions };
-        }
-      },
-    });
-    // For the Demo: should make this dymanic
-    const completionTriggerKeywords = [
-      {
-        label: "TexeraCustomSuggestion1",
-        kind: monaco.languages.CompletionItemKind.Function,
-        insertText: "Sreetej",
-        description: "1.1, 1.2, 1.3",
-        insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-      },
-      {
-        label: "TexeraCustomSuggestion2",
-        kind: monaco.languages.CompletionItemKind.Function,
-        insertText: "TexeraCustomSuggestion2",
-        description: "2.1",
-      },
-      {
-        label: "Texera-Jiaxi",
-        kind: monaco.languages.CompletionItemKind.Function,
-        insertText: "TexeraCustomSuggestion3",
-        description: "3.1, 3.2, 3.3",
-      },
-      {
-        label: "Texera-Sreetej",
-        kind: monaco.languages.CompletionItemKind.Function,
-        insertText: "Sreetej",
-        description: "4.1",
-        insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-      },
-      {
-        label: "Texera-Aditya",
-        kind: monaco.languages.CompletionItemKind.Function,
-        insertText: "Test5",
-        description: "5.1",
-        insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-      },
-      {
-        label: "Texera-Dhruv",
-        kind: monaco.languages.CompletionItemKind.Function,
-        insertText: "Test6",
-        description: "6.1",
-        insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-      },
-    ];
-  }
-
   /**
    * Create a Monaco editor and connect it to MonacoBinding.
    * @private
    */
   private connectLanguageServer() {
     const url = createUrl(WEB_SOCKET_HOST, LANGUAGE_SERVER_PORT, PYTHON_LANGUAGE_SERVER);
-    const webSocket = new WebSocket(url);
-
-    webSocket.onopen = () => {
-      const socket = toSocket(webSocket);
-      const reader = new WebSocketMessageReader(socket);
-      const writer = new WebSocketMessageWriter(socket);
-      const languageClient = this.createLanguageClient({
-        reader,
-        writer,
-      });
-      languageClient.start();
-      reader.onClose(() => languageClient.stop());
-    };
+    if(!this.socket) {
+      this.socket = new WebSocket(url);
+      this.socket.onopen = () => {
+        const socket = toSocket(this.socket);
+        const reader = new WebSocketMessageReader(socket);
+        const writer = new WebSocketMessageWriter(socket);
+        const languageClient = this.createLanguageClient({
+          reader,
+          writer,
+        });
+        languageClient.start();
+        reader.onClose(() => languageClient.stop());
+      };
+    }
   }
 
   /**
