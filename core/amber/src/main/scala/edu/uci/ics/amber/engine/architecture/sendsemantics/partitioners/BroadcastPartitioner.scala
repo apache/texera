9e422a8d9befd2c9b53df175c3f1d88f82f45120
yunyad <yunyad1@uci.edu>
2023-04-24 20:37:54 -0700

    broadcast

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/sendsemantics/partitioners/BroadcastPartitioner.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/sendsemantics/partitioners/BroadcastPartitioner.scala
@@ -18,40 +18,4 @@ case class BroadcastPartitioner(partitioning: BroadcastPartitioning) extends Par
   }
 
   override def allReceivers: Seq[ActorVirtualIdentity] = partitioning.receivers
-//  var batch: Array[ITuple] = new Array[ITuple](partitioning.batchSize)
-//  var currentSize = 0
-//
-//  override def addTupleToBatch(
-//      tuple: ITuple
-//  ): Iterator[(ActorVirtualIdentity, DataPayload)] = {
-//    val it = Iterator()
-//    batch(currentSize) = tuple
-//    if (currentSize == partitioning.batchSize) {
-//      currentSize = 0
-//      val retBatch = batch
-//      for (i <- partitioning.receivers.indices) {
-//        it ++ Some(partitioning.receivers(i), DataFrame(retBatch))
-//      }
-//    }
-//    it
-//  }
-//
-//  override def noMore(): Array[(ActorVirtualIdentity, DataPayload)] = {
-//    val ret = new ArrayBuffer[(ActorVirtualIdentity, DataPayload)]
-//    if (currentSize > 0) {
-//      for (i <- partitioning.receivers.indices) {
-//        ret.append((partitioning.receivers(i), DataFrame(batch.slice(0, currentSize))))
-//      }
-//    }
-//    for (i <- partitioning.receivers.indices) {
-//      ret.append((partitioning.receivers(i), EndOfUpstream()))
-//    }
-//
-//    ret.toArray
-//  }
-//
-//  override def reset(): Unit = {
-//    batch = new Array[ITuple](partitioning.batchSize)
-//    currentSize = 0
-//  }
 }
