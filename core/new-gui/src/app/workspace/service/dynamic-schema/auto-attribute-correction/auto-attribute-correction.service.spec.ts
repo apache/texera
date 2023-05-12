39dff3cafcb28b37db8c0e177939aa3cbd5189de
linxinyuan <linxinyuan@gmail.com>
2023-05-12 00:29:29 -0700

    resolve conflit

--- core/new-gui/src/app/workspace/service/dynamic-schema/auto-attribute-correction/auto-attribute-correction.service.spec.ts
+++ core/new-gui/src/app/workspace/service/dynamic-schema/auto-attribute-correction/auto-attribute-correction.service.spec.ts
@@ -3,7 +3,12 @@ import { HttpClientTestingModule, HttpTestingController } from "@angular/common/
 import { fakeAsync, inject, TestBed, tick, flush, discardPeriodicTasks } from "@angular/core/testing";
 import { environment } from "../../../../../environments/environment";
 import { OperatorMetadataService } from "../../operator-metadata/operator-metadata.service";
-import { mockPoint } from "../../workflow-graph/model/mock-workflow-data";
+import {
+  mockPoint,
+  mockScanPredicate,
+  mockScanSentimentLink,
+  mockSentimentPredicate,
+} from "../../workflow-graph/model/mock-workflow-data";
 import { StubOperatorMetadataService } from "../../operator-metadata/stub-operator-metadata.service";
 import { WorkflowActionService } from "../../workflow-graph/model/workflow-action.service";
 import { DynamicSchemaService } from "../dynamic-schema.service";
@@ -58,28 +63,26 @@ describe("AttributeChangePropagationService", () => {
 
   it("should propagate new attribute name when atteibute is renamed", fakeAsync(() => {
     const workflowActionService: WorkflowActionService = TestBed.inject(WorkflowActionService);
-    TestBed.inject(SchemaPropagationService);
-    TestBed.inject(AutoAttributeCorrectionService);
+    const schemaPropagationService: SchemaPropagationService = TestBed.inject(SchemaPropagationService);
+    const autoAttributeCorrectionService: AutoAttributeCorrectionService =
+      TestBed.inject(AutoAttributeCorrectionService);
+
     workflowActionService.addOperator(mockSentimentOperatorA, mockPoint);
     workflowActionService.addOperator(mockSentimentOperatorB, mockPoint);
     workflowActionService.addLink(mockLinkAtoB);
 
-    const req1 = httpTestingController.expectOne(
-      `${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}/undefined`
-    );
+    const req1 = httpTestingController.expectOne(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
     expect(req1.request.method === "POST");
-    expect(req1.request.url).toEqual(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}/undefined`);
+    expect(req1.request.url).toEqual(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
     req1.flush(mockSchemaPropagationResponse1);
     httpTestingController.verify();
 
     // trigger inputSchemaChangeStream
     workflowActionService.setOperatorProperty(mockSentimentOperatorA.operatorID, { testAttr: "test" });
     tick(SCHEMA_PROPAGATION_DEBOUNCE_TIME_MS);
-    const req2 = httpTestingController.expectOne(
-      `${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}/undefined`
-    );
+    const req2 = httpTestingController.expectOne(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
     expect(req2.request.method === "POST");
-    expect(req2.request.url).toEqual(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}/undefined`);
+    expect(req2.request.url).toEqual(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
     req2.flush(mockSchemaPropagationResponse2);
     httpTestingController.verify();
 
@@ -92,28 +95,26 @@ describe("AttributeChangePropagationService", () => {
 
   it("should delete attribute in succeeding operators when attribute is deleted", fakeAsync(() => {
     const workflowActionService: WorkflowActionService = TestBed.inject(WorkflowActionService);
-    TestBed.inject(SchemaPropagationService);
-    TestBed.inject(AutoAttributeCorrectionService);
+    const schemaPropagationService: SchemaPropagationService = TestBed.inject(SchemaPropagationService);
+    const autoAttributeCorrectionService: AutoAttributeCorrectionService =
+      TestBed.inject(AutoAttributeCorrectionService);
+
     workflowActionService.addOperator(mockSentimentOperatorA, mockPoint);
     workflowActionService.addOperator(mockSentimentOperatorB, mockPoint);
     workflowActionService.addLink(mockLinkAtoB);
 
-    const req1 = httpTestingController.expectOne(
-      `${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}/undefined`
-    );
+    const req1 = httpTestingController.expectOne(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
     expect(req1.request.method === "POST");
-    expect(req1.request.url).toEqual(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}/undefined`);
+    expect(req1.request.url).toEqual(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
     req1.flush(mockSchemaPropagationResponse1);
     httpTestingController.verify();
 
     // trigger inputSchemaChangeStream
     workflowActionService.setOperatorProperty(mockSentimentOperatorA.operatorID, { testAttr: "test" });
     tick(SCHEMA_PROPAGATION_DEBOUNCE_TIME_MS + 1);
-    const req2 = httpTestingController.expectOne(
-      `${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}/undefined`
-    );
+    const req2 = httpTestingController.expectOne(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
     expect(req2.request.method === "POST");
-    expect(req2.request.url).toEqual(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}/undefined`);
+    expect(req2.request.url).toEqual(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
     req2.flush(mockSchemaPropagationResponse3);
     httpTestingController.verify();
 
@@ -127,31 +128,29 @@ describe("AttributeChangePropagationService", () => {
 
   it("should propagate new attribute name in three consecutive operators", fakeAsync(() => {
     const workflowActionService: WorkflowActionService = TestBed.inject(WorkflowActionService);
-    TestBed.inject(SchemaPropagationService);
-    TestBed.inject(AutoAttributeCorrectionService);
+    const schemaPropagationService: SchemaPropagationService = TestBed.inject(SchemaPropagationService);
+    const autoAttributeCorrectionService: AutoAttributeCorrectionService =
+      TestBed.inject(AutoAttributeCorrectionService);
+
     workflowActionService.addOperator(mockSentimentOperatorA, mockPoint);
     workflowActionService.addOperator(mockSentimentOperatorB, mockPoint);
     workflowActionService.addOperator(mockSentimentOperatorC, mockPoint);
     workflowActionService.addLink(mockLinkAtoB);
-    httpTestingController.expectOne(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}/undefined`);
+    httpTestingController.expectOne(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
     workflowActionService.addLink(mockLinkBtoC);
 
-    const req1 = httpTestingController.expectOne(
-      `${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}/undefined`
-    );
+    const req1 = httpTestingController.expectOne(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
     expect(req1.request.method === "POST");
-    expect(req1.request.url).toEqual(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}/undefined`);
+    expect(req1.request.url).toEqual(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
     req1.flush(mockSchemaPropagationResponse4);
     httpTestingController.verify();
 
     // trigger inputSchemaChangeStream
     workflowActionService.setOperatorProperty(mockSentimentOperatorA.operatorID, { testAttr: "test" });
     tick(SCHEMA_PROPAGATION_DEBOUNCE_TIME_MS);
-    const req2 = httpTestingController.expectOne(
-      `${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}/undefined`
-    );
+    const req2 = httpTestingController.expectOne(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
     expect(req2.request.method === "POST");
-    expect(req2.request.url).toEqual(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}/undefined`);
+    expect(req2.request.url).toEqual(`${AppSettings.getApiEndpoint()}/${SCHEMA_PROPAGATION_ENDPOINT}`);
     req2.flush(mockSchemaPropagationResponse5);
     httpTestingController.verify();
 
