8a5ad8c410c25b34f9d4c70bb8cef7c55a214688
linxinyuan <linxinyuan@gmail.com>
2023-05-20 23:56:48 -0700

    Revert "fix format"
    This reverts commit 6ed0bc79010c9fab40be610f6bbcf8e6c5a8f452.

--- core/amber/src/main/scala/edu/uci/ics/texera/web/resource/dashboard/user/workflow/WorkflowAccessResource.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/resource/dashboard/user/workflow/WorkflowAccessResource.scala
@@ -51,12 +51,16 @@ object WorkflowAccessResource {
     * @return WorkflowUserAccessPrivilege value indicating NONE/READ/WRITE
     */
   def getPrivilege(wid: UInteger, uid: UInteger): WorkflowUserAccessPrivilege = {
-    context
+    val access = context
       .select()
       .from(WORKFLOW_USER_ACCESS)
       .where(WORKFLOW_USER_ACCESS.WID.eq(wid).and(WORKFLOW_USER_ACCESS.UID.eq(uid)))
       .fetchOneInto(classOf[WorkflowUserAccess])
-      .getPrivilege
+    if (access == null) {
+      WorkflowUserAccessPrivilege.NONE
+    } else {
+      access.getPrivilege
+    }
   }
 }
 
