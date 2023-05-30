c31bd1d43f32410ccc7f3569ceb937582593b953
Jingchao Feng <fjch1997@outlook.com>
2023-05-30 14:18:15 -0700

    Fix formatting

--- core/new-gui/src/app/dashboard/user/component/user-workflow/user-workflow-list-item/dashboard-workflow-entry-view-model.ts
+++ core/new-gui/src/app/dashboard/user/component/user-workflow/user-workflow-list-item/dashboard-workflow-entry-view-model.ts
@@ -2,4 +2,4 @@ import { DashboardWorkflowEntry } from "../../../type/dashboard-workflow-entry";
 
 export default interface DashboardWorkflowEntryViewModel extends DashboardWorkflowEntry {
   checked: boolean;
-}
+};;;;;;;;;;
