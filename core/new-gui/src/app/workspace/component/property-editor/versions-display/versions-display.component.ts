f10784c83bd72b510334d7af0dd098f90135169d
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2023-05-31 14:22:05 -0700

    move functions into service and fix some typos

--- core/new-gui/src/app/workspace/component/property-editor/versions-display/versions-display.component.ts
+++ core/new-gui/src/app/workspace/component/property-editor/versions-display/versions-display.component.ts
@@ -1,17 +1,8 @@
-import { Component, OnInit } from "@angular/core";
-import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
-import {
-  WorkflowVersionCollapsableEntry,
-  WorkflowVersionEntry,
-} from "../../../../dashboard/user/type/workflow-version-entry";
-import { WorkflowActionService } from "../../../service/workflow-graph/model/workflow-action.service";
-import { WorkflowVersionService } from "../../../../dashboard/user/service/workflow-version/workflow-version.service";
-import { Observable } from "rxjs";
-import { AppSettings } from "../../../../common/app-setting";
-import { HttpClient } from "@angular/common/http";
-import { Workflow } from "src/app/common/type/workflow";
-import { filter, map } from "rxjs/operators";
-import { WorkflowUtilService } from "src/app/workspace/service/workflow-graph/util/workflow-util.service";
+import {Component, OnInit} from "@angular/core";
+import {UntilDestroy, untilDestroyed} from "@ngneat/until-destroy";
+import {WorkflowVersionCollapsableEntry} from "../../../../dashboard/user/type/workflow-version-entry";
+import {WorkflowActionService} from "../../../service/workflow-graph/model/workflow-action.service";
+import {WorkflowVersionService} from "../../../../dashboard/user/service/workflow-version/workflow-version.service";
 
 export const WORKFLOW_VERSIONS_API_BASE_URL = "version";
 
@@ -27,10 +18,10 @@ export class VersionsListDisplayComponent implements OnInit {
   public versionTableHeaders: string[] = ["", "Version#", "Timestamp"];
 
   constructor(
-    private http: HttpClient,
     private workflowActionService: WorkflowActionService,
     private workflowVersionService: WorkflowVersionService
-  ) {}
+  ) {
+  }
 
   collapse(index: number, $event: boolean): void {
     if (this.versionsList == undefined) {
@@ -46,13 +37,14 @@ export class VersionsListDisplayComponent implements OnInit {
       }
     }
   }
+
   ngOnInit(): void {
     // gets the versions result and updates the workflow versions table displayed on the form
     this.displayWorkflowVersions();
   }
 
   getVersion(vid: number) {
-    this.retrieveWorkflowByVersion(<number>this.workflowActionService.getWorkflowMetadata()?.wid, vid)
+    this.workflowVersionService.retrieveWorkflowByVersion(<number>this.workflowActionService.getWorkflowMetadata()?.wid, vid)
       .pipe(untilDestroyed(this))
       .subscribe(workflow => {
         this.workflowVersionService.displayParticularVersion(workflow);
@@ -67,7 +59,7 @@ export class VersionsListDisplayComponent implements OnInit {
     if (wid === undefined) {
       return;
     }
-    this.retrieveVersionsOfWorkflow(wid)
+    this.workflowVersionService.retrieveVersionsOfWorkflow(wid)
       .pipe(untilDestroyed(this))
       .subscribe(versionsList => {
         this.versionsList = versionsList.map(version => ({
@@ -80,24 +72,5 @@ export class VersionsListDisplayComponent implements OnInit {
       });
   }
 
-  /**
-   * retrieves a list of versions for a particular workflow from backend database
-   */
-  retrieveVersionsOfWorkflow(wid: number): Observable<WorkflowVersionEntry[]> {
-    return this.http.get<WorkflowVersionEntry[]>(
-      `${AppSettings.getApiEndpoint()}/${WORKFLOW_VERSIONS_API_BASE_URL}/${wid}`
-    );
-  }
 
-  /**
-   * retrieves a version of the workflow from backend database
-   */
-  retrieveWorkflowByVersion(wid: number, vid: number): Observable<Workflow> {
-    return this.http
-      .get<Workflow>(`${AppSettings.getApiEndpoint()}/${WORKFLOW_VERSIONS_API_BASE_URL}/${wid}/${vid}`)
-      .pipe(
-        filter((updatedWorkflow: Workflow) => updatedWorkflow != null),
-        map(WorkflowUtilService.parseWorkflowInfo)
-      );
-  }
 }
