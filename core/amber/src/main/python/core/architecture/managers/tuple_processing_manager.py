cf9cb85fbda5dbb0e01a1b1f6f4ad57d6a6c9df0
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2023-06-05 01:54:04 -0700

    change type annotation

--- core/amber/src/main/python/core/architecture/managers/tuple_processing_manager.py
+++ core/amber/src/main/python/core/architecture/managers/tuple_processing_manager.py
@@ -13,7 +13,7 @@ class TupleProcessingManager:
             Iterator[Union[Tuple, InputExhausted]]
         ] = None
         self.current_output_tuple: Optional[Tuple] = None
-        self.input_link_map: Mapping[LinkIdentity, int] = None
+        self.input_link_map: Mapping[LinkIdentity, int] = dict()
         self.context_switch_condition: Condition = Condition()
         self.finished_current: Event = Event()
 
