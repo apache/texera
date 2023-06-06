d6c1a30423d377c88d9fcbc8542d463335a7bd2f
Xinyuan Lin <xinyual3@uci.edu>
2023-06-06 13:13:00 -0700

    init

--- core/new-gui/src/app/dashboard/user/type/dashboard-workflow.interface.ts
+++ core/new-gui/src/app/dashboard/user/type/dashboard-workflow.interface.ts
@@ -7,13 +7,3 @@ export interface DashboardWorkflow {
   workflow: Workflow;
   projectIDs: number[];
 }
-
-/**
- * This enum type helps indicate the method in which DashboardWorkflowEntry[] is sorted
- */
-export enum SortMethod {
-  NameAsc,
-  NameDesc,
-  CreateTimeDesc,
-  EditTimeDesc,
-}
