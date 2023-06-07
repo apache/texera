342008f225ac6561409e7f079d1c2a1ced7f71b8
linxinyuan <linxinyuan@gmail.com>
2023-06-06 23:24:21 -0700

    init

--- core/amber/src/main/scala/edu/uci/ics/texera/web/resource/GoogleResource.java
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/resource/GoogleResource.java
@@ -15,10 +15,6 @@ import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
-import edu.uci.ics.texera.Utils;
-
 
 import java.io.*;
 import java.security.GeneralSecurityException;
@@ -29,17 +25,14 @@ import java.util.List;
 public class GoogleResource {
     private static final int GOOGLE_TIMEOUT_IN_MS = 10000;
     private static final String APPLICATION_NAME = "Texera";
-    private static final Config config = ConfigFactory.load("google_api");
-    private static final String TOKENS_DIRECTORY_PATH = Utils.amberHomePath()
-            .resolve("../conf").resolve(config.getString("google.tokenPath")).toString();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
     /**
      * Global instance of the scopes required by this quickstart.
      * If modifying these scopes, delete your previously saved tokens/ folder.
      */
-    private static final String CREDENTIALS_FILE_PATH = Utils.amberHomePath()
-            .resolve("../conf").resolve(config.getString("google.credentialPath")).toString();
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
     private static final List<String> SCOPES = Arrays.asList(SheetsScopes.SPREADSHEETS, DriveScopes.DRIVE);
 
     // singleton service
