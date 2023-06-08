b063b2934da7d69ed5d13c9d4fb7998587621267
linxinyuan <linxinyuan@gmail.com>
2023-06-07 20:05:57 -0700

    merge master

--- core/new-gui/src/app/dashboard/user/type/dashboard-workflow.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Workflow } from "../../../common/type/workflow";
-
-export interface DashboardWorkflow {
-  isOwner: boolean;
-  accessLevel: string;
-  ownerName: string | undefined;
-  workflow: Workflow;
-  projectIDs: number[];
-}
