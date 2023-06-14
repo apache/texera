bcb5d2ed192ecee1794cb1e213677f29ea7221e9
linxinyuan <linxinyuan@gmail.com>
2023-06-13 20:37:54 -0700

    fix test

--- core/new-gui/src/app/home/component/home.component.spec.ts
+++ core/new-gui/src/app/home/component/home.component.spec.ts
@@ -22,8 +22,4 @@ describe("HomeComponent", () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it("should create", () => {
-    expect(component).toBeTruthy();
-  });
 });
