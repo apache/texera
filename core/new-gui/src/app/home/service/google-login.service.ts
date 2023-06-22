ca634fa90d57da6b0e909ea99ae7bd403b14304c
linxinyuan <linxinyuan@gmail.com>
2023-06-21 21:16:49 -0700

    init

--- core/new-gui/src/app/home/service/google-login.service.ts
+++ core/new-gui/src/app/home/service/google-login.service.ts
@@ -4,26 +4,19 @@ import { HttpClient } from "@angular/common/http";
 import { AppSettings } from "../../common/app-setting";
 declare var window: any;
 
-export interface credentialRes {
-  client_id: string;
-  credential: string;
-  select_by: string;
-}
 @Injectable({
   providedIn: "root",
 })
-export class GoogleService {
-  private _googleCredentialResponse = new Subject<credentialRes>();
-
+export class GoogleLoginService {
+  private _googleCredentialResponse = new Subject<any>();
   constructor(private http: HttpClient) {}
-
   public googleInit(parent: HTMLElement | null) {
     this.http.get(`${AppSettings.getApiEndpoint()}/auth/google/clientid`, { responseType: "text" }).subscribe({
       next: response => {
         window.onGoogleLibraryLoad = () => {
           window.google.accounts.id.initialize({
             client_id: response,
-            callback: (auth: credentialRes) => {
+            callback: (auth: any) => {
               this._googleCredentialResponse.next(auth);
             },
           });
