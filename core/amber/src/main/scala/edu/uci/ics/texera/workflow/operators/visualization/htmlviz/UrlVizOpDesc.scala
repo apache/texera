3e0fc1d6e3c640a9a16ecc65471aaeda59f1fdca
yunyad <yunyad1@uci.edu>
2023-07-07 18:11:39 -0700

    url visualization operator

---
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/visualization/htmlviz/UrlVizOpDesc.scala
@@ -0,0 +1,55 @@
+package edu.uci.ics.texera.workflow.operators.visualization.htmlviz
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaTitle
+import edu.uci.ics.amber.engine.architecture.deploysemantics.layer.OpExecConfig
+import edu.uci.ics.texera.workflow.common.metadata.annotations.AutofillAttributeName
+import edu.uci.ics.texera.workflow.common.metadata.{
+  InputPort,
+  OperatorGroupConstants,
+  OperatorInfo,
+  OutputPort
+}
+import edu.uci.ics.texera.workflow.common.tuple.schema.{
+  Attribute,
+  AttributeType,
+  OperatorSchemaInfo,
+  Schema
+}
+import edu.uci.ics.texera.workflow.operators.visualization.{
+  VisualizationConstants,
+  VisualizationOperator
+}
+
+import java.util.Collections.singletonList
+import scala.collection.JavaConverters.asScalaBuffer
+
+/**
+  * HTML Visualization operator to render any given HTML code
+  * This is the description of the operator
+  */
+class UrlVizOpDesc extends VisualizationOperator {
+  @JsonProperty(required = true)
+  @JsonSchemaTitle("URL")
+  @AutofillAttributeName var urlContentAttrName: String = _
+
+  override def chartType: String = VisualizationConstants.HTML_VIZ
+
+  override def operatorExecutor(operatorSchemaInfo: OperatorSchemaInfo) =
+    OpExecConfig.oneToOneLayer(
+      operatorIdentifier,
+      _ => new HtmlVizOpExec(urlContentAttrName, true, operatorSchemaInfo)
+    )
+
+  override def operatorInfo: OperatorInfo =
+    OperatorInfo(
+      "URL visualizer",
+      "Render the content of URL",
+      OperatorGroupConstants.VISUALIZATION_GROUP,
+      asScalaBuffer(singletonList(InputPort(""))).toList,
+      asScalaBuffer(singletonList(OutputPort(""))).toList
+    )
+
+  override def getOutputSchema(schemas: Array[Schema]): Schema =
+    Schema.newBuilder.add(new Attribute("html-content", AttributeType.STRING)).build
+}
