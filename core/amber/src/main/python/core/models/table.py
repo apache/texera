a0c48a36a378a2dec7cef8516738605a6789143e
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2023-07-17 23:36:13 -0700

    fix format

--- core/amber/src/main/python/core/models/table.py
+++ core/amber/src/main/python/core/models/table.py
@@ -1,4 +1,4 @@
-from typing import Protocol, Sized, Container, Iterator
+from typing import Protocol, Sized, Container, Iterator, Callable
 
 import pandas
 
@@ -22,32 +22,33 @@ class Table(TableLike):
         if isinstance(table_like, pandas.DataFrame):
             self.__data_frame = table_like
         elif isinstance(table_like, list):
-            self.__data_frame = pandas.DataFrame.from_records((i.as_dict() for i in
-                                                               table_like))
+            self.__data_frame = pandas.DataFrame.from_records(
+                (i.as_dict() for i in table_like)
+            )
 
     def __getattr__(self, item):
         return self.__data_frame.__getattr__(item)
 
-    def __contains__(self, item):
+    def __contains__(self, item) -> bool:
         return self.__data_frame.__contains__(item)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self.__data_frame.__len__()
 
-    def __str__(self):
+    def __str__(self) -> str:
         return "Table " + self.__data_frame.__str__()
-
-    def as_tuples(self) -> Iterator[Tuple]:
-        for t in self.__data_frame.itertuples():
-            d = t._asdict()
-            del d['Index']
-            yield Tuple(d)
-
-    def as_dataframe(self):
-        return self.__data_frame.__deepcopy__()
 
     def __getitem__(self, item):
         return self.__data_frame.__getitem__(item)
 
-    def __setitem__(self, key, value):
-        self.__data_frame.__setitem__(key, value)
+    def __setitem__(self, key, value) -> None:
+        return self.__data_frame.__setitem__(key, value)
+
+    def as_tuples(self) -> Iterator[Tuple]:
+        for t in self.__data_frame.itertuples():
+            d = t._asdict()
+            del d["Index"]
+            yield Tuple(d)
+
+    def as_dataframe(self) -> pandas.DataFrame:
+        return self.__data_frame.__deepcopy__()
