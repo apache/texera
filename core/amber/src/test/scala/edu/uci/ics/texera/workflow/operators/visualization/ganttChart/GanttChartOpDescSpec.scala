ba13cf05d51e5c4314b68b35b7e5226dc03fe201
ucikrm <mohantyk@uci.edu>
2023-07-25 02:27:33 -0700

    added operators viz3

---
+++ core/amber/src/test/scala/edu/uci/ics/texera/workflow/operators/visualization/ganttChart/GanttChartOpDescSpec.scala
@@ -0,0 +1,15 @@
+package edu.uci.ics.texera.workflow.operators.visualization.ganttChart
+import org.scalatest.BeforeAndAfter
+import org.scalatest.flatspec.AnyFlatSpec
+class GanttChartOpDescSpec extends AnyFlatSpec with BeforeAndAfter{
+  var opDesc: GanttChartOpDesc = _
+  before {
+    opDesc = new GanttChartOpDesc()
+  }
+
+  it should "throw assertion error if value is empty" in {
+    assertThrows[AssertionError] {
+      opDesc.manipulateTable()
+    }
+  }
+}
