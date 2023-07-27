8203eed6cde88f61243097bc8b1c39d898b3891f
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2023-07-26 22:56:21 -0700

    revert changes

--- core/amber/src/main/python/pytexera/udf/examples/test_echo_operator.py
+++ core/amber/src/main/python/pytexera/udf/examples/test_echo_operator.py
@@ -12,8 +12,10 @@ class TestEchoOperator:
     def test_echo_operator(self, echo_operator):
         echo_operator.open()
         tuple_ = Tuple({"test-1": "hello", "test-2": 10})
+
         outputs = echo_operator.process_tuple(tuple_, 0)
         output_tuple = next(outputs)
+
         assert output_tuple == tuple_
         with pytest.raises(StopIteration):
             next(outputs)
