f7a16e7a8c403dda7b5fc5c44a9fec42aba840cf
Xinyuan Lin <xinyual3@uci.edu>
2023-09-08 14:16:22 -0700

    update

--- core/new-gui/src/app/dashboard/admin/component/gmail/gmail.component.ts
+++ core/new-gui/src/app/dashboard/admin/component/gmail/gmail.component.ts
@@ -1,17 +1,55 @@
 import { Component, OnInit } from "@angular/core";
 import { GmailService } from "../../service/gmail.service";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
+@UntilDestroy()
 @Component({
   selector: "texera-gmail",
   templateUrl: "./gmail.component.html",
   styleUrls: ["./gmail.component.scss"],
 })
 export class GmailComponent implements OnInit {
-  constructor(private gmailAuthService: GmailService) {}
+  public validateForm!: FormGroup;
+  isVisible = false;
+  public email: String|undefined;
+  constructor(private gmailAuthService: GmailService,
+              private formBuilder: FormBuilder) {}
+
+  sendEmail(): void {
+    this.gmailAuthService.sendEmail(
+      this.validateForm.value.subject,
+      this.validateForm.value.content,
+      this.validateForm.value.email)
+    this.isVisible = true;
+  }
+
   ngOnInit(): void {
+    this.validateForm = this.formBuilder.group({
+      email: [null, [Validators.email, Validators.required]],
+      subject: [null, [Validators.required]],
+      content: [null, [Validators.required]]
+    });
     this.gmailAuthService.auth();
+    this.getEmail();
+  }
+
+  getEmail() {
+    this.gmailAuthService.getEmail()
+      .pipe(untilDestroyed(this))
+      .subscribe({
+        next: email => this.email = email,
+        error: (err: unknown) => this.email = undefined
+      })
   }
 
   public auth() {
     this.gmailAuthService.client.requestCode();
+    this.gmailAuthService.googleCredentialResponse
+      .pipe(untilDestroyed(this))
+      .subscribe(() => this.getEmail());
+  }
+
+  public revoke() {
+    this.gmailAuthService.deleteEmail().pipe(untilDestroyed(this)).subscribe(() => this.getEmail());
   }
 }
--- core/new-gui/src/app/dashboard/admin/service/gmail.service.ts
+++ core/new-gui/src/app/dashboard/admin/service/gmail.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { AppSettings } from "../../../common/app-setting";
+import { Subject } from "rxjs";
+import { CredentialResponse } from "../../../common/service/user/google-auth.service";
 declare var window: any;
 @Injectable({
   providedIn: "root",
 })
 export class GmailService {
   public client: any;
+  private _googleCredentialResponse = new Subject<CredentialResponse>();
   constructor(private http: HttpClient) {}
   public auth() {
     this.http
@@ -17,13 +20,25 @@ export class GmailService {
           scope: "https://mail.google.com/",
           client_id: response,
           callback: (auth: any) => {
-            this.http.post(`${AppSettings.getApiEndpoint()}/gmail/auth`, `${auth.code}`).subscribe();
+            this.http.post(`${AppSettings.getApiEndpoint()}/gmail/auth`, `${auth.code}`).subscribe(() => this._googleCredentialResponse.next(auth));
           },
         });
       });
   }
 
-  public sendEmail(title: string, content: string) {
-     this.http.put(`${AppSettings.getApiEndpoint()}/gmail/send`,{title: title, content: content}).subscribe();
+  public sendEmail(subject: string, content: string, email: string= "") {
+     this.http.put(`${AppSettings.getApiEndpoint()}/gmail/send`,{email: email, subject: subject, content: content}).subscribe();
+  }
+
+  public getEmail() {
+    return this.http.get(`${AppSettings.getApiEndpoint()}/gmail/email`, { responseType: "text" })
+  }
+
+  public deleteEmail() {
+    return this.http.delete(`${AppSettings.getApiEndpoint()}/gmail/revoke`)
+  }
+
+  get googleCredentialResponse() {
+    return this._googleCredentialResponse.asObservable();
   }
 }
