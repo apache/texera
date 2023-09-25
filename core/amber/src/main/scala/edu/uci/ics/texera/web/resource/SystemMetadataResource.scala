7460eda7fbbbed9efb94a869f739f720aa75956a
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2023-09-25 22:50:41 +0800

    require no auth on metadata

--- core/amber/src/main/scala/edu/uci/ics/texera/web/resource/SystemMetadataResource.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/resource/SystemMetadataResource.scala
@@ -7,13 +7,11 @@ import javax.ws.rs.core.MediaType
 import javax.ws.rs.{GET, Path, Produces}
 
 @Path("/resources")
-@RolesAllowed(Array("REGULAR", "ADMIN"))
 @Produces(Array(MediaType.APPLICATION_JSON))
 class SystemMetadataResource {
 
   @GET
   @Path("/operator-metadata")
-  @RolesAllowed(Array("REGULAR", "ADMIN"))
   def getOperatorMetadata: AllOperatorMetadata = {
     OperatorMetadataGenerator.allOperatorMetadata
   }
