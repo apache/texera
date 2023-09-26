d351e497700d932929b49eb364db6c02b021f145
Xinyuan Lin <xinyual3@uci.edu>
2023-09-26 15:33:52 -0700

    update

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/ifstatement/IfStatementOpDesc.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/ifstatement/IfStatementOpDesc.scala
@@ -1,9 +1,6 @@
 package edu.uci.ics.texera.workflow.operators.ifstatement
 
-import com.fasterxml.jackson.annotation.JsonProperty
-import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaTitle
 import edu.uci.ics.amber.engine.architecture.deploysemantics.layer.OpExecConfig
-import edu.uci.ics.amber.engine.common.virtualidentity.LinkIdentity
 import edu.uci.ics.texera.workflow.common.metadata.{
   InputPort,
   OperatorGroupConstants,
@@ -12,41 +9,14 @@ import edu.uci.ics.texera.workflow.common.metadata.{
 }
 import edu.uci.ics.texera.workflow.common.operators.OperatorDescriptor
 import edu.uci.ics.texera.workflow.common.tuple.schema.{OperatorSchemaInfo, Schema}
-import edu.uci.ics.texera.workflow.common.workflow.PhysicalPlan
-import edu.uci.ics.texera.workflow.operators.udf.python.PythonUDFOpExecV2
 class IfStatementOpDesc extends OperatorDescriptor {
-  @JsonProperty(required = true)
-  @JsonSchemaTitle("Expression")
-  var exp: String = _
-  def generatePythonCode: String = {
-    s"""
-from pytexera import *
-class ProcessTableOperator(UDFTableOperator):
-    @overrides
-    def process_table(self, table: Table, port: int) -> Iterator[Optional[TableLike]]:
-        yield {"condition":$exp}
-"""
-  }
   override def operatorExecutor(operatorSchemaInfo: OperatorSchemaInfo): OpExecConfig = {
     OpExecConfig
       .manyToOneLayer(operatorIdentifier, _ => new IfStatementOpExec())
-      .copy(blockingInputs = List(0), dependency = Map(1 -> 0))
-  }
-
-  override def operatorExecutorMultiLayer(operatorSchemaInfo: OperatorSchemaInfo): PhysicalPlan = {
-    val firstLayer = OpExecConfig.manyToOneLayer(
-      operatorIdentifier,
-      _ => new PythonUDFOpExecV2(generatePythonCode, operatorSchemaInfo.outputSchemas.head)
-    )
-
-    val finalLayer = OpExecConfig
-      .manyToOneLayer(operatorIdentifier, _ => new IfStatementOpExec())
-      .copy(blockingInputs = List(0), dependency = Map(1 -> 0))
-
-    new PhysicalPlan(
-      List(firstLayer, finalLayer),
-      List(LinkIdentity(firstLayer.id, finalLayer.id))
-    )
+      .copy(
+        blockingInputs = List(0),
+        dependency = Map(1 -> 0)
+      )
   }
 
   override def operatorInfo: OperatorInfo =
