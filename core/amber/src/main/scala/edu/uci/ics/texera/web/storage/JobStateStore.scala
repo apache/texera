4d97cfc95f855399f13e7c3169c8da70a58c75f5
Shengquan Ni <shengqun@uci.edu>
2023-09-28 17:11:27 +0800

    Update JobStateStore.scala

--- core/amber/src/main/scala/edu/uci/ics/texera/web/storage/JobStateStore.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/storage/JobStateStore.scala
@@ -1,6 +1,6 @@
 package edu.uci.ics.texera.web.storage
 
-import edu.uci.ics.texera.web.service.{ExecutionsMetadataPersistService, WorkflowService}
+import edu.uci.ics.texera.web.service.ExecutionsMetadataPersistService
 
 import edu.uci.ics.texera.web.workflowruntimestate.{
   JobBreakpointStore,
