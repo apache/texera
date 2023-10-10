74f23273350145e9785b56b85677e4bbe3ca3050
Xinyuan Lin <xinyual3@uci.edu>
2023-10-10 16:52:38 -0700

    Update gmail.service.ts

--- core/new-gui/src/app/dashboard/admin/service/gmail.service.ts
+++ core/new-gui/src/app/dashboard/admin/service/gmail.service.ts
@@ -36,9 +36,9 @@ export class GmailService {
     return this.http.get(`${AppSettings.getApiEndpoint()}/gmail/sender/email`, { responseType: "text" });
   }
 
-  public sendEmail(subject: string, content: string, email: string = "") {
+  public sendEmail(subject: string, content: string, receiver: string = "") {
     this.http
-      .put(`${AppSettings.getApiEndpoint()}/gmail/send`, { email: email, subject: subject, content: content })
+      .put(`${AppSettings.getApiEndpoint()}/gmail/send`, { receiver: receiver, subject: subject, content: content })
       .subscribe({
         next: () => this.notificationService.success("Email sent successfully"),
         error: (error: unknown) => {
