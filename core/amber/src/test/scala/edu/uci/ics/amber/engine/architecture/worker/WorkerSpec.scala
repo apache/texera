aae62982f61f7819b6aa8f008ea22b922811fa42
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2023-10-30 14:13:36 -0700

    first commit

--- core/amber/src/test/scala/edu/uci/ics/amber/engine/architecture/worker/WorkerSpec.scala
+++ core/amber/src/test/scala/edu/uci/ics/amber/engine/architecture/worker/WorkerSpec.scala
@@ -162,7 +162,7 @@ class WorkerSpec
             msgID,
             WorkflowControlMessage(_, _, ReturnInvocation(id, returnValue))
           ) =>
-        probe.sender() ! NetworkAck(msgID, Some(Constants.unprocessedBatchesSizeLimitPerSender))
+        probe.sender() ! NetworkAck(msgID, Some(Constants.unprocessedBatchesSizeLimitInBytesPerWorkerPair))
         returnValue match {
           case e: Throwable => throw e
           case _ =>
