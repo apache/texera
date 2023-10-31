bcbe737653ade0f6104669f736867ad57276d171
Xinyuan Lin <xinyual3@uci.edu>
2023-10-30 19:50:43 -0700

    fix format

--- core/new-gui/src/app/dashboard/user/component/flarum/flarum.component.ts
+++ core/new-gui/src/app/dashboard/user/component/flarum/flarum.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Component, OnInit } from "@angular/core";
+import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
 import { FlarumService } from "../../service/flarum/flarum.service";
-import { catchError } from 'rxjs/operators';
-import { of } from 'rxjs';
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { catchError } from "rxjs/operators";
+import { of } from "rxjs";
+import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
 
 @UntilDestroy()
 @Component({
-  selector: 'app-flarum',
-  templateUrl: './flarum.component.html',
-  styleUrls: ['./flarum.component.scss']
+  selector: "texera-app-flarum",
+  templateUrl: "./flarum.component.html",
+  styleUrls: ["./flarum.component.scss"],
 })
 export class FlarumComponent implements OnInit {
   flarumUrl: SafeResourceUrl | undefined;
 
-  constructor( private sanitizer: DomSanitizer, private flarumService: FlarumService) { }
+  constructor(private sanitizer: DomSanitizer, private flarumService: FlarumService) {}
 
   ngOnInit(): void {
     this.authenticateAndRedirect();
@@ -24,22 +24,25 @@ export class FlarumComponent implements OnInit {
     const rawUrl = "http://localhost:80";
     if (!this.isFlarumAuthenticated()) {
       console.log("Not authenticated, redirecting to Flarum");
-      this.flarumService.authenticateAndRedirect().pipe(
-        catchError(err => {
-          console.error("Error during authentication:", err);
-          return of(null);
-        }),
-        untilDestroyed(this) // Auto-unsubscribe when the component is destroyed
-      ).subscribe(() => {
-        this.flarumUrl = this.sanitizer.bypassSecurityTrustResourceUrl(rawUrl);
-        console.log("Successfully authenticated");
-      });
+      this.flarumService
+        .authenticateAndRedirect()
+        .pipe(
+          catchError((err: unknown) => {
+            console.error("Error during authentication:", err);
+            return of(null);
+          }),
+          untilDestroyed(this) // Auto-unsubscribe when the component is destroyed
+        )
+        .subscribe(() => {
+          this.flarumUrl = this.sanitizer.bypassSecurityTrustResourceUrl(rawUrl);
+          console.log("Successfully authenticated");
+        });
     } else {
       this.flarumUrl = this.sanitizer.bypassSecurityTrustResourceUrl(rawUrl);
     }
   }
 
   isFlarumAuthenticated(): boolean {
-    return document.cookie.includes('flarum_remember');
+    return document.cookie.includes("flarum_remember");
   }
 }
--- core/new-gui/src/app/dashboard/user/service/flarum/flarum.service.ts
+++ core/new-gui/src/app/dashboard/user/service/flarum/flarum.service.ts
@@ -1,17 +1,16 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
-import { UserService } from '../../../../common/service/user/user.service';
-import { Observable, of, throwError } from 'rxjs';
-import { switchMap, catchError } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { UserService } from "../../../../common/service/user/user.service";
+import { Observable, of, throwError } from "rxjs";
+import { switchMap, catchError } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class FlarumService {
-  constructor(private http: HttpClient, private userService: UserService) { }
+  constructor(private http: HttpClient, private userService: UserService) {}
 
-  createFlarumUser(username: string, identification: string, password: string):Observable<any> {
+  createFlarumUser(username: string, identification: string, password: string): Observable<any> {
     const url = "http://localhost:80/api/users";
     const body = {
       data: {
@@ -23,12 +22,12 @@ export class FlarumService {
       },
     };
     const headers = {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       Authorization: `Token ${"http://localhost:80/api/token"}`,
     };
     return this.http.post(url, body, { headers }).pipe(
-      catchError(error => {
-        console.error('Error creating Flarum user:', error);
+      catchError((error: unknown) => {
+        console.error("Error creating Flarum user:", error);
         return throwError(error);
       })
     );
@@ -37,37 +36,39 @@ export class FlarumService {
   authenticateAndRedirect(): Observable<any> {
     const currentUser = this.userService.getCurrentUser();
     if (!currentUser) {
-      console.error('No user is currently logged in');
+      console.error("No user is currently logged in");
       return of(null);
     }
 
-    const username = currentUser.name.replace(/[^a-zA-Z0-9-]/g, '');
+    const username = currentUser.name.replace(/[^a-zA-Z0-9-]/g, "");
     const identification = currentUser.email;
     const password = "myPassword";
 
-    return this.http.post(
-      "http://localhost:80/public/api/token",
-      { identification, password, remember: '1' },
-      { headers: { 'Content-Type': 'application/json' }, withCredentials: true }
-    ).pipe(
-      switchMap((tokenResponse: any) => {
-        if (tokenResponse.errors) {
-          return this.createFlarumUser(username, identification, password).pipe(
-            switchMap(response => {
-              if (response && response.status === 200) {
-                return this.authenticateAndRedirect();
-              }
-              return of(null);
-            })
-          );
-        }
-        document.cookie = `flarum_remember=${tokenResponse.token};path=/`;
-        return of(null);
-      }),
-      catchError(error => {
-        console.error("Error during authentication:", error);
-        return throwError(error);
-      })
-    );
+    return this.http
+      .post(
+        "http://localhost:80/public/api/token",
+        { identification, password, remember: "1" },
+        { headers: { "Content-Type": "application/json" }, withCredentials: true }
+      )
+      .pipe(
+        switchMap((tokenResponse: any) => {
+          if (tokenResponse.errors) {
+            return this.createFlarumUser(username, identification, password).pipe(
+              switchMap(response => {
+                if (response && response.status === 200) {
+                  return this.authenticateAndRedirect();
+                }
+                return of(null);
+              })
+            );
+          }
+          document.cookie = `flarum_remember=${tokenResponse.token};path=/`;
+          return of(null);
+        }),
+        catchError((error: unknown) => {
+          console.error("Error during authentication:", error);
+          return throwError(error);
+        })
+      );
   }
 }
