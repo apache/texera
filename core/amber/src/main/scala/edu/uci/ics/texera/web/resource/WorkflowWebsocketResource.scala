apache/texera

commit 6ca08d9e959f994ea1b6b9f0eff8a0cfe8a754cf

Enhance frontend error reporting (#2195)
This PR improves the front-end error reporting process. Here are a few
important notes:

### We now differentiate two types of errors:
1. (Editing-time) Static Errors: shown during the editing time of a
workflow.
- Compilation Error: **Unfixable**, an error happens during 1) operator
compilation, 2) workflow compilation, and 3) schema propagation. Those
errors can be updated while user edits a workflow.
2. Runtime Errors: describes errors that happen during an execution.
- Runtime Exception: **Fixable**, can retry, debug, etc. The original
Breakpoints for local operator exceptions are refactored as runtime
exceptions.
- Execution Failure: **Unixable**, the runtime is dead. This includes
errors that happen during the initiating of a worker and its operator,
e.g., Python code compilation, PVM handshake failure, actor crash, etc.

### Error view on the UI:

- This PR adds a new global view of the errors for the entire workflow
when the user unhighlights any specific operator.

- For each specific operator, its errors will be displayed in different
components:
1. Unfixable errors, including compilation errors and execution
failures, will be displayed in the Static Error Frame of each operator.
2. Fixable errors, currently only include runtime errors, will be
displayed as a console message in the Console Frame of each operator.

### Demo:
- The global Static Error Frame, and each specific operator's Static
Error Frame.
<p align="center"><img
src="https://github.com/Texera/texera/assets/17627829/748c998c-3395-469f-a5fe-1cd32d352014"
width="600" height="350" /></p>

- The runtime exceptions are displayed in the corresponding operator's
Console Frame.
<p align="center"><img
src="https://github.com/Texera/texera/assets/17627829/908a2cc6-c45b-4150-9472-df41ef1cb2e9"
width="600" height="350" /></p>

---------

Co-authored-by: Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>

core/amber/src/main/scala/edu/uci/ics/texera/web/resource/WorkflowWebsocketResource.scala
@@ -1,5 +1,6 @@
 package edu.uci.ics.texera.web.resource
 
+import com.google.protobuf.timestamp.Timestamp
 import com.typesafe.scalalogging.LazyLogging
 import edu.uci.ics.amber.clustering.ClusterListener
 import edu.uci.ics.texera.Utils.objectMapper
@@ -8,9 +9,15 @@ import edu.uci.ics.texera.web.model.websocket.event.{WorkflowErrorEvent, Workflo
 import edu.uci.ics.texera.web.model.websocket.request._
 import edu.uci.ics.texera.web.model.websocket.response._
 import edu.uci.ics.texera.web.service.{WorkflowCacheChecker, WorkflowService}
+import edu.uci.ics.texera.web.storage.JobStateStore
+import edu.uci.ics.texera.web.workflowruntimestate.FatalErrorType.COMPILATION_ERROR
+import edu.uci.ics.texera.web.workflowruntimestate.WorkflowAggregatedState.{FAILED, PAUSED, RUNNING}
+import edu.uci.ics.texera.web.workflowruntimestate.WorkflowFatalError
 import edu.uci.ics.texera.web.{ServletAwareConfigurator, SessionState}
-import edu.uci.ics.texera.workflow.common.workflow.WorkflowCompiler.ConstraintViolationException
+import edu.uci.ics.texera.workflow.common.WorkflowContext
+import edu.uci.ics.texera.workflow.common.workflow.LogicalPlan
 
+import java.time.Instant
 import java.util.concurrent.atomic.AtomicInteger
 import javax.websocket._
 import javax.websocket.server.ServerEndpoint
@@ -71,21 +78,64 @@ class WorkflowWebsocketResource extends LazyLogging {
               jobService.jobReconfigurationService.modifyOperatorLogic(modifyLogicRequest)
             sessionState.send(modifyLogicResponse)
           }
-        case cacheStatusUpdateRequest: CacheStatusUpdateRequest =>
-          WorkflowCacheChecker.handleCacheStatusUpdateRequest(session, cacheStatusUpdateRequest)
-        case other =>
+        case editingTimeCompilationRequest: EditingTimeCompilationRequest =>
+          if (workflowStateOpt.isDefined) {
+            var stateStore = new JobStateStore()
+            if (workflowStateOpt.get.jobService.hasValue) {
+              val currentState =
+                workflowStateOpt.get.jobService.getValue.stateStore.jobMetadataStore.getState.state
+              if (currentState == RUNNING || currentState == PAUSED) {
+                // disable check if the workflow execution is active.
+                return
+              }
+              stateStore = workflowStateOpt.get.jobService.getValue.stateStore
+            }
+            val newPlan = {
+              LogicalPlan.apply(
+                editingTimeCompilationRequest.toLogicalPlanPojo(),
+                new WorkflowContext()
+              )
+            }
+            newPlan.initializeLogicalPlan(stateStore)
+            if (stateStore.jobMetadataStore.getState.state == FAILED) {
+              sessionState.send(WorkflowStateEvent("Failed"))
+              sessionState.send(
+                WorkflowErrorEvent(stateStore.jobMetadataStore.getState.fatalErrors)
+              )
+            } else {
+              WorkflowCacheChecker.handleCacheStatusUpdate(
+                workflowStateOpt.get.lastCompletedLogicalPlan,
+                newPlan,
+                sessionState,
+                editingTimeCompilationRequest
+              )
+            }
+          }
+        case workflowExecuteRequest: WorkflowExecuteRequest =>
           workflowStateOpt match {
-            case Some(workflow) => workflow.wsInput.onNext(other, uidOpt)
+            case Some(workflow) => workflow.initJobService(workflowExecuteRequest, uidOpt)
             case None           => throw new IllegalStateException("workflow is not initialized")
+          }
+        case other =>
+          workflowStateOpt.map(_.jobService.getValue) match {
+            case Some(value) => value.wsInput.onNext(other, uidOpt)
+            case None        => throw new IllegalStateException("workflow job is not initialized")
           }
       }
     } catch {
-      case x: ConstraintViolationException =>
-        sessionState.send(WorkflowErrorEvent(operatorErrors = x.violations))
       case err: Exception =>
+        sessionState.send(WorkflowStateEvent("Failed"))
         sessionState.send(
-          WorkflowErrorEvent(generalErrors =
-            Map("exception" -> (err.getMessage + "\n" + err.getStackTrace.mkString("\n")))
+          WorkflowErrorEvent(
+            Seq(
+              WorkflowFatalError(
+                COMPILATION_ERROR,
+                Timestamp(Instant.now),
+                err.toString,
+                err.getStackTrace.mkString("\n"),
+                "unknown operator"
+              )
+            )
           )
         )
         throw err

core/amber/src/main/scala/edu/uci/ics/texera/web/service/JobStatsService.scala
@@ -1,5 +1,6 @@
 package edu.uci.ics.texera.web.service
 
+import com.google.protobuf.timestamp.Timestamp
 import edu.uci.ics.amber.engine.architecture.controller.ControllerEvent.{
   WorkerAssignmentUpdate,
   WorkflowCompleted,
@@ -7,6 +8,7 @@ import edu.uci.ics.amber.engine.architecture.controller.ControllerEvent.{
   WorkflowStatusUpdate
 }
 import edu.uci.ics.amber.engine.architecture.controller.promisehandlers.FatalErrorHandler.FatalError
+import edu.uci.ics.amber.engine.common.VirtualIdentityUtils
 import edu.uci.ics.amber.engine.common.client.AmberClient
 import edu.uci.ics.texera.Utils
 import edu.uci.ics.texera.web.SubscriptionManager
@@ -18,8 +20,11 @@ import edu.uci.ics.texera.web.model.websocket.event.{
 }
 import edu.uci.ics.texera.web.storage.JobStateStore
 import edu.uci.ics.texera.web.storage.JobStateStore.updateWorkflowState
-import edu.uci.ics.texera.web.workflowruntimestate.OperatorWorkerMapping
-import edu.uci.ics.texera.web.workflowruntimestate.WorkflowAggregatedState.{FAILED, COMPLETED}
+import edu.uci.ics.texera.web.workflowruntimestate.FatalErrorType.{EXECUTION_FAILURE}
+import edu.uci.ics.texera.web.workflowruntimestate.{OperatorWorkerMapping, WorkflowFatalError}
+import edu.uci.ics.texera.web.workflowruntimestate.WorkflowAggregatedState.{COMPLETED, FAILED}
+
+import java.time.Instant
 
 class JobStatsService(
     client: AmberClient,
@@ -156,11 +161,26 @@ class JobStatsService(
       client
         .registerCallback[FatalError]((evt: FatalError) => {
           client.shutdown()
+          var opeartorId = "unknown operator"
+          var workerId = ""
+          if (evt.fromActor.isDefined) {
+            opeartorId = VirtualIdentityUtils.getOperator(evt.fromActor.get).operator
+            workerId = evt.fromActor.get.name
+          }
           stateStore.statsStore.updateState(stats =>
             stats.withEndTimeStamp(System.currentTimeMillis())
           )
           stateStore.jobMetadataStore.updateState { jobInfo =>
-            updateWorkflowState(FAILED, jobInfo).withError(evt.e.getLocalizedMessage)
+            updateWorkflowState(FAILED, jobInfo).addFatalErrors(
+              WorkflowFatalError(
+                EXECUTION_FAILURE,
+                Timestamp(Instant.now),
+                evt.e.toString,
+                evt.e.getStackTrace.mkString("\n"),
+                opeartorId,
+                workerId
+              )
+            )
           }
         })
     )

core/amber/src/main/scala/edu/uci/ics/texera/web/service/WorkflowService.scala
@@ -5,8 +5,8 @@ import com.typesafe.scalalogging.LazyLogging
 import edu.uci.ics.amber.engine.common.AmberUtils
 
 import scala.collection.JavaConverters._
-import edu.uci.ics.texera.web.model.websocket.event.{TexeraWebSocketEvent, WorkflowErrorEvent}
-import edu.uci.ics.texera.web.{SubscriptionManager, WebsocketInput, WorkflowLifecycleManager}
+import edu.uci.ics.texera.web.model.websocket.event.TexeraWebSocketEvent
+import edu.uci.ics.texera.web.{SubscriptionManager, WorkflowLifecycleManager}
 import edu.uci.ics.texera.web.model.websocket.request.{WorkflowExecuteRequest, WorkflowKillRequest}
 import edu.uci.ics.texera.web.resource.WorkflowWebsocketResource
 import edu.uci.ics.texera.web.service.WorkflowService.mkWorkflowStateId
@@ -55,17 +55,6 @@ class WorkflowService(
   // state across execution:
   var opResultStorage: OpResultStorage = new OpResultStorage()
   private val errorSubject = BehaviorSubject.create[TexeraWebSocketEvent]().toSerialized
-  val errorHandler: Throwable => Unit = { t =>
-    {
-      t.printStackTrace()
-      errorSubject.onNext(
-        WorkflowErrorEvent(generalErrors =
-          Map("error" -> (t.getMessage + "\n" + t.getStackTrace.mkString("\n")))
-        )
-      )
-    }
-  }
-  val wsInput = new WebsocketInput(errorHandler)
   val stateStore = new WorkflowStateStore()
   var jobService: BehaviorSubject[WorkflowJobService] = BehaviorSubject.create()
 
@@ -79,7 +68,9 @@ class WorkflowService(
     () => {
       opResultStorage.close()
       WorkflowService.wIdToWorkflowState.remove(mkWorkflowStateId(wId))
-      wsInput.onNext(WorkflowKillRequest(), None)
+      if (jobService.getValue != null) {
+        jobService.getValue.wsInput.onNext(WorkflowKillRequest(), None)
+      }
       unsubscribeAll()
     }
   )
@@ -93,15 +84,12 @@ class WorkflowService(
           if (oldState.state != COMPLETED && newState.state == COMPLETED) {
             lastCompletedLogicalPlan = Option.apply(job.workflowCompiler.logicalPlan)
           }
+
           Iterable.empty
         }
       }
     }
   }
-
-  addSubscription(
-    wsInput.subscribe((evt: WorkflowExecuteRequest, uidOpt) => initJobService(evt, uidOpt))
-  )
 
   def connect(onNext: TexeraWebSocketEvent => Unit): Disposable = {
     lifeCycleManager.increaseUserCount()
@@ -158,16 +146,16 @@ class WorkflowService(
 
     val job = new WorkflowJobService(
       workflowContext,
-      wsInput,
       resultService,
       req,
-      errorHandler,
       lastCompletedLogicalPlan
     )
 
     lifeCycleManager.registerCleanUpOnStateChange(job.stateStore)
     jobService.onNext(job)
-    job.startWorkflow()
+    if (job.stateStore.jobMetadataStore.getState.fatalErrors.isEmpty) {
+      job.startWorkflow()
+    }
   }
 
   def convertToJson(frontendVersion: String): String = {

commit 0deeb640022ef1609b3f23335b86d5fff898234f

Fix error during editingTimeCompilationRequest and error display with cause (#2219)
This PR fixes issues caused by #2195.
- during editingTimeCompilationRequest, the workflow was analyzed
without the context (which could contain user information). Now, we give
proper workflow context for it to analyze.
- when a `WorkflowFatalError` is caused by another underlying error, we
now display the underlying error with "caused by:" in the frontend as
well to help users better understand the root cause.

core/amber/src/main/scala/edu/uci/ics/texera/web/resource/WorkflowWebsocketResource.scala
@@ -16,6 +16,7 @@ import edu.uci.ics.texera.web.workflowruntimestate.WorkflowFatalError
 import edu.uci.ics.texera.web.{ServletAwareConfigurator, SessionState}
 import edu.uci.ics.texera.workflow.common.WorkflowContext
 import edu.uci.ics.texera.workflow.common.workflow.LogicalPlan
+import org.jooq.types.UInteger
 
 import java.time.Instant
 import java.util.concurrent.atomic.AtomicInteger
@@ -90,10 +91,16 @@ class WorkflowWebsocketResource extends LazyLogging {
               }
               stateStore = workflowStateOpt.get.jobService.getValue.stateStore
             }
+
+            val workflowContext = new WorkflowContext(
+              null,
+              uidOpt,
+              UInteger.valueOf(sessionState.getCurrentWorkflowState.get.wId)
+            )
             val newPlan = {
               LogicalPlan.apply(
                 editingTimeCompilationRequest.toLogicalPlanPojo(),
-                new WorkflowContext()
+                workflowContext
               )
             }
             newPlan.initializeLogicalPlan(stateStore)

core/amber/src/main/scala/edu/uci/ics/texera/web/service/JobStatsService.scala
@@ -163,10 +163,10 @@ class JobStatsService(
       client
         .registerCallback[FatalError]((evt: FatalError) => {
           client.shutdown()
-          var opeartorId = "unknown operator"
+          var operatorId = "unknown operator"
           var workerId = ""
           if (evt.fromActor.isDefined) {
-            opeartorId = VirtualIdentityUtils.getOperator(evt.fromActor.get).operator
+            operatorId = VirtualIdentityUtils.getOperator(evt.fromActor.get).operator
             workerId = evt.fromActor.get.name
           }
           stateStore.statsStore.updateState(stats =>
@@ -179,8 +179,11 @@ class JobStatsService(
                 EXECUTION_FAILURE,
                 Timestamp(Instant.now),
                 evt.e.toString,
-                evt.e.getStackTrace.mkString("\n"),
-                opeartorId,
+                evt.e.getStackTrace.mkString(
+                  "\n"
+                ) + "\n\nCaused by:\n" + evt.e.getCause.toString + "\n" + evt.e.getCause.getStackTrace
+                  .mkString("\n"),
+                operatorId,
                 workerId
               )
             )

core/amber/src/main/scala/edu/uci/ics/texera/web/service/WorkflowService.scala
@@ -48,7 +48,7 @@ object WorkflowService {
 }
 
 class WorkflowService(
-    wId: Int,
+    val wId: Int,
     cleanUpTimeout: Int
 ) extends SubscriptionManager
     with LazyLogging {