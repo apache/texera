3ac334a8fbef8b68a30d5b1b41a8c09d87198412
linxinyuan <linxinyuan@gmail.com>
2023-11-27 22:54:19 -0800

    fix comments

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/source/scan/text/TextSourceOpDesc.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/source/scan/text/TextSourceOpDesc.scala
@@ -7,9 +7,10 @@ import edu.uci.ics.texera.workflow.common.metadata.annotations.HideAnnotation
 import edu.uci.ics.texera.workflow.operators.source.scan.FileAttributeType
 
 /**
-  * TextSourceOpDesc is a trait holding commonly used properties and functions
-  * used for variations of text input processing
-  */
+ * TextSourceOpDesc is a trait holding commonly used properties and functions used for variations of text input processing
+ * Create new, identical limit and offset fields with additional annotations to make hideable binary attributes
+ * and strings that are in outputAsSingleTuple mode will always read the entire input, so limit / offset are disabled in these cases
+ */
 trait TextSourceOpDesc {
   @JsonProperty(defaultValue = "string", required = true)
   @JsonSchemaTitle("Attribute Type")
