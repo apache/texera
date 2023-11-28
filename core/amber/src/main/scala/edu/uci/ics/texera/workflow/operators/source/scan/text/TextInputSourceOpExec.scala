bd5445453aa5dd5f8a359c78a7784943ad2037d2
linxinyuan <linxinyuan@gmail.com>
2023-11-27 23:41:56 -0800

    fix

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/source/scan/text/TextInputSourceOpExec.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/source/scan/text/TextInputSourceOpExec.scala
@@ -2,36 +2,33 @@ package edu.uci.ics.texera.workflow.operators.source.scan.text
 
 import edu.uci.ics.texera.workflow.common.operators.source.SourceOperatorExecutor
 import edu.uci.ics.texera.workflow.common.tuple.Tuple
-import edu.uci.ics.texera.workflow.common.tuple.schema.AttributeTypeUtils
+import edu.uci.ics.texera.workflow.common.tuple.schema.AttributeTypeUtils.parseField
 import edu.uci.ics.texera.workflow.operators.source.scan.FileAttributeType
 
 class TextInputSourceOpExec private[text] (val desc: TextInputSourceOpDesc)
     extends SourceOperatorExecutor {
 
   override def produceTexeraTuple(): Iterator[Tuple] = {
-    if (desc.attributeType.isSingle) {
-      Iterator(
-        new Tuple(
-          desc.sourceSchema(),
+    (if (desc.attributeType.isSingle) {
+       Iterator(desc.textInput)
+     } else {
+       desc.textInput.linesIterator.slice(
+         desc.fileScanOffset.getOrElse(0),
+         desc.fileScanOffset.getOrElse(0) + desc.fileScanLimit.getOrElse(Int.MaxValue)
+       )
+     }).map(line =>
+      Tuple
+        .newBuilder(desc.sourceSchema())
+        .add(
+          desc.sourceSchema().getAttributes.get(0),
           desc.attributeType match {
-            case FileAttributeType.BINARY        => desc.textInput.getBytes()
-            case FileAttributeType.SINGLE_STRING => desc.textInput
+            case FileAttributeType.SINGLE_STRING => line
+            case FileAttributeType.BINARY        => line.getBytes
+            case _                               => parseField(line, desc.attributeType.getType)
           }
         )
-      )
-    } else {
-      desc.textInput.linesIterator
-        .slice(
-          desc.fileScanOffset.getOrElse(0),
-          desc.fileScanOffset.getOrElse(0) + desc.fileScanLimit.getOrElse(Int.MaxValue)
-        )
-        .map(line =>
-          new Tuple(
-            desc.sourceSchema(),
-            AttributeTypeUtils.parseField(line, desc.attributeType.getType)
-          )
-        )
-    }
+        .build()
+    )
   }
 
   override def open(): Unit = {}
