c0e527d2404dd89894b3318d1fb0ee1a0bf31731
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2023-12-04 22:07:56 -0800

    fix format

--- core/amber/src/main/scala/edu/uci/ics/texera/web/model/websocket/request/SkipTupleRequest.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/model/websocket/request/SkipTupleRequest.scala
@@ -1,6 +1,3 @@
 package edu.uci.ics.texera.web.model.websocket.request
 
-import edu.uci.ics.texera.web.model.common.FaultedTupleFrontend
-
-case class SkipTupleRequest(workerIds: Array[String])
-    extends TexeraWebSocketRequest
+case class SkipTupleRequest(workerIds: Array[String]) extends TexeraWebSocketRequest
