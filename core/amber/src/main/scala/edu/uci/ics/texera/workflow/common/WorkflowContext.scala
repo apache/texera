aa9021f5cf3d5f9dd6912de9c8fa3a406e0dc10e
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2023-12-06 12:18:59 -0800

    revert executionID to -1

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/common/WorkflowContext.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/common/WorkflowContext.scala
@@ -5,5 +5,5 @@ class WorkflowContext(
     var jobId: String = "default",
     var userId: Option[UInteger] = None,
     var wId: UInteger = UInteger.valueOf(0),
-    var executionID: Long = 0
+    var executionID: Long = -1
 )
