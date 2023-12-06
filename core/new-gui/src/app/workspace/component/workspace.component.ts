7ad204d085383d42313d7142f73bfc2700d5ec2f
linxinyuan <linxinyuan@gmail.com>
2023-12-05 23:25:08 -0800

    add /schemaPropagationService and autoAttributeCorrectionService

--- core/new-gui/src/app/workspace/component/workspace.component.ts
+++ core/new-gui/src/app/workspace/component/workspace.component.ts
@@ -19,6 +19,8 @@ import { isDefined } from "../../common/util/predicate";
 import { NotificationService } from "src/app/common/service/notification/notification.service";
 import { NzResizeEvent } from "ng-zorro-antd/resizable";
 import { Version } from "../../../environments/version";
+import { AutoAttributeCorrectionService } from "../service/dynamic-schema/auto-attribute-correction/auto-attribute-correction.service";
+import { SchemaPropagationService } from "../service/dynamic-schema/schema-propagation/schema-propagation.service";
 
 export const SAVE_DEBOUNCE_TIME_IN_MS = 300;
 
@@ -53,6 +55,9 @@ export class WorkspaceComponent implements AfterViewInit, OnInit, OnDestroy {
   constructor(
     private userService: UserService,
     private resultPanelToggleService: ResultPanelToggleService,
+    // list additional services in constructor so they are initialized even if no one use them directly
+    private schemaPropagationService: SchemaPropagationService,
+    private autoAttributeCorrectionService: AutoAttributeCorrectionService,
     private undoRedoService: UndoRedoService,
     private workflowCacheService: WorkflowCacheService,
     private workflowPersistService: WorkflowPersistService,
