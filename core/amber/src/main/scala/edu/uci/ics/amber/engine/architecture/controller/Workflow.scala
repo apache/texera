e559effc454e0d4668bf422e7858c70f9fca5844
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2023-12-18 11:27:51 -0800

    minor changes

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/controller/Workflow.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/controller/Workflow.scala
@@ -4,10 +4,10 @@ import edu.uci.ics.amber.engine.architecture.scheduling.RegionPlan
 import edu.uci.ics.amber.engine.common.virtualidentity._
 import edu.uci.ics.texera.workflow.common.workflow.{LogicalPlan, PhysicalPlan}
 
-class Workflow(
-    val workflowId: WorkflowIdentity,
-    val originalLogicalPlan: LogicalPlan,
-    val logicalPlan: LogicalPlan,
-    val physicalPlan: PhysicalPlan,
-    val regionPlan: RegionPlan
+case class Workflow(
+    workflowId: WorkflowIdentity,
+    originalLogicalPlan: LogicalPlan,
+    logicalPlan: LogicalPlan,
+    physicalPlan: PhysicalPlan,
+    regionPlan: RegionPlan
 ) extends java.io.Serializable {}
