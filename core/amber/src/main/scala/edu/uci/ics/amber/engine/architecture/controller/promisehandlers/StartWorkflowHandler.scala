90c87fbc85b77a8ee061f6bc8f983767b624114a
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2023-12-20 18:09:12 -0800

    fix merge

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/controller/promisehandlers/StartWorkflowHandler.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/controller/promisehandlers/StartWorkflowHandler.scala
@@ -19,7 +19,7 @@ trait StartWorkflowHandler {
   registerHandler { (msg: StartWorkflow, sender) =>
     {
       cp.workflowScheduler
-        .startWorkflow(cp.workflow, cp.actorService)
+        .startWorkflow(cp.workflow, cp.actorRefService, cp.actorService)
         .map(_ => {
           cp.controllerTimerService.enableStatusUpdate()
           cp.controllerTimerService.enableMonitoring()
