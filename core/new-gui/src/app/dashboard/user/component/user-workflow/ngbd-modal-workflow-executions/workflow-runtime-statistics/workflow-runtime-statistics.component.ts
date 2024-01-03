3bba770eded8d1a02fb1d5e313d76aa83d688c2c
kunwp1 <kunwp1@uci.edu>
2024-01-03 11:51:09 -0800

    Attempt to fix memory leak

--- core/new-gui/src/app/dashboard/user/component/user-workflow/ngbd-modal-workflow-executions/workflow-runtime-statistics/workflow-runtime-statistics.component.ts
+++ core/new-gui/src/app/dashboard/user/component/user-workflow/ngbd-modal-workflow-executions/workflow-runtime-statistics/workflow-runtime-statistics.component.ts
@@ -25,75 +25,12 @@ export class WorkflowRuntimeStatisticsComponent implements OnInit {
       return;
     }
 
-    this.groupedStats = this.groupStatsByOperatorId();
+    //this.groupedStats = this.groupStatsByOperatorId();
     //this.createChart();
   }
 
   onTabChanged(event: MatTabChangeEvent): void {
     this.tab_index = event.index;
     //this.createChart();
-  }
-
-  private groupStatsByOperatorId(): Record<string, WorkflowRuntimeStatistics[]> {
-    return (
-      this.workflowRuntimeStatistics?.reduce((acc: Record<string, WorkflowRuntimeStatistics[]>, stat) => {
-        const statsArray = acc[stat.operatorId] || [];
-        const lastStat = statsArray[statsArray.length - 1];
-
-        if (lastStat) {
-          stat.inputTupleCount += lastStat.inputTupleCount;
-          stat.outputTupleCount += lastStat.outputTupleCount;
-        }
-
-        acc[stat.operatorId] = [...statsArray, stat];
-        return acc;
-      }, {}) || {}
-    );
-  }
-
-  private createDatasets(): any[] {
-    if (!this.groupedStats) {
-      return [];
-    }
-
-    return Object.keys(this.groupedStats)
-      .map(operatorId => {
-        const operatorName = operatorId.split("-")[0];
-        const uuidLast6Digits = operatorId.slice(-6);
-
-        if (operatorName === "ProgressiveSinkOpDesc") {
-          return null;
-        }
-
-        const yValues = this.tab_index === 0 ? "inputTupleCount" : "outputTupleCount";
-        if (!this.groupedStats) {
-          return null;
-        }
-        const stats = this.groupedStats[operatorId];
-
-        return {
-          x: stats.map((_, index) => index * 0.5),
-          y: stats.map(stat => stat[yValues]),
-          mode: "lines",
-          name: `${operatorName}-${uuidLast6Digits}`,
-        };
-      })
-      .filter(Boolean);
-  }
-
-  private createChart(): void {
-    const datasets = this.createDatasets();
-
-    if (!datasets || datasets.length === 0) {
-      return;
-    }
-
-    const layout = {
-      title: this.metrics[this.tab_index],
-      xaxis: { title: "Time (s)" },
-      yaxis: { title: this.metrics[this.tab_index] },
-    };
-
-    Plotly.newPlot("chart", datasets, layout);
   }
 }
