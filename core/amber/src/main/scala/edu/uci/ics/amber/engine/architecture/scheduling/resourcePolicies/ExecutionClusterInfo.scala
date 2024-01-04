a2ebb39cde68966887069aa10b35b9f999d568a4
yunyad <yunyad1@uci.edu>
2024-01-03 21:14:25 -0800

    format

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/scheduling/resourcePolicies/ExecutionClusterInfo.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/scheduling/resourcePolicies/ExecutionClusterInfo.scala
@@ -1,13 +1,9 @@
 package edu.uci.ics.amber.engine.architecture.scheduling.resourcePolicies
 
-import edu.uci.ics.amber.engine.architecture.scheduling.Region
-
 object ExecutionClusterInfo {
   def apply(): ExecutionClusterInfo = {
     ExecutionClusterInfo()
   }
 }
 
-case class ExecutionClusterInfo() {
-
-}
+case class ExecutionClusterInfo() {}
