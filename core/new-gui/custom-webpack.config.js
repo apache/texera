34afcf1c73c2deb76610c9b5bc69a0b296762024
linxinyuan <linxinyuan@gmail.com>
2024-01-26 03:20:14 -0800

    update

--- core/new-gui/custom-webpack.config.js
+++ core/new-gui/custom-webpack.config.js
@@ -1,19 +1,11 @@
-const path = require('path');
-
 module.exports = {
   module: {
     rules: [
       {
-        test: /(codicon.css$)/i,
-        type: 'asset'
-      },
-      {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-        include: [
-          path.resolve(__dirname, 'node_modules/monaco-editor')
-        ]
-      }
-    ]
-  }
+        use: ["style-loader", "css-loader"],
+        include: [require("path").resolve(__dirname, "node_modules/monaco-editor")],
+      },
+    ],
+  },
 };
