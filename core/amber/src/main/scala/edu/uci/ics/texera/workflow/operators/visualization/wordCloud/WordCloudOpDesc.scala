920667d8990eb4a0f82fd603f7508e7671bfb5d8
linxinyuan <linxinyuan@gmail.com>
2024-02-08 16:38:10 -0800

    init

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/visualization/wordCloud/WordCloudOpDesc.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/visualization/wordCloud/WordCloudOpDesc.scala
@@ -17,7 +17,7 @@ import edu.uci.ics.texera.workflow.operators.visualization.{
   VisualizationOperator
 }
 
-class WordCloudV2OpDesc extends VisualizationOperator with PythonOperatorDescriptor {
+class WordCloudOpDesc extends VisualizationOperator with PythonOperatorDescriptor {
   @JsonProperty(required = true)
   @JsonSchemaTitle("Text column")
   @AutofillAttributeName
