c57fe6d33e9f77ebd197551d7331c57f160760a6
Xinyuan Lin <xinyual3@uci.edu>
2024-02-13 13:00:28 -0800

    update css

--- core/new-gui/src/app/common/formly/array.type.ts
+++ core/new-gui/src/app/common/formly/array.type.ts
@@ -3,21 +3,19 @@ import { FieldArrayType } from "@ngx-formly/core";
 
 @Component({
   template: `
-    <div *ngIf="!props.hideLabel" style="padding-bottom: 10px;">
+    <hr>
+    <div *ngFor="let field of field.fieldGroup; let i = index;" style="margin: 0;">
+      <formly-field [field]="field" style="padding-left: 0;display:inline-block;width:calc(100% - 24px)" class="dynamic-fields"></formly-field>
+      <button nz-button [nzSize]="'small'" [nzShape]="'circle'" nzDanger type="button" (click)="remove(i)">
+        <span nz-icon nzType="delete"></span>
+      </button>
       <hr>
-      <h5 style="display:inline-block;">{{ props.label }}</h5>
-      <button nz-button [nzSize]="'small'" [nzType]="'primary'" [nzShape]="'circle'" type="button" (click)="add()" style="display:inline-block;vertical-align: baseline;float: right;">
-        <span *ngIf="props.addText">{{ props.addText }}</span>
-        <i nz-icon nzType="plus"></i>
-      </button>
     </div>
-    <div *ngFor="let field of field.fieldGroup; let i = index;" class="row" style="margin: 0;border-top: 1px solid rgba(0,0,0,.1); padding-top: 15px;">
-      <formly-field class="col" [field]="field" style="padding-left: 0;"></formly-field>
-      <button nz-button [nzSize]="'small'" [nzShape]="'circle'" nzDanger *ngIf="!props.hideRemoveButtons" type="button" (click)="remove(i)">
-        <span *ngIf="props.removeText">{{ props.removeText }}</span>
-        <i nz-icon nzType="delete"></i>
-      </button>
-    </div>
+    <h4 style="display:inline-block;">{{ props.label }}</h4>
+    <button nz-button [nzSize]="'small'" [nzType]="'primary'" [nzShape]="'circle'" type="button" (click)="add()" style="display:inline-block;vertical-align: baseline;float: right;">
+      <span nz-icon nzType="plus"></span>
+    </button>
+
   `,
 })
 export class ArrayTypeComponent extends FieldArrayType {}
