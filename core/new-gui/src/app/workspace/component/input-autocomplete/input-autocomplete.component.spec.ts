67c30f58a2662d2d0a13bd366ce946e85ebfcf39
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2024-02-21 15:12:49 -0800

    fix tests

--- core/new-gui/src/app/workspace/component/input-autocomplete/input-autocomplete.component.spec.ts
+++ core/new-gui/src/app/workspace/component/input-autocomplete/input-autocomplete.component.spec.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars, @typescript-eslint/no-unused-vars */
 import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
-import { ReactiveFormsModule } from "@angular/forms";
+import { FormControl, ReactiveFormsModule } from "@angular/forms";
 import { InputAutoCompleteComponent } from "./input-autocomplete.component";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { HttpClientModule } from "@angular/common/http";
@@ -19,6 +19,7 @@ describe("InputAutoCompleteComponent", () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(InputAutoCompleteComponent);
     component = fixture.componentInstance;
+    component.field = { props: {}, formControl: new FormControl() };
     fixture.detectChanges();
   });
 
