a1cd6a7813d4ac85984cd35d861cfd4bbc72fee9
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2024-02-26 20:17:31 -0800

    fix test

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/projection/ProjectionOpExec.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/projection/ProjectionOpExec.scala
@@ -5,16 +5,23 @@ import edu.uci.ics.amber.engine.common.tuple.amber.TupleLike
 import edu.uci.ics.texera.workflow.common.operators.map.MapOpExec
 import edu.uci.ics.texera.workflow.common.tuple.Tuple
 
+import scala.collection.mutable
+
 class ProjectionOpExec(attributeUnits: List[AttributeUnit]) extends MapOpExec {
 
   setMapFunc(project)
   def project(tuple: Tuple): TupleLike = {
     Preconditions.checkArgument(attributeUnits.nonEmpty)
-    TupleLike(
-      attributeUnits.map(attributeUnit =>
-        tuple.getField[Any](attributeUnit.getOriginalAttribute)
-      ): _*
-    )
+    val fields = mutable.LinkedHashMap[String, Any]()
+    attributeUnits.foreach { attributeUnit =>
+      val alias = attributeUnit.getAlias
+      if (fields.contains(alias)) {
+        throw new RuntimeException("have duplicated attribute name/alias")
+      }
+      fields(alias) = tuple.getField[Any](attributeUnit.getOriginalAttribute)
+    }
+
+    TupleLike(fields.toSeq: _*)
   }
 
 }
