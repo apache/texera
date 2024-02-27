9b02db290119e6a935228b78be89d30651baacf7
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2024-02-26 17:26:14 -0800

    simplify

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/dictionary/DictionaryMatcherOpExec.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/dictionary/DictionaryMatcherOpExec.scala
@@ -6,11 +6,11 @@ import edu.uci.ics.texera.workflow.common.operators.map.MapOpExec
 import edu.uci.ics.texera.workflow.common.tuple.Tuple
 import org.apache.lucene.analysis.Analyzer
 import org.apache.lucene.analysis.en.EnglishAnalyzer
-import org.apache.lucene.analysis.tokenattributes.OffsetAttribute
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute
 
 import java.io.StringReader
 import scala.collection.mutable
-import scala.collection.mutable.{ListBuffer, Set}
+import scala.collection.mutable.ListBuffer
 import scala.jdk.CollectionConverters.CollectionHasAsScala
 
 class DictionaryMatcherOpExec(
@@ -55,11 +55,11 @@ class DictionaryMatcherOpExec(
   }
 
   /**
-    * Checks if a given tuple matches any entry in a dictionary according to specified matching criteria.
-    * The tuple is first converted to a lower-cased string, and the matching is performed based on the {@link MatchingType}.
+    * Determines whether a given tuple matches any dictionary entry based on defined matching criteria.
+    * The tuple's specified field is converted to a lowercase string for comparison.
     *
-    * @param tuple The tuple to match against the dictionary entries.
-    * @return Boolean indicating whether the tuple matches any dictionary entry according to the specified matching type.
+    * @param tuple The tuple whose field is to be checked against dictionary entries.
+    * @return true if the tuple matches a dictionary entry according to the matching criteria; false otherwise.
     */
   private def isTupleInDictionary(tuple: Tuple): Boolean = {
     val text = tuple.getField(attributeName).asInstanceOf[String].toLowerCase
@@ -86,45 +86,43 @@ class DictionaryMatcherOpExec(
   }
 
   /**
-    * tokenize a given text to a list of tokens after removing stopwords
+    * Tokenizes a given text into a set of unique tokens, excluding stopwords.
     *
-    * @param text String
-    * @return
+    * @param text The input text to tokenize.
+    * @return A mutable set of tokens derived from the input text, excluding stopwords.
     */
-  def tokenize(text: String): mutable.Set[String] = {
-    val tokenizedSet = mutable.Set[String]()
-    val tokenStream = luceneAnalyzer.tokenStream(null, new StringReader(text))
-    val offsetAttribute = tokenStream.addAttribute(classOf[OffsetAttribute])
-    tokenStream.reset()
-    while ({
-      tokenStream.incrementToken
-    }) {
-      val charStart = offsetAttribute.startOffset
-      val charEnd = offsetAttribute.endOffset
-      val termStr = text.substring(charStart, charEnd).toLowerCase
-      if (
-        !EnglishAnalyzer.ENGLISH_STOP_WORDS_SET.contains(termStr) && !Utils.URL_STOP_WORDS_SET
-          .contains(termStr)
-      )
-        tokenizedSet += termStr
+  private def tokenize(text: String): mutable.Set[String] = {
+    val tokens = mutable.Set[String]()
+    val tokenStream = luceneAnalyzer.tokenStream("", new StringReader(text))
+    try {
+      val charTermAttribute = tokenStream.addAttribute(classOf[CharTermAttribute])
+      tokenStream.reset()
+      while (tokenStream.incrementToken()) {
+        val term = charTermAttribute.toString.toLowerCase
+        if (
+          !EnglishAnalyzer.ENGLISH_STOP_WORDS_SET.contains(term) && !Utils.URL_STOP_WORDS_SET
+            .contains(term)
+        ) {
+          tokens += term
+        }
+      }
+    } finally {
+      tokenStream.close() // Ensure the token stream is always closed
     }
-    tokenStream.close()
-    tokenizedSet
+    tokens
   }
 
   /**
-    * match an input tuple and give it a label if it is present in the dictionary
+    * Labels an input tuple as matched if it is present in the dictionary.
     *
-    * @param tuple Tuple
-    * @return
+    * @param tuple The input tuple to be checked against the dictionary.
+    * @return A TupleLike object containing the original fields of the tuple and a boolean indicating the match status.
     */
-  private def matchTuple(tuple: Tuple): TupleLike = {
-    var matched: Boolean = false
-    if (tuple.getField(attributeName) != null) {
-      matched = isTupleInDictionary(tuple)
-    }
-    TupleLike(tuple.getFields.asScala ++ List(matched))
+  private def labelTupleIfMatched(tuple: Tuple): TupleLike = {
+    val isMatched =
+      Option(tuple.getField[Any](attributeName)).exists(_ => isTupleInDictionary(tuple))
+    TupleLike(tuple.getFields.asScala.toSeq ++ Seq(isMatched): _*)
   }
 
-  setMapFunc(matchTuple)
+  setMapFunc(labelTupleIfMatched)
 }
