0a85bca49ecf26bdf78e01b3e747d353f296af59
kunwp1 <kunwp1@uci.edu>
2024-03-15 18:00:21 -0700

    second version

--- core/amber/src/main/python/core/architecture/handlers/control/query_statistics_handler.py
+++ core/amber/src/main/python/core/architecture/handlers/control/query_statistics_handler.py
@@ -10,4 +10,18 @@ class QueryStatisticsHandler(ControlHandler):
     cmd = QueryStatisticsV2
 
     def __call__(self, context: Context, command: QueryStatisticsV2, *args, **kwargs):
-        return context.state_manager.get_current_state(), WorkerStatistics(*context.statistics_manager.get_statistics())
+        (
+            input_count,
+            output_count,
+            data_processing_time,
+            control_processing_time,
+            idle_time,
+        ) = context.statistics_manager.get_statistics()
+        state = context.state_manager.get_current_state()
+        return state, WorkerStatistics(
+            input_tuple_count=0,
+            output_tuple_count=0,
+            data_processing_time=data_processing_time,
+            control_processing_time=control_processing_time,
+            idle_time=idle_time,
+        )
