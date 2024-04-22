79d8faefea660fe610e7db16676e09003fcc1cfd
MiuMiuMiue <zhey16@uci.edu>
2024-04-21 17:03:26 -0700

    fmt fix

--- core/gui/src/app/workspace/service/workflow-result/panel-resize/panel-resize.service.ts
+++ core/gui/src/app/workspace/service/workflow-result/panel-resize/panel-resize.service.ts
@@ -7,7 +7,7 @@ import { BehaviorSubject } from "rxjs";
 export class PanelResizeService {
   private panelSizeSource = new BehaviorSubject<{ width: number; height: number }>({ width: 800, height: 300 });
   currentSize = this.panelSizeSource.asObservable();
-  public pageSize =  1 + Math.floor((300 - 200) / 35);
+  public pageSize = 1 + Math.floor((300 - 200) / 35);
 
   changePanelSize(width: number, height: number) {
     this.panelSizeSource.next({ width, height });
