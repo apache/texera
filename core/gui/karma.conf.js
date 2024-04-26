51aa42beeb911b72582759499283f647b544ad82
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2024-04-26 11:31:36 -0700

    apply waitwebpack

--- core/gui/karma.conf.js
+++ core/gui/karma.conf.js
@@ -4,13 +4,15 @@
 module.exports = function(config) {
   config.set({
     basePath: "",
-    frameworks: ["jasmine", "@angular-devkit/build-angular"],
+    frameworks: ['waitwebpack', "jasmine", "@angular-devkit/build-angular"],
     plugins: [
       require("karma-jasmine"),
       require("karma-chrome-launcher"),
       require("karma-coverage"),
       require("karma-jasmine-html-reporter"),
-      require("@angular-devkit/build-angular/plugins/karma")
+      require("@angular-devkit/build-angular/plugins/karma"),
+      require('./karma.waitwebpack'),
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
