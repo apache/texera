e8934b4ea4b559af71e2ec7c1c320e2003e83087
yunyad <yunyad1@uci.edu>
2024-04-28 12:00:06 -0700

    fmt

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/visualization/ImageViz/ImageVisualizerOpDesc.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/visualization/ImageViz/ImageVisualizerOpDesc.scala
@@ -2,11 +2,11 @@ package edu.uci.ics.texera.workflow.operators.visualization.ImageViz
 
 import com.fasterxml.jackson.annotation.{JsonProperty, JsonPropertyDescription}
 import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaTitle
-import edu.uci.ics.amber.engine.common.workflow.{InputPort, OutputPort}
 import edu.uci.ics.texera.workflow.common.metadata.annotations.AutofillAttributeName
 import edu.uci.ics.texera.workflow.common.metadata.{OperatorGroupConstants, OperatorInfo}
 import edu.uci.ics.texera.workflow.common.operators.PythonOperatorDescriptor
 import edu.uci.ics.texera.workflow.common.tuple.schema.{Attribute, AttributeType, Schema}
+import edu.uci.ics.amber.engine.common.workflow.{InputPort, OutputPort}
 import edu.uci.ics.texera.workflow.operators.visualization.{
   VisualizationConstants,
   VisualizationOperator
@@ -73,7 +73,6 @@ class ImageVisualizerOpDesc extends VisualizationOperator with PythonOperatorDes
                        |        yield {"html-content": all_images_html}
                        |""".stripMargin
     finalCode
-
   }
 
   // make the chart type to html visualization so it can be recognized by both backend and frontend.
