3dd7ea8e41874b54e96c96d43451c20442eecbc5
Jiadong Bai <bobbaicloudwithpants@gmail.com>
2024-05-17 15:05:09 -0700

    try to pass ci

--- core/amber/src/test/scala/edu/uci/ics/texera/workflow/common/storage/partition/PartitionDocumentSpec.scala
+++ core/amber/src/test/scala/edu/uci/ics/texera/workflow/common/storage/partition/PartitionDocumentSpec.scala
@@ -8,6 +8,7 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.BeforeAndAfter
 import org.scalatest.matchers.should.Matchers
 
+import java.io.{ByteArrayOutputStream, InputStream}
 import scala.util.Using
 
 class PartitionDocumentSpec extends AnyFlatSpec with Matchers with BeforeAndAfter {
@@ -15,6 +16,20 @@ class PartitionDocumentSpec extends AnyFlatSpec with Matchers with BeforeAndAfte
   var baseTempURI: Path = _
   var partitionDocument: PartitionDocument = _
   val numOfPartitions: Int = 5
+
+  private def readAllBytes(inputStream: InputStream): Array[Byte] = {
+    val buffer = new ByteArrayOutputStream()
+    val data = new Array[Byte](1024)
+    var nRead = 0
+    while ( {
+      nRead = inputStream.read(data, 0, data.length)
+      nRead != -1
+    }) {
+      buffer.write(data, 0, nRead)
+    }
+    buffer.flush()
+    buffer.toByteArray
+  }
 
   before {
     // Create a temporary directory for the base URI
@@ -40,7 +55,7 @@ class PartitionDocumentSpec extends AnyFlatSpec with Matchers with BeforeAndAfte
     for (i <- 0 until numOfPartitions) {
       val doc = partitionDocument.getItem(i)
       val content = Using(doc.asInputStream()) { inStream =>
-        new String(inStream.readAllBytes())
+        new String(readAllBytes(inStream))
       }.getOrElse(fail("Failed to read from the partition"))
       content should be(s"Content for partition $i")
     }
