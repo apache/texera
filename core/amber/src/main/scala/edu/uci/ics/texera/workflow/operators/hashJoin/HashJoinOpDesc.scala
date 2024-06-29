1c12ee191a1c723888d9123065bc001cbb5475e1
Xinyuan Lin <xinyual3@uci.edu>
2024-06-29 10:41:05 -0700

    fix fmt

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/hashJoin/HashJoinOpDesc.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/hashJoin/HashJoinOpDesc.scala
@@ -11,11 +11,18 @@ import edu.uci.ics.amber.engine.common.virtualidentity.{
   WorkflowIdentity
 }
 import edu.uci.ics.amber.engine.common.workflow.{InputPort, OutputPort, PhysicalLink, PortIdentity}
-import edu.uci.ics.texera.workflow.common.metadata.annotations.{AutofillAttributeName, AutofillAttributeNameOnPort1}
+import edu.uci.ics.texera.workflow.common.metadata.annotations.{
+  AutofillAttributeName,
+  AutofillAttributeNameOnPort1
+}
 import edu.uci.ics.texera.workflow.common.metadata.{OperatorGroupConstants, OperatorInfo}
 import edu.uci.ics.texera.workflow.common.operators.LogicalOp
 import edu.uci.ics.texera.workflow.common.tuple.schema.{Attribute, AttributeType, Schema}
-import edu.uci.ics.texera.workflow.common.workflow.{HashPartition, OneToOnePartition, PhysicalPlan}
+import edu.uci.ics.texera.workflow.common.workflow.{
+  HashPartition,
+  OneToOnePartition,
+  PhysicalPlan
+}
 
 object HashJoinOpDesc {
   val HASH_JOIN_INTERNAL_KEY_NAME = "__internal__hashtable__key__"
