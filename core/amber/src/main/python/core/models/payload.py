4ecc6334ed9c5e8dc8c8e41fbc0981492b8549b0
linxinyuan <linxinyuan@gmail.com>
2024-08-10 20:30:26 -0700

    update

--- core/amber/src/main/python/core/models/payload.py
+++ core/amber/src/main/python/core/models/payload.py
@@ -2,13 +2,16 @@ from dataclasses import dataclass
 from pyarrow.lib import Table
 from typing import Optional
 
+
 @dataclass
 class DataPayload:
     frame: Optional[Table] = None
 
+
 @dataclass
 class DataFrame(DataPayload):
     pass
+
 
 @dataclass
 class EndOfUpstream(DataPayload):
