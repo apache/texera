145d5f09de5db7463aa8702bd85c019c3ce61b59
Kunwoo Park <kunwoopark@Kunwoos-MacBook-Pro.local>
2024-08-15 12:46:45 -0700

    Format files

--- core/amber/src/main/scala/edu/uci/ics/texera/web/resource/dashboard/user/cluster/ClusterResource.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/resource/dashboard/user/cluster/ClusterResource.scala
@@ -6,7 +6,12 @@ import edu.uci.ics.texera.web.model.jooq.generated.enums.ClusterStatus
 import edu.uci.ics.texera.web.model.jooq.generated.tables.Cluster.CLUSTER
 import edu.uci.ics.texera.web.model.jooq.generated.tables.ClusterActivity.CLUSTER_ACTIVITY
 import edu.uci.ics.texera.web.model.jooq.generated.tables.daos.{ClusterActivityDao, ClusterDao}
-import edu.uci.ics.texera.web.resource.dashboard.user.cluster.ClusterResource.{ERR_USER_HAS_NO_ACCESS_TO_CLUSTER_MESSAGE, clusterActivityDao, clusterDao, context}
+import edu.uci.ics.texera.web.resource.dashboard.user.cluster.ClusterResource.{
+  ERR_USER_HAS_NO_ACCESS_TO_CLUSTER_MESSAGE,
+  clusterActivityDao,
+  clusterDao,
+  context
+}
 import io.dropwizard.auth.Auth
 import org.glassfish.jersey.media.multipart.FormDataParam
 import edu.uci.ics.texera.web.model.jooq.generated.tables.pojos.{Cluster, ClusterActivity}
@@ -34,23 +39,23 @@ object ClusterResource {
 class ClusterResource {
 
   /**
-   * Creates a new cluster and records the start time in cluster_activity.
-   *
-   * @param user The authenticated user creating the cluster.
-   * @param name The name of the cluster.
-   * @param machineType The type of machines in the cluster.
-   * @param numberOfMachines The number of machines in the cluster.
-   * @return The created Cluster object.
-   */
+    * Creates a new cluster and records the start time in cluster_activity.
+    *
+    * @param user The authenticated user creating the cluster.
+    * @param name The name of the cluster.
+    * @param machineType The type of machines in the cluster.
+    * @param numberOfMachines The number of machines in the cluster.
+    * @return The created Cluster object.
+    */
   @POST
   @Path("/create")
   @Consumes(Array(MediaType.MULTIPART_FORM_DATA))
   def createCluster(
-                     @Auth user: SessionUser,
-                     @FormDataParam("Name") name: String,
-                     @FormDataParam("machineType") machineType: String,
-                     @FormDataParam("numberOfMachines") numberOfMachines: Integer
-                   ): Cluster = {
+      @Auth user: SessionUser,
+      @FormDataParam("Name") name: String,
+      @FormDataParam("machineType") machineType: String,
+      @FormDataParam("numberOfMachines") numberOfMachines: Integer
+  ): Cluster = {
     val cluster = new Cluster()
     cluster.setName(name)
     cluster.setOwnerId(user.getUid)
@@ -71,12 +76,12 @@ class ClusterResource {
   }
 
   /**
-   * Deletes a cluster and records the termination time in cluster_activity.
-   *
-   * @param user The authenticated user requesting the deletion.
-   * @param cluster The cluster to be deleted.
-   * @return A Response indicating the result of the operation.
-   */
+    * Deletes a cluster and records the termination time in cluster_activity.
+    *
+    * @param user The authenticated user requesting the deletion.
+    * @param cluster The cluster to be deleted.
+    * @return A Response indicating the result of the operation.
+    */
   @POST
   @Path("/delete")
   def deleteCluster(@Auth user: SessionUser, cluster: Cluster): Response = {
@@ -95,12 +100,12 @@ class ClusterResource {
   }
 
   /**
-   * Pauses a cluster and records the pause time in cluster_activity.
-   *
-   * @param user The authenticated user requesting the pause.
-   * @param cluster The cluster to be paused.
-   * @return A Response indicating the result of the operation.
-   */
+    * Pauses a cluster and records the pause time in cluster_activity.
+    *
+    * @param user The authenticated user requesting the pause.
+    * @param cluster The cluster to be paused.
+    * @return A Response indicating the result of the operation.
+    */
   @POST
   @Path("/pause")
   def pauseCluster(@Auth user: SessionUser, cluster: Cluster): Response = {
@@ -119,12 +124,12 @@ class ClusterResource {
   }
 
   /**
-   * Resumes a paused cluster and records the resume time in cluster_activity.
-   *
-   * @param user The authenticated user requesting the resume.
-   * @param cluster The cluster to be resumed.
-   * @return A Response indicating the result of the operation.
-   */
+    * Resumes a paused cluster and records the resume time in cluster_activity.
+    *
+    * @param user The authenticated user requesting the resume.
+    * @param cluster The cluster to be resumed.
+    * @return A Response indicating the result of the operation.
+    */
   @POST
   @Path("/resume")
   def resumeCluster(@Auth user: SessionUser, cluster: Cluster): Response = {
@@ -143,19 +148,20 @@ class ClusterResource {
   }
 
   /**
-   * Updates the name of a cluster.
-   *
-   * @param user The authenticated user requesting the update.
-   * @param cluster The cluster with the new name.
-   * @return A Response indicating the result of the operation.
-   */
+    * Updates the name of a cluster.
+    *
+    * @param user The authenticated user requesting the update.
+    * @param cluster The cluster with the new name.
+    * @return A Response indicating the result of the operation.
+    */
   @POST
   @Consumes(Array(MediaType.APPLICATION_JSON))
   @Path("/update/name")
   def updateClusterName(@Auth user: SessionUser, cluster: Cluster): Response = {
     validateClusterOwnership(user, cluster.getId)
 
-    context.update(CLUSTER)
+    context
+      .update(CLUSTER)
       .set(CLUSTER.NAME, cluster.getName)
       .where(CLUSTER.ID.eq(cluster.getId))
       .execute()
@@ -164,11 +170,11 @@ class ClusterResource {
   }
 
   /**
-   * Lists all clusters owned by the authenticated user.
-   *
-   * @param user The authenticated user.
-   * @return A list of Clusters owned by the user.
-   */
+    * Lists all clusters owned by the authenticated user.
+    *
+    * @param user The authenticated user.
+    * @return A list of Clusters owned by the user.
+    */
   @GET
   @Path("")
   def listClusters(@Auth user: SessionUser): util.List[Cluster] = {
@@ -176,11 +182,11 @@ class ClusterResource {
   }
 
   /**
-   * Validates that the authenticated user has ownership of the cluster.
-   *
-   * @param user The authenticated user.
-   * @param clusterId The ID of the cluster to validate ownership.
-   */
+    * Validates that the authenticated user has ownership of the cluster.
+    *
+    * @param user The authenticated user.
+    * @param clusterId The ID of the cluster to validate ownership.
+    */
   private def validateClusterOwnership(user: SessionUser, clusterId: Int): Unit = {
     val clusterOwnerId = clusterDao.fetchOneById(clusterId).getOwnerId
     if (clusterOwnerId != user.getUid) {
@@ -189,24 +195,25 @@ class ClusterResource {
   }
 
   /**
-   * Updates the status of a cluster.
-   *
-   * @param clusterId The ID of the cluster to update.
-   * @param status The new status of the cluster.
-   */
+    * Updates the status of a cluster.
+    *
+    * @param clusterId The ID of the cluster to update.
+    * @param status The new status of the cluster.
+    */
   private def updateClusterStatus(clusterId: Int, status: ClusterStatus): Unit = {
-    context.update(CLUSTER)
+    context
+      .update(CLUSTER)
       .set(CLUSTER.STATUS, status)
       .where(CLUSTER.ID.eq(clusterId))
       .execute()
   }
 
   /**
-   * Inserts a new cluster activity record with the given start time.
-   *
-   * @param clusterId The ID of the cluster.
-   * @param startTime The start time of the activity.
-   */
+    * Inserts a new cluster activity record with the given start time.
+    *
+    * @param clusterId The ID of the cluster.
+    * @param startTime The start time of the activity.
+    */
   private def insertClusterActivity(clusterId: Int, startTime: Timestamp): Unit = {
     val clusterActivity = new ClusterActivity()
     clusterActivity.setClusterId(clusterId)
@@ -215,20 +222,24 @@ class ClusterResource {
   }
 
   /**
-   * Updates the end time of the most recent cluster activity to the current time.
-   *
-   * @param clusterId The ID of the cluster.
-   */
+    * Updates the end time of the most recent cluster activity to the current time.
+    *
+    * @param clusterId The ID of the cluster.
+    */
   private def updateClusterActivityEndTime(clusterId: Int): Unit = {
-    context.update(CLUSTER_ACTIVITY)
+    context
+      .update(CLUSTER_ACTIVITY)
       .set(CLUSTER_ACTIVITY.END_TIME, Timestamp.from(Instant.now()))
       .where(CLUSTER_ACTIVITY.CLUSTER_ID.eq(clusterId))
-      .and(CLUSTER_ACTIVITY.START_TIME.eq(
-        context.select(max(CLUSTER_ACTIVITY.START_TIME))
-          .from(CLUSTER_ACTIVITY)
-          .where(CLUSTER_ACTIVITY.CLUSTER_ID.eq(clusterId))
-          .and(CLUSTER_ACTIVITY.END_TIME.isNull)
-      ))
+      .and(
+        CLUSTER_ACTIVITY.START_TIME.eq(
+          context
+            .select(max(CLUSTER_ACTIVITY.START_TIME))
+            .from(CLUSTER_ACTIVITY)
+            .where(CLUSTER_ACTIVITY.CLUSTER_ID.eq(clusterId))
+            .and(CLUSTER_ACTIVITY.END_TIME.isNull)
+        )
+      )
       .execute()
   }
 }
