e75447b53f8bb4be903b1d55f9ad646b178c13b7
Kelvin Zhao <kelvinyz@uci.edu>
2024-08-15 19:59:43 -0700

    Implement Websocket Proxy and Expose User-Accessible Endpoint

--- core/workflow-pod-brain/src/main/scala/WorkflowPodBrainApplication.scala
+++ core/workflow-pod-brain/src/main/scala/WorkflowPodBrainApplication.scala
@@ -1,16 +1,24 @@
+import be.tomcools.dropwizard.websocket.WebsocketBundle
 import config.ApplicationConf
 import io.dropwizard.core.{Application, Configuration}
 import io.dropwizard.core.setup.{Bootstrap, Environment}
-import web.resources.{HelloWorldResource, WorkflowPodBrainResource}
-
-import service.KubernetesClientService
+import web.resources.{HelloWorldResource, WebsocketProxyEndpoint, WorkflowPodBrainResource}
 
 class WorkflowPodBrainApplication extends Application[Configuration] {
-  override def initialize(bootstrap: Bootstrap[Configuration]): Unit = {}
+  var websocket = new WebsocketBundle[Configuration]
+
+  override def initialize(bootstrap: Bootstrap[Configuration]): Unit = {
+    // Add WebsocketBundle to dropwizard
+    bootstrap.addBundle(websocket)
+  }
 
   override def run(configuration: Configuration, environment: Environment): Unit = {
     val appConfig = ApplicationConf.appConfig
 
+    // Add any websocket endpoint here
+    websocket.addEndpoint(classOf[WebsocketProxyEndpoint])
+
+    // Register http resources
     environment.jersey().register(new HelloWorldResource)
     environment.jersey().register(new WorkflowPodBrainResource)
 
