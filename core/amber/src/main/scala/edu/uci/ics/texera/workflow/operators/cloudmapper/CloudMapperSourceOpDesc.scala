763d91ddcbef7e553332f46fcf7e1b227217c6d1
Kunwoo Park <kunwoopark@vcv066233.vpn.uci.edu>
2024-08-29 08:24:00 -0700

    Create enum for reference genome

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/cloudmapper/CloudMapperSourceOpDesc.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/cloudmapper/CloudMapperSourceOpDesc.scala
@@ -3,43 +3,28 @@
   import com.fasterxml.jackson.annotation.{JsonProperty, JsonPropertyDescription}
   import com.kjetland.jackson.jsonSchema.annotations.{JsonSchemaInject, JsonSchemaString, JsonSchemaTitle}
   import edu.uci.ics.amber.engine.common.workflow.OutputPort
-  import edu.uci.ics.texera.workflow.common.metadata.{OperatorGroupConstants, OperatorInfo}
   import edu.uci.ics.texera.workflow.common.metadata.annotations.HideAnnotation
+  import edu.uci.ics.texera.workflow.common.metadata.{OperatorGroupConstants, OperatorInfo}
   import edu.uci.ics.texera.workflow.common.operators.source.PythonSourceOperatorDescriptor
   import edu.uci.ics.texera.workflow.common.tuple.schema.{Attribute, AttributeType, Schema}
 
   class CloudMapperSourceOpDesc extends PythonSourceOperatorDescriptor {
-    @JsonProperty(defaultValue = "false")
-    @JsonSchemaTitle("Human Genome (GRCh38)")
-    @JsonPropertyDescription("GRCh38 (2024-A)")
-    val referenceGenome1: Boolean = false
+    @JsonProperty(required = true)
+    @JsonSchemaTitle("FastQ Files")
+    @JsonPropertyDescription("Zip file containing FASTQ files")
+    val fastQFiles: Option[String] = None
 
-    @JsonProperty(defaultValue = "false")
-    @JsonSchemaTitle("Human Genome (hg19)")
-    @JsonPropertyDescription("hg19 (3.0.0)")
-    val referenceGenome2: Boolean = false
-
-    @JsonProperty(defaultValue = "false")
-    @JsonSchemaTitle("Mouse Genome (GRCh38)")
-    @JsonPropertyDescription("GRCm38 (2024-A)")
-    val referenceGenome3: Boolean = false
-
-    @JsonProperty(defaultValue = "false")
-    @JsonSchemaTitle("Mouse Genome (mm10)")
-    @JsonPropertyDescription("mm10 (2020-A)")
-    val referenceGenome4: Boolean = false
-
-    @JsonProperty(defaultValue = "false")
-    @JsonSchemaTitle("Others")
-    @JsonPropertyDescription("Other reference genome")
-    val others: Boolean = false
+    @JsonProperty(required = true)
+    @JsonSchemaTitle("Reference Genomes")
+    @JsonPropertyDescription("Select one or more reference genomes")
+    var referenceGenomes: List[ReferenceGenome] = List()
 
     @JsonSchemaTitle("FastA Files")
     @JsonSchemaInject(
       strings = Array(
-        new JsonSchemaString(path = HideAnnotation.hideTarget, value = "others"),
-        new JsonSchemaString(path = HideAnnotation.hideType, value = HideAnnotation.Type.equals),
-        new JsonSchemaString(path = HideAnnotation.hideExpectedValue, value = "false")
+        new JsonSchemaString(path = HideAnnotation.hideTarget, value = "referenceGenomes"),
+        new JsonSchemaString(path = HideAnnotation.hideType, value = HideAnnotation.Type.regex),
+        new JsonSchemaString(path = HideAnnotation.hideExpectedValue, value = "^((?!others).)*$") // regex to hide when "others" is not present
       )
     )
     val fastAFiles: Option[String] = None
@@ -47,26 +32,17 @@
     @JsonSchemaTitle("Gtf File")
     @JsonSchemaInject(
       strings = Array(
-        new JsonSchemaString(path = HideAnnotation.hideTarget, value = "others"),
-        new JsonSchemaString(path = HideAnnotation.hideType, value = HideAnnotation.Type.equals),
-        new JsonSchemaString(path = HideAnnotation.hideExpectedValue, value = "false")
+        new JsonSchemaString(path = HideAnnotation.hideTarget, value = "referenceGenomes"),
+        new JsonSchemaString(path = HideAnnotation.hideType, value = HideAnnotation.Type.regex),
+        new JsonSchemaString(path = HideAnnotation.hideExpectedValue, value = "^((?!others).)*$") // regex to hide when "others" is not present
       )
     )
     val gtfFile: Option[String] = None
 
     @JsonProperty(required = true)
-    @JsonSchemaTitle("FastQ Files")
-    @JsonPropertyDescription("Zip file containing FASTQ files")
-    val fastQFiles: Option[String] = None
-
-    @JsonProperty(required = true)
     @JsonSchemaTitle("Cluster Id")
     @JsonPropertyDescription("Cluster Id")
     val clusterId: Option[String] = None
-
-    // Custom validation logic in constructor
-    require(!(others && fastAFiles.isEmpty), "FastA Files must be provided if 'Others' is selected.")
-    require(!(others && gtfFile.isEmpty), "GTF File must be provided if 'Others' is selected.")
 
     override def generatePythonCode(): String = {
         s"""from pytexera import *
---
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/cloudmapper/ReferenceGenome.java
@@ -0,0 +1,26 @@
+package edu.uci.ics.texera.workflow.operators.cloudmapper;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum ReferenceGenome {
+    HUMAN_GRCh38("GRCh38"),
+
+    MOUSE_GRCm39("GRCm39"),
+
+    MOUSE_mm10("mm10"),
+
+    HUMAN_hg19("hg19"),
+
+    OTHERS("others");
+
+    private final String name;
+
+    ReferenceGenome(String name) {
+        this.name = name;
+    }
+
+    @JsonValue
+    public String getName() {
+        return this.name;
+    }
+}
