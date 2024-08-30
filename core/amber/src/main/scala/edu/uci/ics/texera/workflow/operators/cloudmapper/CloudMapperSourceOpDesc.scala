f8f62f422d467a83c444c5f79708ee122cd2ba86
Kunwoo Park <kunwoopark@dhcp-10-8-053-190.mobile.reshsg.uci.edu>
2024-08-30 12:04:23 -0700

    receive fasta files and gtf file from frontend

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/cloudmapper/CloudMapperSourceOpDesc.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/cloudmapper/CloudMapperSourceOpDesc.scala
@@ -24,71 +24,79 @@ class CloudMapperSourceOpDesc extends PythonSourceOperatorDescriptor {
   val clusterId: String = ""
 
   override def generatePythonCode(): String = {
+    // Convert the Scala referenceGenomes list to a Python list format
+    val pythonReferenceGenomes = referenceGenomes.map(_.referenceGenome.getName).map(name => s"'$name'").mkString("[", ", ", "]")
+
+    // Convert the Scala referenceGenomes list to a Python list format for FASTA files
+    val pythonFastaFiles = referenceGenomes.flatMap(_.fastAFiles).map(file => s"open('$file', 'rb')").mkString("[", ", ", "]")
+
+    // Extract GTF file if exists for 'Others'
+    val pythonGtfFile = referenceGenomes.find(_.referenceGenome == ReferenceGenomeEnum.OTHERS).flatMap(_.gtfFile).map(file => s"open('$file', 'rb')").getOrElse("")
+
     s"""from pytexera import *
-           |
-           |class GenerateOperator(UDFSourceOperator):
-           |
-           |    @overrides
-           |    def produce(self) -> Iterator[Union[TupleLike, TableLike, None]]:
-           |        import requests
-           |
-           |        # Set the URL to the Go endpoint
-           |        url = "http://localhost:3000/api/job/create"
-           |
-           |        def create_job_form_data(cluster_id, job_form):
-           |            form_data = {
-           |                'cid': str(cluster_id)
-           |            }
-           |
-           |            # Append the 'reads' file from the form control
-           |            reads_file = job_form.get('reads')
-           |            files = {}
-           |            if reads_file:
-           |                files['file'] = reads_file
-           |
-           |            # Append selected genomes and related files
-           |            append_selected_genomes_to_form_data(form_data, files, job_form)
-           |
-           |            return form_data, files
-           |
-           |        def append_selected_genomes_to_form_data(form_data, files, job_form):
-           |            selected_genomes = job_form.get('referenceGenome', [])
-           |
-           |            for index, genome in enumerate(selected_genomes):
-           |                form_data[f'referenceGenome[{index}]'] = genome
-           |
-           |            if 'Others' in selected_genomes:
-           |                append_fasta_and_gtf_files_to_form_data(files, job_form)
-           |
-           |        def append_fasta_and_gtf_files_to_form_data(files, job_form):
-           |            fasta_files = job_form.get('fastaFiles', [])
-           |            gtf_file = job_form.get('gtfFile')
-           |
-           |            for index, fasta_file in enumerate(fasta_files):
-           |                files[f'fastaFiles[{index}]'] = fasta_file
-           |
-           |            if gtf_file:
-           |                files['gtfFile'] = gtf_file
-           |
-           |        # Example job_form dictionary, replace with your actual data
-           |        job_form = {
-           |            'reads': open('$fastQFiles', 'rb'),  # Replace with actual file path
-           |            'referenceGenome': ['Others'],
-           |            'fastaFiles': [
-           |                open('/Users/kunwoopark/Downloads/NC_001802.fna', 'rb'),  # Replace with actual file path
-           |            ],
-           |            'gtfFile': open('/Users/kunwoopark/Downloads/NC_001802.gtf', 'rb')  # Replace with actual file path
-           |        }
-           |        # Create form data and files based on the provided job_form
-           |        form_data, files = create_job_form_data($clusterId, job_form)
-           |
-           |        # Send the POST request to the Go program
-           |        response = requests.post(url, data=form_data, files=files)
-           |
-           |        # Print the response from the Go program
-           |        print(f"Response Status Code: {response.status_code}")
-           |        print(f"Response Text: {response.text}")
-           |        yield {'response': 200}
+       |
+       |class GenerateOperator(UDFSourceOperator):
+       |
+       |    @overrides
+       |    def produce(self) -> Iterator[Union[TupleLike, TableLike, None]]:
+       |        import requests
+       |
+       |        # Set the URL to the Go endpoint
+       |        url = "http://localhost:3000/api/job/create"
+       |
+       |        def create_job_form_data(cluster_id, job_form):
+       |            form_data = {
+       |                'cid': str(cluster_id)
+       |            }
+       |
+       |            # Append the 'reads' file from the form control
+       |            reads_file = job_form.get('reads')
+       |            files = {}
+       |            if reads_file:
+       |                files['file'] = reads_file
+       |
+       |            # Append selected genomes and related files
+       |            append_selected_genomes_to_form_data(form_data, files, job_form)
+       |
+       |            return form_data, files
+       |
+       |        def append_selected_genomes_to_form_data(form_data, files, job_form):
+       |            selected_genomes = job_form.get('referenceGenome', [])
+       |
+       |            for index, genome in enumerate(selected_genomes):
+       |                form_data[f'referenceGenome[{index}]'] = genome
+       |
+       |            if 'Others' in selected_genomes:
+       |                append_fasta_and_gtf_files_to_form_data(files, job_form)
+       |
+       |        def append_fasta_and_gtf_files_to_form_data(files, job_form):
+       |            fasta_files = job_form.get('fastaFiles', [])
+       |            gtf_file = job_form.get('gtfFile')
+       |
+       |            for index, fasta_file in enumerate(fasta_files):
+       |                files[f'fastaFiles[{index}]'] = fasta_file
+       |
+       |            if gtf_file:
+       |                files['gtfFile'] = gtf_file
+       |
+       |        # Example job_form dictionary using inputs from Scala
+       |        job_form = {
+       |            'reads': open('${fastQFiles}', 'rb'),
+       |            'referenceGenome': ${pythonReferenceGenomes},
+       |            'fastaFiles': ${pythonFastaFiles} if 'Others' in ${pythonReferenceGenomes} else [],
+       |            'gtfFile': ${pythonGtfFile}
+       |        }
+       |
+       |        # Create form data and files based on the provided job_form
+       |        form_data, files = create_job_form_data(${clusterId}, job_form)
+       |
+       |        # Send the POST request to the Go program
+       |        response = requests.post(url, data=form_data, files=files)
+       |
+       |        # Print the response from the Go program
+       |        print(f"Response Status Code: {response.status_code}")
+       |        print(f"Response Text: {response.text}")
+       |        yield {'response': 200}
            """.stripMargin
   }
   override def operatorInfo: OperatorInfo =
