372eefa003bec42691a385a55467e01144a54966
mengw15 <125719918+mengw15@users.noreply.github.com>
2024-08-30 03:29:16 -0700

    fmt fix

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/common/workflow/WorkflowSettings.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/common/workflow/WorkflowSettings.scala
@@ -3,7 +3,8 @@ package edu.uci.ics.texera.workflow.common.workflow
 import edu.uci.ics.amber.engine.common.AmberConfig
 
 object WorkflowSettings {
-  def apply(dataTransferBatchSize: Int = AmberConfig.defaultBatchSize): WorkflowSettings = new WorkflowSettings(dataTransferBatchSize)
+  def apply(dataTransferBatchSize: Int = AmberConfig.defaultBatchSize): WorkflowSettings =
+    new WorkflowSettings(dataTransferBatchSize)
 }
 
 class WorkflowSettings(var dataTransferBatchSize: Int = AmberConfig.defaultBatchSize)
