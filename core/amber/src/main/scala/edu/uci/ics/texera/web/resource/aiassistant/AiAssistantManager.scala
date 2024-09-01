038be99c02e51a2ddeb51cebd4c8b55f4328751e
Minchong(Brian) Wu <2638932112@qq.com>
2024-09-01 19:52:11 -0400

    fix comment

--- core/amber/src/main/scala/edu/uci/ics/texera/web/resource/aiassistant/AiAssistantManager.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/resource/aiassistant/AiAssistantManager.scala
@@ -5,34 +5,41 @@ import java.net.{HttpURLConnection, URL}
 object AiAssistantManager {
   private val aiAssistantConfig = AmberConfig.aiAssistantConfig
   val assistantType: String = aiAssistantConfig.getString("assistant")
+  // The accountKey is the OpenAI authentication key used to authenticate API requests and obtain responses from the OpenAI service.
+
   val accountKey: String = aiAssistantConfig.getString("ai-service-key")
+  val sharedUrl: String = aiAssistantConfig.getString("ai-service-url")
+
+  private def initOpenAI(): Boolean = {
+    var isKeyValid: Boolean = false
+    var connection: HttpURLConnection = null
+    try {
+      val url = new URL(s"${sharedUrl}/models")
+      connection = url.openConnection().asInstanceOf[HttpURLConnection]
+      connection.setRequestMethod("GET")
+      connection.setRequestProperty(
+        "Authorization",
+        s"Bearer ${accountKey.trim.replaceAll("^\"|\"$", "")}"
+      )
+      val responseCode = connection.getResponseCode
+      isKeyValid = responseCode == 200
+    } catch {
+      case e: Exception =>
+        isKeyValid = false
+    } finally {
+      if (connection != null) {
+        connection.disconnect()
+      }
+    }
+    isKeyValid
+  }
+
   val validAIAssistant: Boolean = assistantType match {
     case "none" =>
       false
 
     case "openai" =>
-      val sharedUrl = "https://api.openai.com/v1"
-      var isKeyValid: Boolean = false
-      var connection: HttpURLConnection = null
-      try {
-        val url = new URL(s"${sharedUrl}/models")
-        connection = url.openConnection().asInstanceOf[HttpURLConnection]
-        connection.setRequestMethod("GET")
-        connection.setRequestProperty(
-          "Authorization",
-          s"Bearer ${accountKey.trim.replaceAll("^\"|\"$", "")}"
-        )
-        val responseCode = connection.getResponseCode
-        isKeyValid = responseCode == 200
-      } catch {
-        case e: Exception =>
-          isKeyValid = false
-      } finally {
-        if (connection != null) {
-          connection.disconnect()
-        }
-      }
-      isKeyValid
+      initOpenAI()
 
     case _ =>
       false
