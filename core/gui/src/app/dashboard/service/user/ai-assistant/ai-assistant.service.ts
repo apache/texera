5769815efdd86dae57d43d11501ce42d713240f0
Minchong(Brian) Wu <2638932112@qq.com>
2024-09-01 20:48:53 -0400

    fmt

--- core/gui/src/app/dashboard/service/user/ai-assistant/ai-assistant.service.ts
+++ core/gui/src/app/dashboard/service/user/ai-assistant/ai-assistant.service.ts
@@ -13,15 +13,17 @@ export class AiAssistantService {
 
   public checkAiAssistantEnabled(): Promise<string> {
     const apiUrl = `${AI_ASSISTANT_API_BASE_URL}/isenabled`;
-    return firstValueFrom(this.http.get(apiUrl, { responseType: "text"}))
+    return firstValueFrom(this.http.get(apiUrl, { responseType: "text" }))
       .then(response => {
         const isEnabled = response !== undefined ? response : "NoAiAssistant";
         console.log(
-          isEnabled === "OpenAI" ? "AI Assistant successfully started" : "No AI Assistant or OpenAI authentication key error"
+          isEnabled === "OpenAI"
+            ? "AI Assistant successfully started"
+            : "No AI Assistant or OpenAI authentication key error"
         );
         return isEnabled;
       })
-      .catch(()=> {
+      .catch(() => {
         return "NoAiAssistant";
       });
   }
