29b5ae3bd800bd5baa16fbe3242d91d97b232260
Janus-W <2373025856w@gmail.com>
2024-09-18 12:17:43 +0100

    add notification

--- core/gui/src/app/common/service/notification/notification.service.ts
+++ core/gui/src/app/common/service/notification/notification.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
-import { NzMessageDataOptions, NzMessageService } from "ng-zorro-antd/message";
+import { NzMessageDataOptions, NzMessageService} from "ng-zorro-antd/message";
+import { NzNotificationService } from "ng-zorro-antd/notification";
 
 /**
  * NotificationService is an entry service for sending notifications
@@ -8,7 +9,27 @@ import { NzMessageDataOptions, NzMessageService } from "ng-zorro-antd/message";
   providedIn: "root",
 })
 export class NotificationService {
-  constructor(private message: NzMessageService) {}
+  constructor(
+    private message: NzMessageService,
+    private notification: NzNotificationService) {}
+
+  createReportNotification(): string {
+    const notificationId = this.notification.blank(
+      "报告生成中",
+      "报告正在生成，请稍候...",
+      { nzDuration: 0 } // 确保通知不会自动关闭
+    ).messageId; // 保存通知的ID，稍后可以用来关闭
+
+    return notificationId;
+  }
+
+  blank(title: string, content: string): void {
+    this.notification.blank(title, content, { nzDuration: 0 });
+  }
+
+  remove(id?: string): void {
+    this.notification.remove(id);
+  }
 
   success(message: string, options: NzMessageDataOptions = {}) {
     this.message.success(message, options);
