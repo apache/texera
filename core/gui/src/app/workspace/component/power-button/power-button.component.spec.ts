c97d706d6c849b2921a76004cf6e39720051050f
Kelvin Zhao <kelvinyz@uci.edu>
2024-09-25 15:09:54 -0700

    Fix Power Button Test

--- core/gui/src/app/workspace/component/power-button/power-button.component.spec.ts
+++ core/gui/src/app/workspace/component/power-button/power-button.component.spec.ts
@@ -1,15 +1,19 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-
+import { HttpClientModule } from "@angular/common/http";
 import { PowerButtonComponent } from "./power-button.component";
 
 describe("PowerButtonComponent", () => {
   let component: PowerButtonComponent;
   let fixture: ComponentFixture<PowerButtonComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [PowerButtonComponent],
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        PowerButtonComponent,  // Import standalone component
+        HttpClientModule       // Import HttpClientModule for dynamic icon loading
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(PowerButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
