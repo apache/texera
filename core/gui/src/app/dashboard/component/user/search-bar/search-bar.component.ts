ed5c881908d91598ac8bdb9d30a163a2f480ddd7
gspikehalo <2318002579@qq.com>
2024-09-25 12:53:25 -0700

    quick fix

--- core/gui/src/app/dashboard/component/user/search-bar/search-bar.component.ts
+++ core/gui/src/app/dashboard/component/user/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 import { SearchService } from "../../../service/user/search.service";
 import { SearchFilterParameters } from "../../../type/search-filter-parameters";
