7942f78da4048f334f67a883946a28c519bb6207
sixsage <sixsage02@gmail.com>
2024-10-07 01:56:14 -0700

    add view table

--- core/scripts/sql/texera_ddl.sql
+++ core/scripts/sql/texera_ddl.sql
@@ -228,3 +228,11 @@ CREATE TABLE IF NOT EXISTS workflow_user_clones
     FOREIGN KEY (`uid`) REFERENCES `user` (`uid`) ON DELETE CASCADE,
     FOREIGN KEY (`wid`) REFERENCES `workflow` (`wid`) ON DELETE CASCADE
     ) ENGINE = INNODB;
+    
+CREATE TABLE IF NOT EXISTS workflow_view_count
+(
+    wid INT UNSIGNED NOT NULL,
+    view_count INT UNSIGNED NOT NULL DEFAULT 0,
+    PRIMARY KEY (wid),
+    FOREIGN KEY (wid) REFERENCES workflow (wid) ON DELETE CASCADE
+    ) ENGINE = INNODB;
