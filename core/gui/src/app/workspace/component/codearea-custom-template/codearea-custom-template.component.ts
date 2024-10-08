09b44296ba56c815f80f2cffd6806f97d7ff8a22
mengw15 <125719918+mengw15@users.noreply.github.com>
2024-10-08 10:15:57 -0700

    make editor cannot be opened mutiple times at same time

--- core/gui/src/app/workspace/component/codearea-custom-template/codearea-custom-template.component.ts
+++ core/gui/src/app/workspace/component/codearea-custom-template/codearea-custom-template.component.ts
@@ -1,10 +1,11 @@
-import { Component, ComponentRef, ChangeDetectorRef } from "@angular/core";
+import { Component, ComponentRef, ChangeDetectorRef, OnInit, OnDestroy } from "@angular/core";
 import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
 import { FieldType, FieldTypeConfig } from "@ngx-formly/core";
 import { CodeEditorComponent } from "../code-editor-dialog/code-editor.component";
 import { CoeditorPresenceService } from "../../service/workflow-graph/model/coeditor-presence.service";
 import { CodeEditorService } from "../../service/code-editor/code-editor.service";
-
+import { WorkflowActionService } from "../../service/workflow-graph/model/workflow-action.service";
+import { EditorStateService } from '../../service/editor-state/editor-state.service';
 
 /**
  * CodeareaCustomTemplateComponent is the custom template for 'codearea' type of formly field.
@@ -18,15 +19,17 @@ import { CodeEditorService } from "../../service/code-editor/code-editor.service
   templateUrl: "codearea-custom-template.component.html",
   styleUrls: ["codearea-custom-template.component.scss"],
 })
-export class CodeareaCustomTemplateComponent extends FieldType<FieldTypeConfig> {
+export class CodeareaCustomTemplateComponent extends FieldType<FieldTypeConfig> implements OnInit, OnDestroy {
   componentRef: ComponentRef<CodeEditorComponent> | undefined;
-
   public isEditorOpen: boolean = false;
+  private operatorID: string = ''; // 用于存储当前的 operatorID
 
   constructor(
     private coeditorPresenceService: CoeditorPresenceService,
     private codeEditorService: CodeEditorService,
-    private changeDetectorRef: ChangeDetectorRef
+    private changeDetectorRef: ChangeDetectorRef,
+    private workflowActionService: WorkflowActionService,
+    private editorStateService: EditorStateService  // 注入服务
   ) {
     super();
     this.coeditorPresenceService
@@ -39,17 +42,35 @@ export class CodeareaCustomTemplateComponent extends FieldType<FieldTypeConfig> 
       .subscribe(_ => this.componentRef?.destroy());
   }
 
+  ngOnInit() {
+    this.operatorID = this.getOperatorID();
+    this.editorStateService.getEditorState(this.operatorID).pipe(untilDestroyed(this)).subscribe(isOpen => {
+      this.isEditorOpen = isOpen;
+      this.changeDetectorRef.detectChanges();
+    });
+  }
+
   openEditor(): void {
     this.componentRef = this.codeEditorService.vc.createComponent(CodeEditorComponent);
     this.componentRef.instance.componentRef = this.componentRef;
     this.componentRef.instance.formControl = this.field.formControl;
     this.isEditorOpen = true;
 
+    this.editorStateService.setEditorState(this.operatorID, true);
+
     this.componentRef.onDestroy(() => {
       this.isEditorOpen = false;
+      this.editorStateService.setEditorState(this.operatorID, false);
       this.changeDetectorRef.detectChanges();
     });
   }
 
+  ngOnDestroy() {
+    this.editorStateService.setEditorState(this.operatorID, this.isEditorOpen);
+  }
 
+  private getOperatorID(): string {
+    return this.workflowActionService.getJointGraphWrapper().getCurrentHighlightedOperatorIDs()[0];
+  }
 }
+
