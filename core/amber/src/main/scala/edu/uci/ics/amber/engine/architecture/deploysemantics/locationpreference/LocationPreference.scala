291b2ad0827f3dc4237f572ac138bc60c5ffaf02
Shengquan Ni <13672781+shengquan-ni@users.noreply.github.com>
2024-10-15 14:08:25 -0700

    move addressinfo to separate file

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/deploysemantics/locationpreference/LocationPreference.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/deploysemantics/locationpreference/LocationPreference.scala
@@ -1,13 +1,5 @@
 package edu.uci.ics.amber.engine.architecture.deploysemantics.locationpreference
 
-import akka.actor.Address
-
-// Holds worker and controller node addresses.
-case class AddressInfo(
-    allAddresses: Array[Address], // e.g., Node 1, Node 2, Node 3
-    controllerAddress: Address // Controller node
-)
-
 // LocationPreference defines where operators should run.
 sealed trait LocationPreference extends Serializable
 
