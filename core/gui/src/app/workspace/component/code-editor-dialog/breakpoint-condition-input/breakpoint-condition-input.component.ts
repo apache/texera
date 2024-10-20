8b2d58625ef6eeac30e284a31823083013a68c97
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2024-10-19 21:17:17 -0700

    simplify

--- core/gui/src/app/workspace/component/code-editor-dialog/breakpoint-condition-input/breakpoint-condition-input.component.ts
+++ core/gui/src/app/workspace/component/code-editor-dialog/breakpoint-condition-input/breakpoint-condition-input.component.ts
@@ -12,7 +12,6 @@ import {
 } from "@angular/core";
 import { UdfDebugService } from "../../../service/operator-debug/udf-debug.service";
 import { isDefined } from "../../../../common/util/predicate";
-import * as monaco from "monaco-editor";
 import { MonacoEditor } from "monaco-breakpoints/dist/types";
 
 @Component({
@@ -21,66 +20,78 @@ import { MonacoEditor } from "monaco-breakpoints/dist/types";
   styleUrls: ["./breakpoint-condition-input.component.scss"],
 })
 export class BreakpointConditionInputComponent implements AfterViewChecked, OnChanges {
+  constructor(private udfDebugService: UdfDebugService) {}
+
   @Input() operatorId = "";
   @Input() lineNum?: number;
   @Input() monacoEditor!: MonacoEditor;
   @Output() closeEmitter = new EventEmitter<void>();
+  @ViewChild("conditionTextarea") conditionTextarea!: ElementRef<HTMLTextAreaElement>;
 
-  @ViewChild("conditionTextarea") textarea!: ElementRef<HTMLTextAreaElement>;
-
-  left(): number {
-    if (isDefined(this.monacoEditor)) {
-      const { glyphMarginLeft } = this.monacoEditor.getLayoutInfo()!;
-      const { left: editorLeft, top: editorTop } = this.monacoEditor.getDomNode()!.getBoundingClientRect();
-      return editorLeft + glyphMarginLeft - this.monacoEditor.getScrollLeft();
-    }
-    return 0;
-  }
-  top(): number {
-    if (isDefined(this.monacoEditor) && isDefined(this.lineNum)) {
-      const topPixel = this.monacoEditor.getTopForLineNumber(this.lineNum);
-      const lineHeight = this.monacoEditor.getOption(monaco.editor.EditorOption.lineHeight);
-      const { left: editorLeft, top: editorTop } = this.monacoEditor.getDomNode()!.getBoundingClientRect();
-      return editorTop + topPixel + lineHeight / 2 - this.monacoEditor.getScrollTop();
-    }
-    return 0;
-  }
-  condition = "";
-
-  constructor(private udfDebugService: UdfDebugService) {}
-
-  isVisible(): boolean {
-    return isDefined(this.lineNum);
-  }
+  public condition = "";
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes["lineNum"]?.currentValue) {
-      const manager = this.udfDebugService.getOrCreateManager(this.operatorId);
-      this.condition = manager.getCondition(this.lineNum!) ?? "";
+    if (!isDefined(changes["lineNum"]?.currentValue)) {
+      return;
     }
+    // when the line number changes, update the condition
+    this.condition = this.udfDebugService.getOrCreateManager(this.operatorId).getCondition(this.lineNum!) ?? "";
   }
 
   ngAfterViewChecked(): void {
-    if (this.isVisible()) {
-      this.textarea?.nativeElement.focus();
-    }
-  }
-
-  @HostListener("window:keydown", ["$event"])
-  @HostListener("focusout")
-  handleEvent(event?: KeyboardEvent): void {
-    if (!event || (event.key === "Enter" && !event.shiftKey)) {
-      event?.preventDefault(); // Prevent default only for Enter key event
-      this.updateConditionAndClose();
-    }
-  }
-
-  private updateConditionAndClose(): void {
-    if (!this.lineNum) {
+    if (!this.isVisible()) {
       return;
     }
 
+    // focus the textarea when it is visible
+    this.conditionTextarea?.nativeElement.focus();
+  }
+
+  public left(): number {
+    if (!isDefined(this.monacoEditor)) {
+      return 0;
+    }
+
+    // Calculate the left position of the input popup based on the editor layout
+    const { glyphMarginLeft } = this.monacoEditor.getLayoutInfo()!;
+    const { left } = this.monacoEditor.getDomNode()!.getBoundingClientRect();
+    return left + glyphMarginLeft - this.monacoEditor.getScrollLeft();
+  }
+
+  public top(): number {
+    if (!(isDefined(this.monacoEditor) && isDefined(this.lineNum))) {
+      return 0;
+    }
+
+    // Calculate the top position of the input popup based on the editor layout
+    const topPixel = this.monacoEditor.getBottomForLineNumber(this.lineNum);
+    const editorRect = this.monacoEditor.getDomNode()?.getBoundingClientRect();
+    return (editorRect?.top || 0) + topPixel - this.monacoEditor.getScrollTop();
+  }
+
+  public isVisible(): boolean {
+    return isDefined(this.lineNum);
+  }
+
+  /**
+   * Update the condition and close the dialog when the user presses Enter or focus out.
+   * @param event the keyboard event, or undefined if the event is focus out.
+   */
+  @HostListener("window:keydown", ["$event"])
+  @HostListener("focusout")
+  handleEvent(event?: KeyboardEvent): void {
+    if (!this.lineNum || (event && !(event.key === "Enter" && !event.shiftKey))) {
+      // perform no changes if no line number or the key is not Enter
+      return;
+    }
+
+    // prevent the default behavior of the Enter key
+    event?.preventDefault();
+
+    // save the updated condition
     this.udfDebugService.doUpdateBreakpointCondition(this.operatorId, this.lineNum, this.condition.trim());
+
+    // close the dialog
     this.closeEmitter.emit();
   }
 }
