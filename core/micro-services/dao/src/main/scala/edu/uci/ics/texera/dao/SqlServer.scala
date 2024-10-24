ae7998e8e4fefd0a801a6cd955a586374a7be73b
Jiadong Bai <bobbaicloudwithpants@gmail.com>
2024-10-24 16:00:30 -0700

    resolve comments

---
+++ core/micro-services/dao/src/main/scala/edu/uci/ics/texera/dao/SqlServer.scala
@@ -0,0 +1,31 @@
+package edu.uci.ics.texera.dao
+
+import com.mysql.cj.jdbc.MysqlDataSource
+import org.jooq.{DSLContext, SQLDialect}
+import org.jooq.impl.DSL
+
+/**
+  * SqlServer class that manages a connection to a MySQL database using jOOQ.
+  *
+  * @param url The database connection URL, specifying the MySQL server and database name.
+  * @param user The username for authenticating with the MySQL database.
+  * @param password The password for authenticating with the MySQL database.
+  */
+class SqlServer(url: String, user: String, password: String) {
+  val SQL_DIALECT: SQLDialect = SQLDialect.MYSQL
+  private val dataSource: MysqlDataSource = new MysqlDataSource()
+  var context: DSLContext = _
+
+  {
+    dataSource.setUrl(url)
+    dataSource.setUser(user)
+    dataSource.setPassword(password)
+    context = DSL.using(dataSource, SQL_DIALECT)
+  }
+
+  def createDSLContext(): DSLContext = context
+
+  def replaceDSLContext(newContext: DSLContext): Unit = {
+    context = newContext
+  }
+}
