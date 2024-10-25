ea875d74067dab901b697af2d2581f427271e7cb
Shengquan Ni <13672781+shengquan-ni@users.noreply.github.com>
2024-10-25 15:10:11 -0700

    Update ControllerAsyncRPCHandlerInitializer.scala

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/controller/ControllerAsyncRPCHandlerInitializer.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/controller/ControllerAsyncRPCHandlerInitializer.scala
@@ -11,6 +11,7 @@ class ControllerAsyncRPCHandlerInitializer(
     with AmberLogging
     with LinkWorkersHandler
     with WorkerExecutionCompletedHandler
+    with WorkerStateUpdatedHandler
     with PauseHandler
     with QueryWorkerStatisticsHandler
     with ResumeHandler
