a3bf73796bcf833dcb5dd546994717cef7c0f13d
Shengquan Ni <shengqun@uci.edu>
2024-10-28 18:19:37 -0700

    update

--- core/amber/src/main/python/core/architecture/handlers/control/add_input_channel_handler.py
+++ core/amber/src/main/python/core/architecture/handlers/control/add_input_channel_handler.py
@@ -1,13 +1,12 @@
 from core.architecture.handlers.control.control_handler_base import ControlHandler
-from proto.edu.uci.ics.amber.engine.architecture.rpc import EmptyReturn
+from proto.edu.uci.ics.amber.engine.architecture.rpc import EmptyReturn, AddInputChannelRequest
 from proto.edu.uci.ics.amber.engine.common import ChannelIdentity, PortIdentity
 
 
 class AddInputChannelHandler(ControlHandler):
     async def add_input_channel(
         self,
-        channel_id: ChannelIdentity,
-        port_id: PortIdentity,
+        req:AddInputChannelRequest
     ) -> EmptyReturn:
-        self.context.input_manager.register_input(channel_id, port_id)
+        self.context.input_manager.register_input(req.channel_id, req.port_id)
         return EmptyReturn()
