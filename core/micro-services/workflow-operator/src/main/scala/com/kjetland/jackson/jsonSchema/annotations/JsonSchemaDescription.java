8bc110019e4bbce8223d553b8c530e39c403ab87
Jiadong Bai <bobbaicloudwithpants@gmail.com>
2024-11-04 10:25:48 -0800

    make sure external package unchanged

--- core/micro-services/workflow-operator/src/main/scala/com/kjetland/jackson/jsonSchema/annotations/JsonSchemaDescription.java
+++ core/micro-services/workflow-operator/src/main/scala/com/kjetland/jackson/jsonSchema/annotations/JsonSchemaDescription.java
@@ -3,10 +3,7 @@ package com.kjetland.jackson.jsonSchema.annotations;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.PARAMETER;
-import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
