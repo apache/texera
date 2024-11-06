3e08043c986105218b787ba42a792e7ea81448d4
Xinyuan Lin <xinyual3@uci.edu>
2024-11-05 21:13:11 -0800

    update

--- core/gui/src/app/hub/component/about/about.component.spec.ts
+++ core/gui/src/app/hub/component/about/about.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { AboutComponent } from "./about.component";
 
-describe("HomeComponent", () => {
+describe("AboutComponent", () => {
   let component: AboutComponent;
   let fixture: ComponentFixture<AboutComponent>;
 
