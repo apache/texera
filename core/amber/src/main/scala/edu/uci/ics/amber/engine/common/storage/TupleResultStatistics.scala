ead242a598b69014c9f6c20053b09f7d28f65cfa
Jiadong Bai <bobbaicloudwithpants@gmail.com>
2024-11-11 08:16:15 -0800

    fix comments

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/common/storage/TupleResultStatistics.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/common/storage/TupleResultStatistics.scala
@@ -1,12 +1,12 @@
 package edu.uci.ics.amber.engine.common.storage
 
-import scala.collection.immutable.Map
-
 trait TupleResultStatistics {
-  // a map from field name to stats
+  // a map from numerical fields to stats
   def getNumericColStats: Map[String, Map[String, Any]]
 
+  // a map from date fields to stats
   def getDateColStats: Map[String, Map[String, Any]]
 
+  // a map from categorical fields to stats
   def getCategoricalStats: Map[String, Map[String, Any]]
 }
