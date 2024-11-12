b45692606a577cf430b070544dc3ac211e3518af
Jiadong Bai <bobbaicloudwithpants@gmail.com>
2024-11-12 13:06:40 -0800

    change url

--- core/micro-services/workflow-compiling-service/src/main/scala/edu/uci/ics/texera/service/WorkflowCompilingService.scala
+++ core/micro-services/workflow-compiling-service/src/main/scala/edu/uci/ics/texera/service/WorkflowCompilingService.scala
@@ -17,7 +17,7 @@ class WorkflowCompilingService extends Application[WorkflowCompilingServiceConfi
       environment: Environment
   ): Unit = {
     // serve backend at /api/texera
-    environment.jersey.setUrlPattern("/api/texera/*")
+    environment.jersey.setUrlPattern("/api/workflow/*")
     // register CORS filter
     environment.jersey.register(classOf[CORSFilter])
     // register the compilation endpoint
