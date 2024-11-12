68af0cdb098ea26c3debe1520a48619859d32ccf
Jiadong Bai <bobbaicloudwithpants@gmail.com>
2024-11-12 12:59:42 -0800

    add more comments

--- core/micro-services/workflow-compiling-service/src/main/scala/edu/uci/ics/amber/compiler/WorkflowCompiler.scala
+++ core/micro-services/workflow-compiling-service/src/main/scala/edu/uci/ics/amber/compiler/WorkflowCompiler.scala
@@ -58,6 +58,7 @@ object WorkflowCompiler {
     opIdToError.toMap
   }
 
+  // util function for collecting the input schemas for each op
   private def collectInputSchemaFromPhysicalPlan(
       physicalPlan: PhysicalPlan,
       errorList: ArrayBuffer[(OperatorIdentity, Throwable)] // Mandatory error list
