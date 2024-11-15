c53d64d016b1275dc0b50da1b24b7f7c4d350e60
Shengquan Ni <13672781+shengquan-ni@users.noreply.github.com>
2024-11-14 17:31:21 -0800

    rename close to clear

--- core/amber/src/main/scala/edu/uci/ics/texera/web/service/WorkflowService.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/service/WorkflowService.scala
@@ -86,7 +86,7 @@ class WorkflowService(
     s"workflowId=$workflowId",
     cleanUpTimeout,
     () => {
-      opResultStorage.close()
+      opResultStorage.clear()
       WorkflowService.workflowServiceMapping.remove(mkWorkflowStateId(workflowId))
       if (executionService.getValue != null) {
         // shutdown client
@@ -230,7 +230,7 @@ class WorkflowService(
     }
 
     // clean up results from previous run
-    opResultStorage.close() // TODO: change this behavior after enabling cache.
+    opResultStorage.clear() // TODO: change this behavior after enabling cache.
     try {
       val execution = new WorkflowExecutionService(
         controllerConf,
