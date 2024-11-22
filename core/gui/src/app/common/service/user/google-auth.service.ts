2c232ac0f0489387a1cf86b66c790286f5be564c
shenghao fu <2318002579@qq.com>
2024-11-21 17:44:57 -0800

    update

--- core/gui/src/app/common/service/user/google-auth.service.ts
+++ core/gui/src/app/common/service/user/google-auth.service.ts
@@ -17,19 +17,23 @@ export class GoogleAuthService {
   public googleAuthInit(parent: HTMLElement | null) {
     this.http.get(`${AppSettings.getApiEndpoint()}/auth/google/clientid`, { responseType: "text" }).subscribe({
       next: response => {
-        window.onGoogleLibraryLoad = () => {
-          window.google.accounts.id.initialize({
-            client_id: response,
-            callback: (auth: CredentialResponse) => {
-              this._googleCredentialResponse.next(auth);
-            },
-          });
-          window.google.accounts.id.renderButton(parent, { width: 270 });
-          window.google.accounts.id.prompt();
+        const initializeGoogleLogin = () => {
+          if (window.google?.accounts?.id) {
+            window.google.accounts.id.initialize({
+              client_id: response,
+              callback: (auth: CredentialResponse) => {
+                this._googleCredentialResponse.next(auth);
+              },
+            });
+            if (document.body.contains(parent)) {
+              window.google.accounts.id.renderButton(parent, { width: 200 });
+            }
+            window.google.accounts.id.prompt();
+          } else {
+            setTimeout(initializeGoogleLogin, 500); // Retry
+          }
         };
-      },
-      error: (err: unknown) => {
-        console.error(err);
+        initializeGoogleLogin();
       },
     });
   }
