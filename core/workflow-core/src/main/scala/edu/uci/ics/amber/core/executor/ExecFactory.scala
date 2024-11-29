d7874f348011a1f7179bdcad8a7d8bc4bda4e7e8
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2024-11-29 11:15:42 -0500

    change join

--- core/workflow-core/src/main/scala/edu/uci/ics/amber/core/executor/ExecFactory.scala
+++ core/workflow-core/src/main/scala/edu/uci/ics/amber/core/executor/ExecFactory.scala
@@ -8,22 +8,21 @@ object ExecFactory {
       .newInstance()
       .asInstanceOf[OperatorExecutor]
   }
-  def newExecFromJavaClassName(
+
+  def newExecFromJavaClassName[K](
       className: String,
       descString: String,
       idx: Int = 0,
       workerCount: Int = 1
   ): OperatorExecutor = {
+    val clazz = Class.forName(className).asInstanceOf[Class[K]]
     if (idx == 0 && workerCount == 1) {
-
-      Class
-        .forName(className)
+      clazz
         .getDeclaredConstructor(classOf[String])
         .newInstance(descString)
         .asInstanceOf[OperatorExecutor]
     } else {
-      Class
-        .forName(className)
+      clazz
         .getDeclaredConstructor(classOf[String], classOf[Int], classOf[Int])
         .newInstance(descString, idx, workerCount)
         .asInstanceOf[OperatorExecutor]
