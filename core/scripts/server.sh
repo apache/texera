877ff08d96dd3e838e2a7f9909c38dde47f66eb8
Shengquan Ni <13672781+shengquan-ni@users.noreply.github.com>
2024-12-01 17:08:45 -0800

    update

--- core/scripts/server.sh
+++ core/scripts/server.sh
@@ -1,5 +1,4 @@
 pylsp --ws --port 3000 &
-cd amber
 if [ ! -z $1 ] 
 then 
     target/texera-0.1-SNAPSHOT/bin/texera-web-application  --cluster $1
--- core/scripts/worker.sh
+++ core/scripts/worker.sh
@@ -1,5 +1,4 @@
-cd amber
-if [ ! -z $1 ] 
+if [ ! -z $1 ]
 then 
     target/texera-0.1-SNAPSHOT/bin/texera-run-worker --serverAddr $1
 else
