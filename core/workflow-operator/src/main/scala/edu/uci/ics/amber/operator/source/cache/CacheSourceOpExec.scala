1f7963dd8b73b6c4bf50fe29ae3b9d4120235fcf
Jiadong Bai <bobbaicloudwithpants@gmail.com>
2025-01-12 12:06:45 -0800

    fix test and add sanity check

--- core/workflow-operator/src/main/scala/edu/uci/ics/amber/operator/source/cache/CacheSourceOpExec.scala
+++ core/workflow-operator/src/main/scala/edu/uci/ics/amber/operator/source/cache/CacheSourceOpExec.scala
@@ -3,12 +3,19 @@ package edu.uci.ics.amber.operator.source.cache
 import com.typesafe.scalalogging.LazyLogging
 import edu.uci.ics.amber.core.executor.SourceOperatorExecutor
 import edu.uci.ics.amber.core.storage.DocumentFactory
+import edu.uci.ics.amber.core.storage.FileResolver.decodeVFSUri
+import edu.uci.ics.amber.core.storage.VFSResourceType.MATERIALIZED_RESULT
 import edu.uci.ics.amber.core.storage.model.VirtualDocument
 import edu.uci.ics.amber.core.tuple.{Tuple, TupleLike}
 
 import java.net.URI
 
 class CacheSourceOpExec(storageUri: URI) extends SourceOperatorExecutor with LazyLogging {
+  val (_, _, _, _, resourceType) = decodeVFSUri(storageUri)
+  if (resourceType != MATERIALIZED_RESULT) {
+    throw new RuntimeException("The storage URI must point to an materialized result storage")
+  }
+
   private val storage =
     DocumentFactory.openDocument(storageUri)._1.asInstanceOf[VirtualDocument[Tuple]]
 
