4e6be6acf3154629b96017a049eac8b3c6fe8ce6
gspikehalo <2318002579@qq.com>
2025-01-15 20:13:07 -0800

    update test file

--- core/gui/src/app/dashboard/component/user/google-login/google-login.component.spec.ts
+++ core/gui/src/app/dashboard/component/user/google-login/google-login.component.spec.ts
@@ -1,20 +1,36 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { GoogleLoginComponent } from "./google-login.component";
-import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { UserService } from "../../../../common/service/user/user.service";
+import { SocialAuthService } from "@abacritt/angularx-social-login";
+import { of } from "rxjs";
 
 describe("GoogleLoginComponent", () => {
   let component: GoogleLoginComponent;
   let fixture: ComponentFixture<GoogleLoginComponent>;
+  let userService: jasmine.SpyObj<UserService>;
+  let socialAuthService: jasmine.SpyObj<SocialAuthService>;
 
   beforeEach(async () => {
+    const userServiceSpy = jasmine.createSpyObj("UserService", ["googleLogin"]);
+    const socialAuthServiceSpy = jasmine.createSpyObj("SocialAuthService", [], {
+      authState: of({ idToken: "mock-id-token" }),
+    });
+
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule],
       declarations: [GoogleLoginComponent],
-      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        { provide: UserService, useValue: userServiceSpy },
+        { provide: SocialAuthService, useValue: socialAuthServiceSpy },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(GoogleLoginComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    userService = TestBed.inject(UserService) as jasmine.SpyObj<UserService>;
+    socialAuthService = TestBed.inject(SocialAuthService) as jasmine.SpyObj<SocialAuthService>;
   });
 
   it("should create the component", () => {
