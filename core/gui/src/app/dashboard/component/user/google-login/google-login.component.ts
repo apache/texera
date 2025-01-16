ea41a249b2d1a1834d3e623f747dd6a4d7e302db
gspikehalo <2318002579@qq.com>
2025-01-15 17:57:17 -0800

    update

--- core/gui/src/app/dashboard/component/user/google-login/google-login.component.ts
+++ core/gui/src/app/dashboard/component/user/google-login/google-login.component.ts
@@ -1,7 +1,6 @@
-import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { AfterViewInit, Component } from "@angular/core";
 import { UserService } from "../../../../common/service/user/user.service";
 import { mergeMap } from "rxjs/operators";
-import { GoogleAuthService } from "../../../../common/service/user/google-auth.service";
 import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
 import { DASHBOARD_USER_WORKFLOW } from "../../../../app-routing.constant";
 import { ActivatedRoute, Router } from "@angular/router";
@@ -12,17 +11,15 @@ import { SocialAuthService } from "@abacritt/angularx-social-login";
   selector: "texera-google-login",
   templateUrl: "./google-login.component.html",
 })
-export class GoogleLoginComponent implements OnInit {
-  @ViewChild("googleButton") googleButton!: ElementRef;
+export class GoogleLoginComponent implements AfterViewInit {
   constructor(
     private userService: UserService,
     private route: ActivatedRoute,
     private router: Router,
-    private elementRef: ElementRef,
     private socialAuthService: SocialAuthService
   ) {}
 
-  ngOnInit() {
+  ngAfterViewInit() {
     this.socialAuthService.authState
       .pipe(
         mergeMap(res => this.userService.googleLogin(res.idToken)),
