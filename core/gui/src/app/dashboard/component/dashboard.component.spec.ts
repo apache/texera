6ac8cbdd4295df8bb9fb5b059ac267c583573b24
gspikehalo <2318002579@qq.com>
2025-01-17 15:26:01 -0800

    fix unit test

--- core/gui/src/app/dashboard/component/dashboard.component.spec.ts
+++ core/gui/src/app/dashboard/component/dashboard.component.spec.ts
@@ -3,12 +3,12 @@ import { DashboardComponent } from "./dashboard.component";
 import { UserService } from "../../common/service/user/user.service";
 import { SocialAuthService } from "@abacritt/angularx-social-login";
 import { FlarumService } from "../service/user/flarum/flarum.service";
-import { Router } from "@angular/router";
+import { Router, ActivatedRoute } from "@angular/router";
 import { RouterTestingModule } from "@angular/router/testing";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { of, BehaviorSubject } from "rxjs";
 import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
-import { DASHBOARD_USER_WORKFLOW } from "../../app-routing.constant";
+import {DASHBOARD_USER_WORKFLOW} from "../../app-routing.constant";
 
 describe("DashboardComponent", () => {
   let component: DashboardComponent;
@@ -17,12 +17,15 @@ describe("DashboardComponent", () => {
   let socialAuthServiceMock: jasmine.SpyObj<SocialAuthService>;
   let flarumServiceMock: jasmine.SpyObj<FlarumService>;
   let routerMock: jasmine.SpyObj<Router>;
+  let activatedRouteMock: any;
   let authStateMock: BehaviorSubject<any>;
 
   beforeEach(async () => {
-    userServiceMock = jasmine.createSpyObj("UserService", ["isLogin", "isAdmin"]);
+    userServiceMock = jasmine.createSpyObj("UserService", ["isLogin", "isAdmin", "userChanged", "googleLogin"]);
     userServiceMock.isLogin.and.returnValue(false);
     userServiceMock.isAdmin.and.returnValue(false);
+    userServiceMock.userChanged.and.returnValue(of(undefined));
+    userServiceMock.googleLogin.and.returnValue(of(undefined));
 
     authStateMock = new BehaviorSubject(null);
     socialAuthServiceMock = jasmine.createSpyObj("SocialAuthService", [], {
@@ -30,17 +33,29 @@ describe("DashboardComponent", () => {
     });
 
     flarumServiceMock = jasmine.createSpyObj("FlarumService", ["auth", "register"]);
+    flarumServiceMock.auth.and.returnValue(of({ token: "fake_token" }));
+    flarumServiceMock.register.and.returnValue(of());
 
     routerMock = jasmine.createSpyObj("Router", ["navigateByUrl"]);
 
+    activatedRouteMock = {
+      snapshot: {
+        queryParams: {},
+      },
+    };
+
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, HttpClientTestingModule],
+      imports: [
+        RouterTestingModule.withRoutes([]), // 确保 Router 正常工作
+        HttpClientTestingModule,
+      ],
       declarations: [DashboardComponent],
       providers: [
         { provide: UserService, useValue: userServiceMock },
         { provide: SocialAuthService, useValue: socialAuthServiceMock },
         { provide: FlarumService, useValue: flarumServiceMock },
         { provide: Router, useValue: routerMock },
+        { provide: ActivatedRoute, useValue: activatedRouteMock },
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
