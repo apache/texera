c8cc344dec96a639d783ff5e105d2213dcf30381
Kunwoo Park <kunwp1@uci.edu>
2025-02-07 20:32:40 -0800

    Fix test

--- core/workflow-core/src/test/scala/edu/uci/ics/amber/storage/result/iceberg/IcebergDocumentConsoleMessagesSpec.scala
+++ core/workflow-core/src/test/scala/edu/uci/ics/amber/storage/result/iceberg/IcebergDocumentConsoleMessagesSpec.scala
@@ -1,29 +1,17 @@
 package edu.uci.ics.amber.storage.result.iceberg
 
-import edu.uci.ics.amber.core.storage.model.{
-  BufferedItemWriter,
-  VirtualDocument,
-  VirtualDocumentSpec
-}
 import edu.uci.ics.amber.core.storage.{DocumentFactory, VFSURIFactory}
+import edu.uci.ics.amber.core.storage.model._
 import edu.uci.ics.amber.core.storage.result.IcebergTableSchema
-import edu.uci.ics.amber.core.tuple.Tuple
-import edu.uci.ics.amber.core.virtualidentity.{
-  ExecutionIdentity,
-  OperatorIdentity,
-  WorkflowIdentity
-}
+import edu.uci.ics.amber.core.tuple.{Schema, Tuple}
+import edu.uci.ics.amber.core.virtualidentity._
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import edu.uci.ics.amber.core.tuple.Schema
-import edu.uci.ics.amber.util.IcebergUtil
-import org.apache.iceberg.{Schema => IcebergSchema}
-
-import scala.util.{Try, Using}
 import org.scalatest.BeforeAndAfterAll
 
-import scala.util.Using.Releasable
+import scala.util.{Try, Using}
 import java.net.URI
+import scala.util.Using.Releasable
 
 class IcebergDocumentConsoleMessagesSpec
     extends AnyFlatSpec
@@ -32,37 +20,39 @@ class IcebergDocumentConsoleMessagesSpec
     with BeforeAndAfterAll {
 
   private val amberSchema: Schema = IcebergTableSchema.consoleMessagesSchema
-  private val icebergSchema: IcebergSchema = IcebergUtil.toIcebergSchema(amberSchema)
-  private val uri: URI = VFSURIFactory.createConsoleMessagesURI(
-    WorkflowIdentity(0),
-    ExecutionIdentity(0),
-    OperatorIdentity("test_operator")
-  )
+  private var uri: URI = _
 
-  override def generateSampleItems(): List[Tuple] = {
+  override def beforeEach(): Unit = {
+    uri = VFSURIFactory.createConsoleMessagesURI(
+      WorkflowIdentity(0),
+      ExecutionIdentity(0),
+      OperatorIdentity("test_operator")
+    )
+    DocumentFactory.createDocument(uri, amberSchema)
+    super.beforeEach()
+  }
+
+  override def afterAll(): Unit = {
+    super.afterAll()
+  }
+
+  override def generateSampleItems(): List[Tuple] =
     List(
       new Tuple(amberSchema, Array("First console message")),
       new Tuple(amberSchema, Array("Second console message")),
       new Tuple(amberSchema, Array("Third console message"))
     )
-  }
 
   implicit val bufferedItemWriterReleasable: Releasable[BufferedItemWriter[Tuple]] =
     (resource: BufferedItemWriter[Tuple]) => resource.close()
 
   "IcebergDocument" should "write and read console messages successfully" in {
-    // Create the document
     val document = getDocument
 
-    Using.resource(document.writer("console_messages_test")) {
-      case writer: BufferedItemWriter[Tuple] =>
-        try {
-          writer.open()
-          generateSampleItems().foreach(writer.putOne)
-          writer.close()
-        } catch {
-          case e: Exception => fail(s"Failed to write messages: ${e.getMessage}")
-        }
+    Using.resource(document.writer("console_messages_test")) { writer =>
+      writer.open()
+      generateSampleItems().foreach(writer.putOne)
+      writer.close()
     }
 
     val retrievedMessages = Try(document.get().toList.collect { case t: Tuple => t }).getOrElse(Nil)
