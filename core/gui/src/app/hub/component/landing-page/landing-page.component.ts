8a1ca4a2c4f64dac250b8b9bb6e6997a05186b9f
Shengquan Ni <shengqun@uci.edu>
2025-02-11 14:58:27 -0800

    Update landing-page.component.ts

--- core/gui/src/app/hub/component/landing-page/landing-page.component.ts
+++ core/gui/src/app/hub/component/landing-page/landing-page.component.ts
@@ -43,7 +43,7 @@ export class LandingPageComponent implements OnInit {
       workflows => (this.topClonedWorkflows = workflows),
       "Top Cloned Workflows"
     );
-    this.hubWorkflowService.getGitCommit().subscribe(commit =>{
+    this.hubService.getGitCommit().subscribe(commit =>{
       // Split the text into parts
       this.deploymentCommit = commit
       const match = this.deploymentCommit.match(/(.*)(#\d+)(.*)/);
@@ -56,7 +56,7 @@ export class LandingPageComponent implements OnInit {
       }
 
     })
-    this.hubWorkflowService.getLastDeploy().subscribe(deployTime => this.lastDeployTime = deployTime)
+    this.hubService.getLastDeploy().subscribe(deployTime => this.lastDeployTime = deployTime)
   }
 
   getWorkflowCount(): void {
