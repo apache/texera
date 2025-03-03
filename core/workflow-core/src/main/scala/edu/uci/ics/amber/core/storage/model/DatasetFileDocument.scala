602033ada0488b151b2ae7319834ab307c602ca4
Jiadong Bai <bobbaicloudwithpants@gmail.com>
2025-03-03 10:18:06 -0800

    add more comments

--- core/workflow-core/src/main/scala/edu/uci/ics/amber/core/storage/model/DatasetFileDocument.scala
+++ core/workflow-core/src/main/scala/edu/uci/ics/amber/core/storage/model/DatasetFileDocument.scala
@@ -15,8 +15,12 @@ import java.nio.file.{Files, Path, Paths}
 import scala.jdk.CollectionConverters.IteratorHasAsScala
 
 object DatasetFileDocument {
+  // Since requests need to be sent to the FileService in order to read the file, we store USER_JWT_TOKEN in the environment vars
+  // This variable should be NON-EMPTY in the dynamic-computing-unit architecture, i.e. each user-created computing unit should store user's jwt token.
+  // In the local development or other architectures, this token can be empty.
   lazy val userJwtToken: String = sys.env.getOrElse("USER_JWT_TOKEN", "").trim
 
+  // The endpoint of getting presigned url from the file service, also stored in the environment vars.
   lazy val fileServiceGetPresignURLEndpoint: String =
     sys.env
       .getOrElse(
