074ff46106bd98429ce7b73a02d92640ae47e522
Jiadong Bai <bobbaicloudwithpants@gmail.com>
2025-03-03 10:19:12 -0800

    save the multipart chunk change on frontend

--- core/gui/src/environments/environment.default.ts
+++ core/gui/src/environments/environment.default.ts
@@ -79,6 +79,11 @@ export const defaultEnvironment = {
   singleFileUploadMaximumSizeMB: 20,
 
   /**
+   * the size of each chunk during the multipart upload of file
+   */
+  multipartUploadChunkSizeByte: 50 * 1024 * 1024, // 50 MB
+
+  /**
    * default data transfer batch size for workflows
    */
   defaultDataTransferBatchSize: 400,
