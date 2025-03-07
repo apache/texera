2cbf8a23573a20e4d4e71a6d9382fab1059772b3
Shengquan Ni <shengqun@uci.edu>
2025-03-07 13:59:38 -0800

    Update WorkflowService.scala

--- core/amber/src/main/scala/edu/uci/ics/texera/web/service/WorkflowService.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/service/WorkflowService.scala
@@ -142,17 +142,18 @@ class WorkflowService(
   }
 
   def connectToExecution(onNext: TexeraWebSocketEvent => Unit): Disposable = {
-    var localDisposable = Disposable.empty()
-    executionService.subscribe { executionService: WorkflowExecutionService =>
-      localDisposable.dispose()
-      val subscriptions = executionService.executionStateStore.getAllStores
+    val localDisposable = new CompositeDisposable()
+    val disposable = executionService.subscribe { execService: WorkflowExecutionService =>
+      localDisposable.clear() // Clears previous subscriptions safely
+      val subscriptions = execService.executionStateStore.getAllStores
         .map(_.getWebsocketEventObservable)
         .map(evtPub =>
           evtPub.subscribe { events: Iterable[TexeraWebSocketEvent] => events.foreach(onNext) }
         )
         .toSeq
-      localDisposable = new CompositeDisposable(subscriptions: _*)
+      localDisposable.addAll(subscriptions: _*)
     }
+    new CompositeDisposable(localDisposable, disposable)
   }
 
   def disconnect(): Unit = {
