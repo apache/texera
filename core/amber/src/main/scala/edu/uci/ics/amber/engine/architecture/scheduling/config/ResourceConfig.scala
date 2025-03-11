50884bdf3a70cddb26389b9421c4a4bdd7647a59
Xiao-zhen-Liu <xiaozl3@uci.edu>
2025-03-10 21:47:19 -0700

    fmt.

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/scheduling/config/ResourceConfig.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/scheduling/config/ResourceConfig.scala
@@ -3,7 +3,6 @@ package edu.uci.ics.amber.engine.architecture.scheduling.config
 import edu.uci.ics.amber.core.virtualidentity.PhysicalOpIdentity
 import edu.uci.ics.amber.core.workflow.{GlobalPortIdentity, PhysicalLink}
 
-
 case class ResourceConfig(
     operatorConfigs: Map[PhysicalOpIdentity, OperatorConfig] = Map.empty,
     linkConfigs: Map[PhysicalLink, LinkConfig] = Map.empty,
