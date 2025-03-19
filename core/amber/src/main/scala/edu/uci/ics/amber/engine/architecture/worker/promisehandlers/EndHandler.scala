19efd6280122e35345bd8340be69b59ecd93ed5b
Xinyuan Lin <xinyual3@uci.edu>
2025-03-19 07:33:49 -0700

    update

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/worker/promisehandlers/EndHandler.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/worker/promisehandlers/EndHandler.scala
@@ -1,9 +1,6 @@
 package edu.uci.ics.amber.engine.architecture.worker.promisehandlers
 
 import com.twitter.util.Future
-import edu.uci.ics.amber.core.executor.SourceOperatorExecutor
-import edu.uci.ics.amber.core.virtualidentity.{ActorVirtualIdentity, ChannelIdentity}
-import edu.uci.ics.amber.core.workflow.PortIdentity
 import edu.uci.ics.amber.engine.architecture.rpc.controlcommands.{AsyncRPCContext, EndInputChannelRequest}
 import edu.uci.ics.amber.engine.architecture.rpc.controlreturns.EmptyReturn
 import edu.uci.ics.amber.engine.architecture.worker.DataProcessorRPCHandlerInitializer
@@ -15,15 +12,7 @@ trait EndHandler {
       request: EndInputChannelRequest,
       ctx: AsyncRPCContext
   ): Future[EmptyReturn] = {
-    var channelId = request.channelId
-    if (dp.executor.isInstanceOf[SourceOperatorExecutor]) {
-      channelId = ChannelIdentity(ActorVirtualIdentity("SOURCE_STARTER"), actorId, isControl = false)
-      // for source operator: add a virtual input channel just for kicking off the execution
-      val dummyInputPortId = PortIdentity()
-      dp.inputManager.addPort(dummyInputPortId, null)
-      dp.inputGateway.getChannel(channelId).setPortId(dummyInputPortId)
-    }
-    dp.endOfInputChannel(channelId)
+    dp.endOfInputChannel(request.channelId)
     EmptyReturn()
   }
 }
