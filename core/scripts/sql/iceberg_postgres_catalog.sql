b1d899b0b9621507c3d244af5c60d15f2fe24e55
Jiadong Bai <bobbaicloudwithpants@gmail.com>
2025-03-21 13:30:41 -0700

    add docker file for build and docker compose

--- core/scripts/sql/iceberg_postgres_catalog.sql
+++ core/scripts/sql/iceberg_postgres_catalog.sql
@@ -1,16 +1,18 @@
--- Important: replace "texera_iceberg_admin" and "password" with your own username
--- and password before executing the script.
-\set db_user texera_iceberg_admin
-\set db_password '\'password\''
+\set db_user texera
 
+\c postgres
+
+DROP DATABASE IF EXISTS texera_iceberg_catalog;
 CREATE DATABASE texera_iceberg_catalog;
 
-CREATE USER :db_user WITH ENCRYPTED PASSWORD :db_password;
+-- Important: the database must exist before you GRANT or ALTER
+-- So this part should ideally go into a separate connection session
+-- or follow the DO block with a short delay to allow connection refresh
 
+-- Next, grant privileges
 GRANT ALL PRIVILEGES ON DATABASE texera_iceberg_catalog TO :db_user;
-
 ALTER DATABASE texera_iceberg_catalog OWNER TO :db_user;
 
-\c texera_iceberg_catalog;
+\c texera_iceberg_catalog
 
 GRANT ALL ON SCHEMA public TO :db_user;
