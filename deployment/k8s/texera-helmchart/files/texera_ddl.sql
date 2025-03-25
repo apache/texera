23080f88f8da32db0bf9961d112db1a95823c1bf
Jiadong Bai <bobbaicloudwithpants@gmail.com>
2025-03-25 16:32:34 -0700

    first working version

--- deployment/k8s/texera-helmchart/files/texera_ddl.sql
+++ deployment/k8s/texera-helmchart/files/texera_ddl.sql
@@ -334,4 +334,15 @@ CREATE TABLE IF NOT EXISTS dataset_view_count
     FOREIGN KEY (did) REFERENCES dataset(did) ON DELETE CASCADE
     );
 
+-- workflow_computing_unit table
+CREATE TABLE IF NOT EXISTS workflow_computing_unit
+(
+    uid                INT           NOT NULL,
+    name               VARCHAR(128)  NOT NULL,
+    cuid               SERIAL PRIMARY KEY,
+    creation_time      TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    terminate_time     TIMESTAMP  DEFAULT NULL,
+    FOREIGN KEY (uid) REFERENCES "user"(uid) ON DELETE CASCADE
+    );
+
 
