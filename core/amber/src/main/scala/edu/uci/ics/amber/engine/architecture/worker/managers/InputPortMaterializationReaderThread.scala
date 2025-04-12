da191d7c5f8ffdcd95f9db3fdda2ae3c98ca4dba
Xiao-zhen-Liu <xiaozl3@uci.edu>
2025-04-12 12:09:37 -0700

    WIP for function calling.

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/worker/managers/InputPortMaterializationReaderThread.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/worker/managers/InputPortMaterializationReaderThread.scala
@@ -1,16 +1,14 @@
 package edu.uci.ics.amber.engine.architecture.worker.managers
 
-import edu.uci.ics.amber.core.marker.{EndOfInputChannel, Marker}
+import edu.uci.ics.amber.core.marker.{EndOfInputChannel, Marker, StartOfInputChannel}
 import edu.uci.ics.amber.core.storage.model.VirtualDocument
 import edu.uci.ics.amber.core.tuple.Tuple
 import edu.uci.ics.amber.core.virtualidentity.{ActorVirtualIdentity, ChannelIdentity}
-import edu.uci.ics.amber.engine.architecture.worker.WorkflowWorker.{
-  DPInputQueueElement,
-  FIFOMessageElement
-}
+import edu.uci.ics.amber.engine.architecture.worker.WorkflowWorker.{DPInputQueueElement, FIFOMessageElement}
 import edu.uci.ics.amber.engine.common.AmberConfig
 import edu.uci.ics.amber.engine.common.ambermessage.{DataFrame, MarkerFrame, WorkflowFIFOMessage}
 import edu.uci.ics.amber.engine.common.virtualidentity.util.MATERIALIZATION_READER_ACTOR_PREFIX
+import edu.uci.ics.amber.util.VirtualIdentityUtils.getWorkerIndex
 
 import java.util.concurrent.LinkedBlockingQueue
 import java.util.concurrent.atomic.AtomicLong
@@ -27,15 +25,20 @@ class InputPortMaterializationReaderThread(
   private val sequenceNum = new AtomicLong()
   private val buffer = new ArrayBuffer[Tuple]()
   private val storageReadIterator = materialization.get()
+  private lazy val workerIndex = getWorkerIndex(workerActorId)
   private lazy val channelId = {
     // A unique channel between this thread (dummy actor) and the worker actor.
     val storageURIStr = materialization.getURI.toString
     val fromActorId = ActorVirtualIdentity(MATERIALIZATION_READER_ACTOR_PREFIX + storageURIStr)
     ChannelIdentity(fromActorId, workerActorId, isControl = false)
   }
+  // TODO: Add a partitionFilter
 
   override def run(): Unit = {
+    // Notify the input port of start of input channel
+    emitMarker(StartOfInputChannel())
     try {
+      // Produce tuples
       while (storageReadIterator.hasNext) {
         buffer.append(storageReadIterator.next())
         if (buffer.size >= batchSize) {
@@ -46,6 +49,7 @@ class InputPortMaterializationReaderThread(
       if (buffer.nonEmpty) flush()
     } finally {
       emitMarker(EndOfInputChannel())
+      // FinalizeInputPort, FinalizeOutputPort, and FinalizeExecutor will be sent by dp once it receives EndOfInputChannel()
     }
   }
 
