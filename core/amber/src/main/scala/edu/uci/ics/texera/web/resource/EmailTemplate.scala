20f4fa753a0d6ab1e43dc031be4ba9e894a3cace
gspikehalo <2318002579@qq.com>
2025-04-25 20:42:23 -0700

    add comments

--- core/amber/src/main/scala/edu/uci/ics/texera/web/resource/EmailTemplate.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/resource/EmailTemplate.scala
@@ -1,6 +1,19 @@
 package edu.uci.ics.texera.web.resource
 
+/**
+  * EmailTemplate provides factory methods to generate email messages
+  * for different user notification scenarios.
+  */
 object EmailTemplate {
+
+  /**
+    * Creates an email message to notify the administrator
+    * that a new user account request is pending approval.
+    *
+    * @param receiverEmail the administrator's email address
+    * @param userEmail the email address of the user requesting an account
+    * @return an EmailMessage ready to be sent to the administrator
+    */
   def createAdminNotification(receiverEmail: String, userEmail: String): EmailMessage = {
     val subject = "New Account Request Pending Approval"
     val content =
@@ -17,6 +30,13 @@ object EmailTemplate {
     EmailMessage(subject = subject, content = content, receiver = receiverEmail)
   }
 
+  /**
+    * Creates an email message to notify the user
+    * that their account request has been received and is under review.
+    *
+    * @param receiverEmail the user's email address
+    * @return an EmailMessage ready to be sent to the user
+    */
   def createUserNotification(receiverEmail: String): EmailMessage = {
     val subject = "Account Request Received"
     val content =
