31b6f98e397c70c60967aca80e95c5b29164b7d7
gspikehalo <2318002579@qq.com>
2025-04-27 20:59:35 -0700

    resolve comments

--- core/amber/src/main/scala/edu/uci/ics/texera/web/resource/EmailTemplate.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/resource/EmailTemplate.scala
@@ -7,48 +7,47 @@ package edu.uci.ics.texera.web.resource
 object EmailTemplate {
 
   /**
-    * Creates an email message to notify the administrator
-    * that a new user account request is pending approval.
+    * Creates an email message for user registration notifications.
+    * Depending on the 'toAdmin' flag, it either notifies an administrator
+    * of a pending account request or acknowledges receipt to the user.
     *
-    * @param receiverEmail the administrator's email address
-    * @param userEmail the email address of the user requesting an account
-    * @return an EmailMessage ready to be sent to the administrator
+    * @param receiverEmail the email address of the receiver (admin or user)
+    * @param userEmail optional; the email address of the user requesting an account (only needed if toAdmin is true)
+    * @param toAdmin flag indicating whether the notification is for the admin (true) or the user (false)
+    * @return an EmailMessage ready to be sent
     */
-  def createAdminNotification(receiverEmail: String, userEmail: String): EmailMessage = {
-    val subject = "New Account Request Pending Approval"
-    val content =
-      s"""
-         |Hello Admin,
-         |
-         |A new user has attempted to log in or register, but their account is not yet approved.
-         |Please review the account request for the following email:
-         |
-         |$userEmail
-         |
-         |Thanks!
-         |""".stripMargin
-    EmailMessage(subject = subject, content = content, receiver = receiverEmail)
-  }
-
-  /**
-    * Creates an email message to notify the user
-    * that their account request has been received and is under review.
-    *
-    * @param receiverEmail the user's email address
-    * @return an EmailMessage ready to be sent to the user
-    */
-  def createUserNotification(receiverEmail: String): EmailMessage = {
-    val subject = "Account Request Received"
-    val content =
-      s"""
-         |Hello,
-         |
-         |Thank you for submitting your account request.
-         |We have received your request and it is currently under review.
-         |Please be patient during this process. You will be notified once your account has been approved.
-         |
-         |Thank you for your interest!
-         |""".stripMargin
-    EmailMessage(subject = subject, content = content, receiver = receiverEmail)
+  def userRegistrationNotification(
+      receiverEmail: String,
+      userEmail: Option[String],
+      toAdmin: Boolean
+  ): EmailMessage = {
+    if (toAdmin) {
+      val subject = "New Account Request Pending Approval"
+      val content =
+        s"""
+           |Hello Admin,
+           |
+           |A new user has attempted to log in or register, but their account is not yet approved.
+           |Please review the account request for the following email:
+           |
+           |${userEmail.getOrElse("Unknown")}
+           |
+           |Thanks!
+           |""".stripMargin
+      EmailMessage(subject = subject, content = content, receiver = receiverEmail)
+    } else {
+      val subject = "Account Request Received"
+      val content =
+        s"""
+           |Hello,
+           |
+           |Thank you for submitting your account request.
+           |We have received your request and it is currently under review.
+           |Please be patient during this process. You will be notified once your account has been approved.
+           |
+           |Thank you for your interest!
+           |""".stripMargin
+      EmailMessage(subject = subject, content = content, receiver = receiverEmail)
+    }
   }
 }
