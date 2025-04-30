a1f51a2261e309bc2936a8ff4783a04c241675c5
Xiao-zhen-Liu <xiaozl3@uci.edu>
2025-04-29 19:33:39 -0700

    Add partitioner in InputPortMaterializationReaderThread

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/worker/managers/InputPortMaterializationReaderThread.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/worker/managers/InputPortMaterializationReaderThread.scala
@@ -5,6 +5,7 @@ import edu.uci.ics.amber.core.storage.DocumentFactory
 import edu.uci.ics.amber.core.storage.model.VirtualDocument
 import edu.uci.ics.amber.core.tuple.Tuple
 import edu.uci.ics.amber.core.virtualidentity.{ActorVirtualIdentity, ChannelIdentity}
+import edu.uci.ics.amber.engine.architecture.messaginglayer.OutputManager.toPartitioner
 import edu.uci.ics.amber.engine.architecture.sendsemantics.partitionings.Partitioning
 import edu.uci.ics.amber.engine.architecture.worker.WorkflowWorker.{
   DPInputQueueElement,
@@ -12,10 +13,7 @@ import edu.uci.ics.amber.engine.architecture.worker.WorkflowWorker.{
 }
 import edu.uci.ics.amber.engine.common.AmberConfig
 import edu.uci.ics.amber.engine.common.ambermessage.{DataFrame, MarkerFrame, WorkflowFIFOMessage}
-import edu.uci.ics.amber.util.VirtualIdentityUtils.{
-  getFromActorIdForInputPortStorage,
-  getWorkerIndex
-}
+import edu.uci.ics.amber.util.VirtualIdentityUtils.getFromActorIdForInputPortStorage
 
 import java.net.URI
 import java.util.concurrent.LinkedBlockingQueue
@@ -33,14 +31,13 @@ class InputPortMaterializationReaderThread(
 
   private val sequenceNum = new AtomicLong()
   private val buffer = new ArrayBuffer[Tuple]()
-  // TODO: Support multi-worker
-  private lazy val workerIndex = getWorkerIndex(workerActorId)
   private lazy val channelId = {
     // A unique channel between this thread (dummy actor) and the worker actor.
     val fromActorId: ActorVirtualIdentity =
       getFromActorIdForInputPortStorage(uri.toString, workerActorId)
     ChannelIdentity(fromActorId, workerActorId, isControl = false)
   }
+  private val partitioner = toPartitioner(partitioning, workerActorId)
 
   override def run(): Unit = {
     // Notify the input port of start of input channel
@@ -51,12 +48,16 @@ class InputPortMaterializationReaderThread(
         ._1
         .asInstanceOf[VirtualDocument[Tuple]]
       val storageReadIterator = materialization.get()
-      // TODO: Support multi-worker
       // Produce tuples
       while (storageReadIterator.hasNext) {
-        buffer.append(storageReadIterator.next())
-        if (buffer.size >= batchSize) {
-          flush()
+        val tuple = storageReadIterator.next()
+        if (partitioner.getBucketIndex(tuple).toList.exists( bucketIndex=>
+          partitioner.allReceivers(bucketIndex) == workerActorId
+        )) {
+          buffer.append(tuple)
+          if (buffer.size >= batchSize) {
+            flush()
+          }
         }
       }
       // Flush any remaining tuples in the buffer.
