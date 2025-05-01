1c8556af57269cf647dc23967cda5d54af0058a4
ali <a_icam@yahoo.com>
2025-05-01 09:25:06 -0700

    fix: ux of workflow description

--- core/gui/src/app/dashboard/component/user/list-item/list-item.component.ts
+++ core/gui/src/app/dashboard/component/user/list-item/list-item.component.ts
@@ -59,6 +59,8 @@ export class ListItemComponent implements OnInit, OnChanges {
   @Input() isPrivateSearch = false;
   @Input() editable = false;
   private _entry?: DashboardEntry;
+  hovering: boolean = false;
+
   @Input()
   get entry(): DashboardEntry {
     if (!this._entry) {
