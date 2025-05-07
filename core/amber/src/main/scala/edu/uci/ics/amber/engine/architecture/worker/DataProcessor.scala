f8ed485390b32815dbcf5e120da675055dd9cc29
Xinyuan Lin <xinyual3@uci.edu>
2025-05-06 21:50:17 -0700

    update

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/worker/DataProcessor.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/worker/DataProcessor.scala
@@ -113,8 +113,6 @@ class DataProcessor(
     * process start of an input port with Executor.produceStateOnStart().
     */
   def startOfInputChannel(channelId: ChannelIdentity): Unit = {
-    val b = this.inputGateway.getChannel(channelId)
-    println(s"start of input channel",b)
     val portId = this.inputGateway.getChannel(channelId).getPortId
     try {
       val outputState = executor.produceStateOnStart(portId.id)
@@ -243,8 +241,6 @@ class DataProcessor(
   }
 
   def endOfInputChannel(channelId: ChannelIdentity): Unit = {
-    val a = this.inputGateway.getChannel(channelId)
-    println(s"end of input channel",a)
     val portId = this.inputGateway.getChannel(channelId).getPortId
     this.inputManager.getPort(portId).channels(channelId) = true
     if (inputManager.isPortCompleted(portId)) {
