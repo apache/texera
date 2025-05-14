662a69baa34864083997275cad79c1def7b0e112
Xinyuan Lin <xinyual3@uci.edu>
2025-05-14 16:28:49 -0700

    update

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/worker/promisehandlers/EndHandler.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/worker/promisehandlers/EndHandler.scala
@@ -3,10 +3,7 @@ package edu.uci.ics.amber.engine.architecture.worker.promisehandlers
 import com.twitter.util.Future
 import edu.uci.ics.amber.core.executor.SourceOperatorExecutor
 import edu.uci.ics.amber.core.virtualidentity.{ActorVirtualIdentity, ChannelIdentity}
-import edu.uci.ics.amber.engine.architecture.rpc.controlcommands.{
-  AsyncRPCContext,
-  EndInputChannelRequest
-}
+import edu.uci.ics.amber.engine.architecture.rpc.controlcommands.{AsyncRPCContext, EmptyRequest}
 import edu.uci.ics.amber.engine.architecture.rpc.controlreturns.EmptyReturn
 import edu.uci.ics.amber.engine.architecture.worker.DataProcessorRPCHandlerInitializer
 
@@ -14,14 +11,14 @@ trait EndHandler {
   this: DataProcessorRPCHandlerInitializer =>
 
   override def endWorker(
-      request: EndInputChannelRequest,
+      request: EmptyRequest,
       ctx: AsyncRPCContext
   ): Future[EmptyReturn] = {
     logger.info("Ending the worker.")
     val channelId = if (dp.executor.isInstanceOf[SourceOperatorExecutor]) {
       ChannelIdentity(ActorVirtualIdentity("SOURCE_STARTER"), actorId, isControl = false)
     } else {
-      request.channelId
+      this.dp.inputManager.currentChannelId
     }
     dp.endOfInputChannel(channelId)
     EmptyReturn()
