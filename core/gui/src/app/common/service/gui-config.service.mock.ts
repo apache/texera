e2198cdddf9a53eab5b5dd5cd390fa0d778eec5c
Jiadong Bai <bobbaicloudwithpants@gmail.com>
2025-06-01 12:56:32 -0700

    fix the header

--- core/gui/src/app/common/service/gui-config.service.mock.ts
+++ core/gui/src/app/common/service/gui-config.service.mock.ts
@@ -1,3 +1,22 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import { Injectable } from "@angular/core";
 import { Observable, of } from "rxjs";
 import { GuiConfig } from "../type/gui-config";
@@ -40,11 +59,9 @@ export class MockGuiConfigService {
   }
 
   load(): Observable<void> {
-    // Mock implementation - immediately returns success
     return of(undefined);
   }
 
-  // Allow tests to override specific config values
   setConfig(config: Partial<GuiConfig>): void {
     this._config = { ...this._config, ...config };
   }
