fd9bbe6c589a12b202752448f2fa62b6ba50f069
Xiao-zhen-Liu <xiaozl3@uci.edu>
2025-06-04 15:27:41 -0700

    wip fix.

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/messaginglayer/InputManager.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/messaginglayer/InputManager.scala
@@ -56,7 +56,7 @@ class InputManager(
       partitionings: List[Partitioning]
   ): Unit = {
     if (urisToRead.size != partitionings.size) println("Unmatched: ", urisToRead, partitionings)
-    if(urisToRead.nonEmpty || partitionings.nonEmpty)
+    if (urisToRead.nonEmpty || partitionings.nonEmpty)
       assert(urisToRead.size == partitionings.size)
     // each port can only be added and initialized once.
     if (this.ports.contains(portId)) {
