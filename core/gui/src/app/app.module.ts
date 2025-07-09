0256cc0287417ae33dd48e97434215cf729c1c79
zaoduyuan <zaoduyuan@gmail.com>
2025-07-09 11:19:45 -0700

    remove unnecessary comment

--- core/gui/src/app/app.module.ts
+++ core/gui/src/app/app.module.ts
@@ -170,15 +170,12 @@ import { ComputingUnitSelectionComponent } from "./workspace/component/power-but
 import { NzSliderModule } from "ng-zorro-antd/slider";
 import { AdminSettingsComponent } from "./dashboard/component/admin/settings/admin-settings.component";
 import { catchError, of } from "rxjs";
-
-// MODIFICATION 1: IMPORT THE CUSTOM COMPONENT
 import { FormlyRepeatDndComponent } from "./common/formly/repeat-dnd/repeat-dnd.component";
 
 registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    // MODIFICATION 2: ADD THE CUSTOM COMPONENT TO THE DECLARATIONS ARRAY
     FormlyRepeatDndComponent,
     AdminGmailComponent,
     PublicProjectComponent,
