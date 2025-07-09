3e7e0e63cb83d8175954bb408db4d6751e2fa885
Xiao-zhen-Liu <xiaozl3@uci.edu>
2025-07-08 20:34:25 -0700

    refactor: use scala deprecated

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/scheduling/ExpansionGreedyScheduleGenerator.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/scheduling/ExpansionGreedyScheduleGenerator.scala
@@ -44,7 +44,7 @@ import scala.collection.mutable
 import scala.jdk.CollectionConverters.CollectionHasAsScala
 
 @deprecated(
-  "This greedy schedule generator will be removed in the future. Use `CostBasedScheduleGenerator` instead."
+  "This greedy schedule generator will be removed in the future. Use CostBasedScheduleGenerator instead."
 )
 class ExpansionGreedyScheduleGenerator(
     workflowContext: WorkflowContext,
