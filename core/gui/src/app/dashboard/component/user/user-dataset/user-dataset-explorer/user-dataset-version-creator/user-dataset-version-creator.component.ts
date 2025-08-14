2812a97dfca14fb02c5c0c7cfa78ae9262e1c3da
Seongjin Yoon <seongjin123.sy@gmail.com>
2025-08-14 15:21:11 -0700

    m

--- core/gui/src/app/dashboard/component/user/user-dataset/user-dataset-explorer/user-dataset-version-creator/user-dataset-version-creator.component.ts
+++ core/gui/src/app/dashboard/component/user/user-dataset/user-dataset-explorer/user-dataset-version-creator/user-dataset-version-creator.component.ts
@@ -44,6 +44,7 @@ export class UserDatasetVersionCreatorComponent implements OnInit {
   model: any = {};
   fields: FormlyFieldConfig[] = [];
   isDatasetPublic: boolean = false;
+  isDatasetDownloadable: boolean = true;
 
   // used when creating the dataset
   isDatasetNameSanitized: boolean = false;
@@ -168,6 +169,7 @@ export class UserDatasetVersionCreatorComponent implements OnInit {
         name: sanitizedName,
         description: this.form.get("description")?.value,
         isPublic: this.isDatasetPublic,
+        isDownloadable: this.isDatasetDownloadable,
         did: undefined,
         ownerUid: undefined,
         storagePath: undefined,
@@ -201,5 +203,20 @@ export class UserDatasetVersionCreatorComponent implements OnInit {
   onPublicStatusChange(newValue: boolean): void {
     // Handle the change in dataset public status
     this.isDatasetPublic = newValue;
+    
+    // If dataset becomes private, it cannot be downloadable
+    if (!newValue) {
+      this.isDatasetDownloadable = false;
+    }
+  }
+
+  onDownloadableStatusChange(newValue: boolean): void {
+    // Handle the change in dataset downloadable status
+    // Only allow downloadable if dataset is public
+    if (newValue && !this.isDatasetPublic) {
+      // Prevent the change and show feedback
+      return;
+    }
+    this.isDatasetDownloadable = newValue;
   }
 }
