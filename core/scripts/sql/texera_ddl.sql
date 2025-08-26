6b48dbe360d5e8f86e739df739976119a58df554
Jae Yun Kim <jaeyk14@uci.edu>
2025-08-26 13:50:21 -0700

    Changed ddl files

--- core/scripts/sql/texera_ddl.sql
+++ core/scripts/sql/texera_ddl.sql
@@ -350,13 +350,13 @@ CREATE TABLE IF NOT EXISTS site_settings
     updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
 
--- time_log table
-CREATE TABLE IF NOT EXISTS time_log
+-- user_time_log table
+CREATE TABLE IF NOT EXISTS user_time_log
 (
     uid            INT          NOT NULL
         PRIMARY KEY
         REFERENCES "user"(uid),
-    last_login     TIMESTAMPTZ
+    last_active_time     TIMESTAMPTZ
 );
 
 -- computing_unit_user_access table
--- deployment/k8s/texera-helmchart/files/texera_ddl.sql
+++ deployment/k8s/texera-helmchart/files/texera_ddl.sql
@@ -385,12 +385,12 @@ CREATE TABLE IF NOT EXISTS site_settings
     );
 
 -- time_log table
-CREATE TABLE IF NOT EXISTS time_log
+CREATE TABLE IF NOT EXISTS user_ime_log
 (
     uid            INT          NOT NULL
         PRIMARY KEY
         REFERENCES "user"(uid),
-    last_login     TIMESTAMPTZ
+    last_active_time     TIMESTAMPTZ
 );
 
 -- computing_unit_user_access table
