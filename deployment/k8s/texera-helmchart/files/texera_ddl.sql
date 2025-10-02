de30ccee7b762cd7dc844e0e3591ad3f516ab900
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2025-10-02 23:21:44 +0100

    revert: texera ddl style change

--- deployment/k8s/texera-helmchart/files/texera_ddl.sql
+++ deployment/k8s/texera-helmchart/files/texera_ddl.sql
@@ -264,11 +264,11 @@ CREATE TABLE IF NOT EXISTS public_project
 -- dataset
 CREATE TABLE IF NOT EXISTS dataset
 (
-    did             SERIAL PRIMARY KEY,
-    owner_uid       INT NOT NULL,
-    name            VARCHAR(128) NOT NULL,
+    did            SERIAL PRIMARY KEY,
+    owner_uid      INT NOT NULL,
+    name           VARCHAR(128) NOT NULL,
     repository_name VARCHAR(128),
-    is_public       BOOLEAN NOT NULL DEFAULT TRUE,
+    is_public      BOOLEAN NOT NULL DEFAULT TRUE,
     is_downloadable BOOLEAN NOT NULL DEFAULT TRUE,
     description    VARCHAR(512) NOT NULL,
     creation_time  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
