1da2b80e6f94b0a08a0ebf30643976b6340d1b58
Xinyuan Lin <xinyual3@uci.edu>
2025-10-14 19:02:14 -0700

    fix fmt

--- frontend/src/app/workspace/component/menu/menu.component.ts
+++ frontend/src/app/workspace/component/menu/menu.component.ts
@@ -89,6 +89,7 @@ export class MenuComponent implements OnInit, OnDestroy {
   public workflowId?: number;
   public isExportDeactivate: boolean = false;
   public grid: boolean = false;
+  public region: boolean = false;
   protected readonly DASHBOARD_USER_WORKFLOW = DASHBOARD_USER_WORKFLOW;
 
   @Input() public writeAccess: boolean = false;
@@ -462,8 +463,8 @@ export class MenuComponent implements OnInit, OnDestroy {
     this.workflowActionService.getJointGraphWrapper().mainPaper.setGridSize(this.grid ? 2 : 1);
   }
 
-  public onClickToggleRegion(): void {
-    this.workflowActionService.getJointGraphWrapper().mainPaper.el.classList.toggle("hide-region");
+  public onRegionChange(): void {
+    this.workflowActionService.getJointGraphWrapper().mainPaper.el.classList.toggle("hide-region", !this.region);
   }
 
 
