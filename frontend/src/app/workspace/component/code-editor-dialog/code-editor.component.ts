c673ea4bac9cf0baad0e8ca5fdb269cffaa1ab1b
Kevin Li <jingxl18@uci.edu>
2025-11-18 16:45:48 -0800

    ran prettier-eslint

--- frontend/src/app/workspace/component/code-editor-dialog/code-editor.component.ts
+++ frontend/src/app/workspace/component/code-editor-dialog/code-editor.component.ts
@@ -17,7 +17,16 @@
  * under the License.
  */
 
-import { AfterViewInit, Component, ComponentRef, ElementRef, OnDestroy, Type, ViewChild, HostListener } from "@angular/core";
+import {
+  AfterViewInit,
+  Component,
+  ComponentRef,
+  ElementRef,
+  OnDestroy,
+  Type,
+  ViewChild,
+  HostListener,
+} from "@angular/core";
 import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
 import { WorkflowActionService } from "../../service/workflow-graph/model/workflow-action.service";
 import { WorkflowVersionService } from "../../../dashboard/service/user/workflow-version/workflow-version.service";
@@ -516,7 +525,7 @@ export class CodeEditorComponent implements AfterViewInit, SafeStyle, OnDestroy 
     this.code?.insert(insertOffset, annotations);
   }
 
-  @HostListener('window:resize')
+  @HostListener("window:resize")
   onWindowResize() {
     this.adjustEditorSize();
   }
@@ -525,17 +534,13 @@ export class CodeEditorComponent implements AfterViewInit, SafeStyle, OnDestroy 
     const container = this.containerElement.nativeElement;
     const viewportWidth = window.innerWidth;
     const viewportHeight = window.innerHeight;
-
     const rect = container.getBoundingClientRect();
-
     if (rect.right > viewportWidth) {
       container.style.width = `${viewportWidth - rect.left}px`;
     }
-
     if (rect.bottom > viewportHeight) {
       container.style.height = `${viewportHeight - rect.top}px`;
     }
-
     this.editorWrapper.getEditor()?.layout();
   }
   onFocus() {
