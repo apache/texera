3ed7fb63b120eca4705d5dc55694a5146b1d4010
Kunwoo Park <kunwp1@uci.edu>
2025-12-12 19:11:11 -0800

    Address comments

--- amber/src/main/python/core/models/schema/arrow_schema_utils.py
+++ amber/src/main/python/core/models/schema/arrow_schema_utils.py
@@ -23,54 +23,11 @@ handling LARGE_BINARY metadata preservation.
 import pyarrow as pa
 from typing import Mapping
 
-from core.models.schema.attribute_type import (
-    AttributeType,
-    FROM_ARROW_MAPPING,
-    TO_ARROW_MAPPING,
+from core.models.schema.attribute_type import AttributeType
+from core.models.schema.attribute_type_utils import (
+    detect_attribute_type_from_arrow_field,
+    create_arrow_field_with_metadata,
 )
-
-# Metadata key used to mark LARGE_BINARY fields in Arrow schemas
-TEXERA_TYPE_METADATA_KEY = b"texera_type"
-LARGE_BINARY_METADATA_VALUE = b"LARGE_BINARY"
-
-
-def detect_attribute_type_from_arrow_field(field: pa.Field) -> AttributeType:
-    """
-    Detects the AttributeType from an Arrow field, checking metadata for LARGE_BINARY.
-
-    :param field: PyArrow field that may contain metadata
-    :return: The detected AttributeType
-    """
-    # Check metadata for LARGE_BINARY type
-    # (can be stored by either Scala ArrowUtils or Python)
-    is_large_binary = (
-        field.metadata
-        and field.metadata.get(TEXERA_TYPE_METADATA_KEY) == LARGE_BINARY_METADATA_VALUE
-    )
-
-    if is_large_binary:
-        return AttributeType.LARGE_BINARY
-    else:
-        return FROM_ARROW_MAPPING[field.type.id]
-
-
-def create_arrow_field_with_metadata(
-    attr_name: str, attr_type: AttributeType
-) -> pa.Field:
-    """
-    Creates a PyArrow field with appropriate metadata for the given AttributeType.
-
-    :param attr_name: Name of the attribute
-    :param attr_type: The AttributeType
-    :return: PyArrow field with metadata if needed
-    """
-    metadata = (
-        {TEXERA_TYPE_METADATA_KEY: LARGE_BINARY_METADATA_VALUE}
-        if attr_type == AttributeType.LARGE_BINARY
-        else None
-    )
-
-    return pa.field(attr_name, TO_ARROW_MAPPING[attr_type], metadata=metadata)
 
 
 def arrow_schema_to_attr_types(arrow_schema: pa.Schema) -> dict[str, AttributeType]:
