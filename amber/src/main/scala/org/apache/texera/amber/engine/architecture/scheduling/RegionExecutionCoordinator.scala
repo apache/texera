1cd48fd12b88ab599b6262fb4b98c0a0baad487b
Xinyuan Lin <xinyual3@uci.edu>
2026-02-15 00:47:24 -0800

    update

--- amber/src/main/scala/org/apache/texera/amber/engine/architecture/scheduling/RegionExecutionCoordinator.scala
+++ amber/src/main/scala/org/apache/texera/amber/engine/architecture/scheduling/RegionExecutionCoordinator.scala
@@ -211,13 +211,13 @@ class RegionExecutionCoordinator(
       attempt: Int = 1
   ): Future[Unit] = {
     terminateWorkers(regionExecution).rescue { case err =>
-        logger.warn(
-          s"Failed to terminate region ${region.id.id} on attempt $attempt. Retrying in ${killRetryDelay.inMilliseconds} ms.",
-          err
-        )
-        Future
-          .sleep(killRetryDelay)(killRetryTimer)
-          .flatMap(_ => terminateWorkersWithRetry(regionExecution, attempt + 1))
+      logger.warn(
+        s"Failed to terminate region ${region.id.id} on attempt $attempt. Retrying in ${killRetryDelay.inMilliseconds} ms.",
+        err
+      )
+      Future
+        .sleep(killRetryDelay)(killRetryTimer)
+        .flatMap(_ => terminateWorkersWithRetry(regionExecution, attempt + 1))
     }
   }
 
