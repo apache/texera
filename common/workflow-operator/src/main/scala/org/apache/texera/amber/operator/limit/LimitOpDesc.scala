d2ebecbfd90de1667b8c3e3aac804c9a54b4eb34
Xinyuan Lin <xinyual3@uci.edu>
2026-04-08 18:32:33 -0700

    update

--- common/workflow-operator/src/main/scala/org/apache/texera/amber/operator/limit/LimitOpDesc.scala
+++ common/workflow-operator/src/main/scala/org/apache/texera/amber/operator/limit/LimitOpDesc.scala
@@ -49,6 +49,7 @@ class LimitOpDesc extends LogicalOp {
         OpExecWithClassName(
           "org.apache.texera.amber.operator.limit.LimitOpExec",
           objectMapper.writeValueAsString(this)
+
         )
       )
       .withInputPorts(operatorInfo.inputPorts)
