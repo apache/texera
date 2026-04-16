e7665e8e5632140037c26ecd51427e2c7baeaa8d
Xinyuan Lin <xinyual3@uci.edu>
2026-04-16 15:49:19 -0700

    fix fmt

--- common/workflow-operator/src/main/scala/org/apache/texera/amber/operator/LogicalOp.scala
+++ common/workflow-operator/src/main/scala/org/apache/texera/amber/operator/LogicalOp.scala
@@ -139,10 +139,7 @@ import org.apache.texera.amber.operator.visualization.waterfallChart.WaterfallCh
 import org.apache.texera.amber.operator.visualization.wordCloud.WordCloudOpDesc
 import org.apache.commons.lang3.builder.{EqualsBuilder, HashCodeBuilder, ToStringBuilder}
 import org.apache.texera.amber.operator.sklearn.testing.SklearnTestingOpDesc
-import org.apache.texera.amber.operator.source.scan.file.{
-  FileScanOpDesc,
-  FileScanSourceOpDesc,
-}
+import org.apache.texera.amber.operator.source.scan.file.{FileScanOpDesc, FileScanSourceOpDesc}
 import org.apache.texera.amber.operator.visualization.stripChart.StripChartOpDesc
 
 import java.util.UUID
