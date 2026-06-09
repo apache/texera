2f2cfb27f146ebe5aa8ac6a9dcab2abadc800e92
Neilk1021 <neilfuntube@gmail.com>
2026-06-08 23:28:56 -0700

    style: remove UTF-8 BOM and redundant vitest imports

--- frontend/src/app/dashboard/component/user/sort-button/sort-button.component.spec.ts
+++ frontend/src/app/dashboard/component/user/sort-button/sort-button.component.spec.ts
@@ -1,4 +1,4 @@
-﻿/**
+/**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -20,7 +20,6 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { SortButtonComponent } from "./sort-button.component";
 import { SortMethod } from "../../../type/sort-method";
-import { describe, beforeEach, it, expect, vi } from "vitest";
 
 describe("SortButtonComponent", () => {
   let component: SortButtonComponent;
