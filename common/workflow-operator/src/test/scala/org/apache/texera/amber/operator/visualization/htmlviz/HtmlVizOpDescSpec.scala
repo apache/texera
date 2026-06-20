581a3f9c466271524d9aa572eb2b38be332563fe
Xinyuan Lin <xinyual3@uci.edu>
2026-06-20 00:14:30 -0700

    test(workflow-operator): add unit test coverage for visualization operator descriptors
    Pin behavior of three previously-untested visualization descriptors:
      - HtmlVizOpDesc: operatorInfo (HTML Visualizer, VISUALIZATION_MEDIA_GROUP); wires HtmlVizOpExec; schema propagation emits a single html-content STRING column; htmlContentAttrName default
      - CandlestickChartOpDesc: operatorInfo (Candlestick Chart, VISUALIZATION_FINANCIAL_GROUP); getOutputSchemas html-content STRING; OHLC field defaults; generatePythonCode emits a Plotly Candlestick figure; field round-trip
      - Histogram2DOpDesc: operatorInfo (Histogram2D, VISUALIZATION_STATISTICAL_GROUP); getOutputSchemas html-content STRING; defaults (xBins/yBins=10, normalize=DENSITY); generatePythonCode rejects a non-positive bin count; field round-trip
    
    getPhysicalOp tests (HtmlViz) assert port IDENTITIES (keySet) rather than counts.
    15 new tests; scalafmtCheck + scalafixAll --check clean; no production-code changes.

---
+++ common/workflow-operator/src/test/scala/org/apache/texera/amber/operator/visualization/htmlviz/HtmlVizOpDescSpec.scala
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.texera.amber.operator.visualization.htmlviz
+
+import org.apache.texera.amber.core.executor.OpExecWithClassName
+import org.apache.texera.amber.core.tuple.{Attribute, AttributeType, Schema}
+import org.apache.texera.amber.core.virtualidentity.{ExecutionIdentity, WorkflowIdentity}
+import org.apache.texera.amber.core.workflow.PortIdentity
+import org.apache.texera.amber.operator.metadata.OperatorGroupConstants
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class HtmlVizOpDescSpec extends AnyFlatSpec with Matchers {
+
+  private val workflowId = WorkflowIdentity(1L)
+  private val executionId = ExecutionIdentity(1L)
+
+  "HtmlVizOpDesc.operatorInfo" should "advertise the name and Media visualization group" in {
+    val info = (new HtmlVizOpDesc).operatorInfo
+    info.userFriendlyName shouldBe "HTML Visualizer"
+    info.operatorGroupName shouldBe OperatorGroupConstants.VISUALIZATION_MEDIA_GROUP
+    info.inputPorts should have length 1
+    info.outputPorts should have length 1
+  }
+
+  "HtmlVizOpDesc.htmlContentAttrName" should "default to the empty string" in {
+    (new HtmlVizOpDesc).htmlContentAttrName shouldBe ""
+  }
+
+  "HtmlVizOpDesc.getPhysicalOp" should "wire HtmlVizOpExec and carry port identities" in {
+    val op = new HtmlVizOpDesc
+    op.htmlContentAttrName = "html"
+    val physical = op.getPhysicalOp(workflowId, executionId)
+    physical.opExecInitInfo match {
+      case OpExecWithClassName(className, descString) =>
+        className shouldBe "org.apache.texera.amber.operator.visualization.htmlviz.HtmlVizOpExec"
+        descString should not be empty
+      case other => fail(s"expected OpExecWithClassName, got $other")
+    }
+    physical.inputPorts.keySet shouldBe op.operatorInfo.inputPorts.map(_.id).toSet
+    physical.outputPorts.keySet shouldBe op.operatorInfo.outputPorts.map(_.id).toSet
+  }
+
+  "HtmlVizOpDesc schema propagation" should
+    "emit a single html-content STRING column regardless of input" in {
+    val op = new HtmlVizOpDesc
+    op.htmlContentAttrName = "html"
+    val input = Schema().add(new Attribute("html", AttributeType.STRING))
+    val out = op.getExternalOutputSchemas(Map(PortIdentity() -> input)).values.head
+    out shouldBe Schema().add("html-content", AttributeType.STRING)
+  }
+}
