d52652d1bd06dc7eda8e2c62a8a95bd7d12a8554
Neilk1021 <neilk1021@gmail.com>
2026-08-14 11:57:08 -0700

    fix(auth): Include new flags in config spec.

--- config-service/src/test/scala/org/apache/texera/service/resource/ConfigResourceSpec.scala
+++ config-service/src/test/scala/org/apache/texera/service/resource/ConfigResourceSpec.scala
@@ -132,6 +132,9 @@ class ConfigResourceSpec
     payload.keySet shouldBe Set(
       "localLogin",
       "googleLogin",
+      // The login page needs this before anyone is signed in, for the same reason as the other two
+      // provider flags: it decides whether the ORCID button is rendered at all.
+      "orcidLogin",
       "defaultLocalUser",
       "attributionEnabled",
       "deploymentVersionCheckEnabled",
@@ -166,6 +169,7 @@ class ConfigResourceSpec
     payload.keySet should contain noneOf (
       "localLogin",
       "googleLogin",
+      "orcidLogin",
       "defaultLocalUser",
       "attributionEnabled"
     )
